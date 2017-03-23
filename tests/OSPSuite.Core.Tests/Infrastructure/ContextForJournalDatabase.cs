87c6faed2b958229db34e136bef68aefa8865dc0
Michael Sevestre <michael@design2code.ca>
2017-03-23 11:11:20 -0400

    Fixes #19 move tests from PKSim into core and optimize some integrationstests (#20)

---
+++ tests/OSPSuite.Core.Tests/Infrastructure/ContextForJournalDatabase.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using OSPSuite.Core.Journal;
+using OSPSuite.Helpers;
+using OSPSuite.Infrastructure.Journal;
+using OSPSuite.Infrastructure.Journal.Queries;
+using OSPSuite.Utility;
+using OSPSuite.Utility.Container;
+
+namespace OSPSuite.Infrastructure
+{
+   public abstract class ContextForJournalDatabase<T> : ContextForIntegration<T>
+   {
+      private string _databaseFile;
+      protected IJournalPageFactory _journalPageFactory;
+      protected IContentLoader _contendLoader;
+      protected IDatabaseMediator _databaseMediator;
+      protected IJournalSession _journalSession;
+      protected IRelatedItemFactory _relatedItemFactory;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _databaseFile = FileHelper.GenerateTemporaryFileName();
+         _journalSession = IoC.Resolve<IJournalSession>();
+         _journalPageFactory = IoC.Resolve<IJournalPageFactory>();
+         _contendLoader = IoC.Resolve<IContentLoader>();
+         _databaseMediator = IoC.Resolve<IDatabaseMediator>();
+         _relatedItemFactory = IoC.Resolve<IRelatedItemFactory>();
+         _journalSession.Create(_databaseFile);
+      }
+
+      protected override void Context()
+      {
+      }
+
+      public override void GlobalCleanup()
+      {
+         base.GlobalCleanup();
+         _journalSession.Close();
+         FileHelper.DeleteFile(_databaseFile);
+      }
+
+      protected byte[] StringToBytes(string stringToConvert)
+      {
+         return Encoding.UTF8.GetBytes(stringToConvert);
+      }
+
+      protected string BytesToString(byte[] bytes)
+      {
+         return Encoding.UTF8.GetString(bytes);
+      }
+
+      protected JournalPage JournalPageById(string id)
+      {
+         return _databaseMediator.ExecuteQuery(new JournalPageById { Id = id });
+      }
+   }
+}
