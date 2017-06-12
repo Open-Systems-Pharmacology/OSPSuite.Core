7e622d28a3abdc30b0ebf0d9e7d7ed18dd23a929
Michael Sevestre <michael@design2code.ca>
2017-06-12 16:55:36 -0400

    Fixes #126 edit of a single curve or axis

--- src/OSPSuite.Presentation/Presenters/Charts/SingleCurveSettingsPresenter.cs
+++ src/OSPSuite.Presentation/Presenters/Charts/SingleCurveSettingsPresenter.cs
@@ -5,7 +5,7 @@ namespace OSPSuite.Presentation.Presenters.Charts
 {
    public interface ISingleCurveSettingsPresenter : IPresenter<ISingleCurveSettingsView>, IDisposablePresenter
    {
-      void Edit(IChart subjectChart, Curve subject);
+      void Edit(IChart chart, Curve curve);
    }
 
    public class SingleCurveSettingsPresenter : AbstractDisposablePresenter<ISingleCurveSettingsView, ISingleCurveSettingsPresenter>, ISingleCurveSettingsPresenter
@@ -18,15 +18,15 @@ namespace OSPSuite.Presentation.Presenters.Charts
          view.CancelVisible = false;
       }
 
-      public void Edit(IChart subjectChart, Curve subject)
+      public void Edit(IChart chart, Curve curve)
       {
-         _view.BindTo(subject);
+         _view.BindTo(curve);
 
          _view.Display();
          if(_view.Canceled)
             return;
 
-         _chartUpdater.Update(subjectChart);
+         _chartUpdater.Update(chart);
       }
    }
 }
