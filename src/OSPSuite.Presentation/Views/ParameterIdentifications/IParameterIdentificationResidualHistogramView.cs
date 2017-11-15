d5ed9df342d1b7c5c39ff1453e00c5012fb84cef
Michael Sevestre <michael@design2code.ca>
2017-11-15 11:21:25 -0500

    Fixes #279 chart copy to clipboard

--- src/OSPSuite.Presentation/Views/ParameterIdentifications/IParameterIdentificationResidualHistogramView.cs
+++ src/OSPSuite.Presentation/Views/ParameterIdentifications/IParameterIdentificationResidualHistogramView.cs
@@ -1,10 +1,14 @@
 ﻿using System.Data;
 using OSPSuite.Core.Chart;
+using OSPSuite.Core.Services;
 
 namespace OSPSuite.Presentation.Views.ParameterIdentifications
 {
-   public interface IParameterIdentificationResidualHistogramView : IView
+   public interface IParameterIdentificationResidualHistogramView : 
+      IView, 
+      ICanCopyToClipboardWithWatermark
    {
       void BindTo(DataTable gaussData, ContinuousDistributionData distributionData, DistributionSettings settings);
+      ICanCopyToClipboard CopyToClipboardManager { get; set; }
    }
 }
