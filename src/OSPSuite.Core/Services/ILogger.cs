a7d4a3118e4656bd7140fc2e19668cea80bf2d56
Michael Sevestre <michael@design2code.ca>
2017-12-01 16:45:26 -0500

    Fixes #257 log presenter debug option for dev

--- src/OSPSuite.Core/Services/ILogger.cs
+++ src/OSPSuite.Core/Services/ILogger.cs
@@ -5,9 +5,9 @@ namespace OSPSuite.Core.Services
    public interface ILogger
    {
       /// <summary>
-      /// Logs the <paramref name="message"/> using the provided <paramref name="logLevel"/> for the <paramref name="categoryName"/>
+      ///    Logs the <paramref name="message" /> using the provided <paramref name="logLevel" /> for the
+      ///    <paramref name="categoryName" />
       /// </summary>
       void AddToLog(string message, LogLevel logLevel, string categoryName);
-
    }
 }
