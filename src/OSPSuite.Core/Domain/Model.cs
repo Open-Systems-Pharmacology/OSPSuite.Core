8d8c5953db3d687041ac4ea684750454c150ead9
Michael Sevestre <michael@design2code.ca>
2017-12-13 08:39:44 -0500

    Fixes #291 Model should be ableo to retrieve MolWeight parameters for… (#292)
    * Fixes #291 Model should be ableo to retrieve MolWeight parameters for parameter defined in a Molecule
    
    * Remove outdated comment

--- src/OSPSuite.Core/Domain/Model.cs
+++ src/OSPSuite.Core/Domain/Model.cs
@@ -1,6 +1,6 @@
-﻿using OSPSuite.Utility.Extensions;
+﻿using OSPSuite.Core.Domain.Services;
+using OSPSuite.Utility.Extensions;
 using OSPSuite.Utility.Visitor;
-using OSPSuite.Core.Domain.Services;
 
 namespace OSPSuite.Core.Domain
 {
@@ -12,11 +12,13 @@ namespace OSPSuite.Core.Domain
       /// <summary>
       ///    Returns the value of the molweight <see cref="IParameter" /> defined in the model. If the parameter is not found for
       ///    the given <paramref name="quantity" />, returns <c>null</c>.
-      ///    We use the following logic: MolWeight is only available for <see cref="IMoleculeAmount" /> and
-      ///    <see cref="IObserver" />. Also the MolWeight parameter is defined in the global container
+      ///    We use the following logic:
+      ///    For a <see cref="IMoleculeAmount" /> a MolWeight parameter will be searched directly in the global container named
+      ///    after the molecule.
+      ///    For all other quantities (e.g. <see cref="IObserver" />,  <see cref="IParameter" />) a MolWeight parameter will be
+      ///    searched in the global container named after the parent.
       /// </summary>
       /// <param name="quantity">Quantity for which the molweight parameter should be retrieved</param>
-      /// <returns></returns>
       double? MolWeightFor(IQuantity quantity);
    }
 
@@ -35,39 +37,31 @@ namespace OSPSuite.Core.Domain
                Root.RemoveChild(_neighborhoods);
             }
             _neighborhoods = value;
-            if (Root != null)
-            {
-               Root.Add(_neighborhoods);
-            }
+            Root?.Add(_neighborhoods);
          }
-         get { return _neighborhoods; }
+         get => _neighborhoods;
       }
 
       public double? MolWeightFor(IQuantity quantity)
       {
-         if (Root == null)
+         if (Root == null || quantity == null)
             return null;
 
-         //Mol weight only available for molecule amount and observer
-         string moleculeName;
-         if (quantity.IsAnImplementationOf<IMoleculeAmount>())
-            moleculeName = quantity.Name;
-         else if (quantity.IsAnImplementationOf<IObserver>())
-            moleculeName = quantity.ParentContainer.Name;
-         else
+         var moleculeName = quantity.IsAnImplementationOf<IMoleculeAmount>() ? 
+            quantity.Name : 
+            quantity.ParentContainer?.Name;
+
+         if (string.IsNullOrEmpty(moleculeName))
             return null;
 
          //try to find the molweight parameter in the global molecule container
          var molWeightParameter = Root.EntityAt<IParameter>(moleculeName, Constants.Parameters.MOL_WEIGHT);
-         if (molWeightParameter != null)
-            return molWeightParameter.Value;
-
-         return null;
+         return molWeightParameter?.Value;
       }
 
       public IContainer Root
       {
-         get { return _root; }
+         get => _root;
          set
          {
             _root = value;
@@ -88,15 +82,15 @@ namespace OSPSuite.Core.Domain
       {
          base.UpdatePropertiesFrom(source, cloneManager);
 
-         var srcModel = source as IModel;
-         if (srcModel == null) return;
+         var sourceModel = source as IModel;
+         if (sourceModel == null) return;
 
          //root container must be cloned BEFORE neighborhoods
-         Root = cloneManager.Clone(srcModel.Root);
+         Root = cloneManager.Clone(sourceModel.Root);
 
          //Neighborhoods-subnode is already cloned (as one of the Root-children)
          // so only internal property must be set
-         _neighborhoods = Root.GetSingleChildByName<IContainer>(srcModel.Neighborhoods.Name);
+         _neighborhoods = Root.Container(sourceModel.Neighborhoods.Name);
       }
    }
 }
--- tests/OSPSuite.Core.Tests/Core/ModelSpecs.cs
+++ tests/OSPSuite.Core.Tests/Core/ModelSpecs.cs
@@ -1,4 +1,5 @@
-﻿using OSPSuite.BDDHelper;
+﻿using FakeItEasy;
+using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Utility.Container;
 using OSPSuite.Core.Domain;
@@ -54,6 +55,8 @@ namespace OSPSuite.Core
    {
       private MoleculeAmount _quantityWithMolWeight;
       private MoleculeAmount _quantityWithoutMolWeight;
+      private Parameter _parameterUnderMoleculeWithMolWeight;
+      private Parameter _parameterUnderMoleculeWithoutMolWeight;
 
       protected override void Context()
       {
@@ -63,24 +66,42 @@ namespace OSPSuite.Core
          _quantityWithMolWeight = new MoleculeAmount().WithName("Molecule");
          var quantityWithMolWeightContainer = new Container().WithName(_quantityWithMolWeight.Name);
          quantityWithMolWeightContainer.Add(new Parameter().WithName(Constants.Parameters.MOL_WEIGHT).WithFormula(new ConstantFormula(400)));
+         _parameterUnderMoleculeWithMolWeight = new Parameter().WithName("Concentration");
+         quantityWithMolWeightContainer.Add(_parameterUnderMoleculeWithMolWeight);
 
          _quantityWithoutMolWeight = new MoleculeAmount().WithName("Molecule2");
          var quantityWithoutMolWeightContainer = new Container().WithName(_quantityWithoutMolWeight.Name);
+         _parameterUnderMoleculeWithoutMolWeight = new Parameter().WithName("Concentration");
+
+         quantityWithoutMolWeightContainer.Add(_parameterUnderMoleculeWithoutMolWeight);
 
          sut.Root.Add(quantityWithMolWeightContainer);
          sut.Root.Add(quantityWithoutMolWeightContainer);
       }
 
       [Observation]
-      public void should_return_null_if_the_quantity_is_not_an_observer_or_an_amount()
+      public void should_return_null_if_the_quantity_is_a_parameter_without_parent()
       {
          sut.MolWeightFor(new Parameter()).ShouldBeNull();
+      }
+
+      [Observation]
+      public void should_return_null_if_the_quantity_is_null()
+      {
+         sut.MolWeightFor(null).ShouldBeNull();
       }
 
       [Observation]
       public void should_return_null_if_no_molweight_parameter_was_found_unter_the_global_container()
       {
          sut.MolWeightFor(_quantityWithoutMolWeight).ShouldBeNull();
+         sut.MolWeightFor(_parameterUnderMoleculeWithoutMolWeight).ShouldBeNull();
+      }
+
+      [Observation]
+      public void should_return_the_molweight_parameter_for_a_parameter_defined_in_a_molecule_for_which_a_molweight_parmaeter_exists()
+      {
+         sut.MolWeightFor(_parameterUnderMoleculeWithMolWeight).ShouldBeEqualTo(400);
       }
 
       [Observation]
