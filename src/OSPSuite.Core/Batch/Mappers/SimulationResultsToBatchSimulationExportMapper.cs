bde113f98f4b5b5ffef1923d30ac80ade6e14e98
Michael Sevestre <michael@design2code.ca>
2018-04-26 19:32:06 -0400

    Fixes #397 Simulation export to json: Do not save path of simulation (#398)

--- src/OSPSuite.Core/Batch/Mappers/SimulationResultsToBatchSimulationExportMapper.cs
+++ src/OSPSuite.Core/Batch/Mappers/SimulationResultsToBatchSimulationExportMapper.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.Data;
+using OSPSuite.Core.Domain.Services;
+using OSPSuite.Core.Extensions;
 using OSPSuite.Utility.Extensions;
 
 namespace OSPSuite.Core.Batch.Mappers
@@ -13,11 +15,11 @@ namespace OSPSuite.Core.Batch.Mappers
 
    public class SimulationResultsToBatchSimulationExportMapper : ISimulationResultsToBatchSimulationExportMapper
    {
-      private readonly IObjectPathFactory _objectPathFactory;
+      private readonly IEntityPathResolver _entityPathResolver;
 
-      public SimulationResultsToBatchSimulationExportMapper(IObjectPathFactory objectPathFactory)
+      public SimulationResultsToBatchSimulationExportMapper(IEntityPathResolver entityPathResolver)
       {
-         _objectPathFactory = objectPathFactory;
+         _entityPathResolver = entityPathResolver;
       }
 
       public BatchSimulationExport MapFrom(ISimulation simulation, DataRepository results)
@@ -40,21 +42,34 @@ namespace OSPSuite.Core.Batch.Mappers
       {
          return simulationModel.Root.GetAllChildren<IParameter>().Select(p => new ParameterValue
          {
-            Path = _objectPathFactory.CreateAbsoluteObjectPath(p).PathAsString,
+            Path = _entityPathResolver.PathFor(p),
             Value = p.Value
          }).ToList();
       }
 
-      private BatchOutputValues quantityResultsFrom( DataColumn column)
+      private BatchOutputValues quantityResultsFrom(DataColumn column)
       {
          return new BatchOutputValues
          {
-            Path = column.PathAsString,
+            Path = consolidatedPathFor(column),
             //ComparisonThreshold should alwyas have a value. If for some reason it is not set, using 0 ensures that value should be compared exactly
             ComparisonThreshold = column.DataInfo.ComparisonThreshold.GetValueOrDefault(0),
             Values = displayValuesFor(column),
             Dimension = column.Dimension.Name
          };
+      }
+
+      private static string consolidatedPathFor(DataColumn column)
+      {
+         var originalPath = column.PathAsString;
+         var pathArray = originalPath.ToPathArray();
+
+         //One element at most, return the original path
+         if (pathArray.Length <= 1)
+            return originalPath;
+
+         //Remove first entry corresponding to simulation name
+         return pathArray.Skip(1).ToPathString();
       }
 
       private float[] displayValuesFor(DataColumn column)
