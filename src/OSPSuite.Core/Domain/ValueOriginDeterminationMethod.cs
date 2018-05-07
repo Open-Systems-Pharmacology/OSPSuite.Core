62c6383ddb1a1b6c645044d998105158163a4efd
Michael Sevestre <michael@design2code.ca>
2018-05-07 11:57:07 -0400

    Fixes #683 typi and #406 ordering

--- src/OSPSuite.Core/Domain/ValueOriginDeterminationMethod.cs
+++ src/OSPSuite.Core/Domain/ValueOriginDeterminationMethod.cs
@@ -19,15 +19,16 @@ namespace OSPSuite.Core.Domain
    public static class ValueOriginDeterminationMethods
    {
       private static readonly Cache<ValueOriginDeterminationMethodId, ValueOriginDeterminationMethod> _allValueOriginDeterminationMethods = new Cache<ValueOriginDeterminationMethodId, ValueOriginDeterminationMethod>(x => x.Id);
+
       public static IReadOnlyCollection<ValueOriginDeterminationMethod> All => _allValueOriginDeterminationMethods;
 
       public static ValueOriginDeterminationMethod Assumption = create(ValueOriginDeterminationMethodId.Assumption, Captions.ValueOrigins.Methods.Assumption, ApplicationIcons.ValueOriginMethodAssumption);
-      public static ValueOriginDeterminationMethod ManualFit = create(ValueOriginDeterminationMethodId.ManualFit, Captions.ValueOrigins.Methods.ManualFit, ApplicationIcons.ValueOriginMethodManualFit);
-      public static ValueOriginDeterminationMethod ParameterIdentification = create(ValueOriginDeterminationMethodId.ParameterIdentification, Captions.ValueOrigins.Methods.ParameterIdentification, ApplicationIcons.ValueOriginMethodParameterIdentification);
       public static ValueOriginDeterminationMethod InVitro = create(ValueOriginDeterminationMethodId.InVitro, Captions.ValueOrigins.Methods.InVitro, ApplicationIcons.ValueOriginMethodInVitro);
       public static ValueOriginDeterminationMethod InVivo = create(ValueOriginDeterminationMethodId.InVivo, Captions.ValueOrigins.Methods.InVivo, ApplicationIcons.ValueOriginMethodInVivo);
-      public static ValueOriginDeterminationMethod Unknown = create(ValueOriginDeterminationMethodId.Unknown, Captions.ValueOrigins.Unknown, ApplicationIcons.ValueOriginMethodUnknown);
+      public static ValueOriginDeterminationMethod ManualFit = create(ValueOriginDeterminationMethodId.ManualFit, Captions.ValueOrigins.Methods.ManualFit, ApplicationIcons.ValueOriginMethodManualFit);
+      public static ValueOriginDeterminationMethod ParameterIdentification = create(ValueOriginDeterminationMethodId.ParameterIdentification, Captions.ValueOrigins.Methods.ParameterIdentification, ApplicationIcons.ValueOriginMethodParameterIdentification);
       public static ValueOriginDeterminationMethod Other = create(ValueOriginDeterminationMethodId.Other, Captions.ValueOrigins.Other, ApplicationIcons.ValueOriginMethodOther);
+      public static ValueOriginDeterminationMethod Unknown = create(ValueOriginDeterminationMethodId.Unknown, Captions.ValueOrigins.Unknown, ApplicationIcons.ValueOriginMethodUnknown);
       public static ValueOriginDeterminationMethod Undefined = create(ValueOriginDeterminationMethodId.Undefined, Captions.ValueOrigins.Undefined, ApplicationIcons.EmptyIcon);
 
       public static ValueOriginDeterminationMethod ById(ValueOriginDeterminationMethodId valueOriginDeterminationMethodId)
