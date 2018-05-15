55b34b210065e2d11c38d5254533ffe77abc2620
Michael Sevestre <michael@design2code.ca>
2018-05-15 13:49:12 -0400

    Fixes #422 project conversion 7.3

--- src/OSPSuite.Core/Converter/v7_3/Converter710To730.cs
+++ src/OSPSuite.Core/Converter/v7_3/Converter710To730.cs
@@ -1,20 +1,30 @@
 ﻿using System.Linq;
 using System.Xml.Linq;
 using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
 using OSPSuite.Core.Serialization;
+using OSPSuite.Utility.Extensions;
+using OSPSuite.Utility.Visitor;
 
 namespace OSPSuite.Core.Converter.v7_3
 {
-   public class Converter710To730 : IObjectConverter
+   public class Converter710To730 : IObjectConverter,
+      IVisitor<IParameterStartValuesBuildingBlock>,
+      IVisitor<IEventGroupBuildingBlock>,
+      IVisitor<IMoleculeBuildingBlock>,
+      IVisitor<IReactionBuildingBlock>,
+      IVisitor<ISpatialStructure>,
+      IVisitor<ISimulation>
    {
-      public bool IsSatisfiedBy(int version)
-      {
-         return version == PKMLVersion.V7_1_0;
-      }
+      private bool _converted = false;
+
+      public bool IsSatisfiedBy(int version) => version == PKMLVersion.V7_1_0;
 
       public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate)
       {
-         return (PKMLVersion.V7_3_0, false);
+         _converted = false;
+         this.Visit(objectToUpdate);
+         return (PKMLVersion.V7_3_0, _converted);
       }
 
       public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element)
@@ -44,6 +54,52 @@ namespace OSPSuite.Core.Converter.v7_3
          var element = new XElement(Constants.Serialization.VALUE_ORIGIN);
          element.SetAttributeValue(Constants.Serialization.Attribute.DESCRIPTION, valueDescriptioon);
          return element;
+      }
+
+      public void Visit(ISimulation simulation)
+      {
+         convertAllParametersIn(simulation.Model.Root);
+      }
+
+      public void Visit(IParameterStartValuesBuildingBlock parameterStartValuesBuildingBlock)
+      {
+         parameterStartValuesBuildingBlock.Each(convertWithDefaultStateObjectToDefault);
+      }
+
+      public void Visit(IEventGroupBuildingBlock eventGroupBuildingBlock)
+      {
+         convertAllParametersIn(eventGroupBuildingBlock);
+      }
+
+      public void Visit(IMoleculeBuildingBlock moleculeBuildingBlock)
+      {
+         convertAllParametersIn(moleculeBuildingBlock);
+      }
+
+      public void Visit(IReactionBuildingBlock reactionBuildingBlock)
+      {
+         convertAllParametersIn(reactionBuildingBlock);
+      }
+
+      public void Visit(ISpatialStructure spatialStructure)
+      {
+         convertAllParametersIn(spatialStructure);
+      }
+
+      private void convertAllParametersIn<T>(IBuildingBlock<T> buildingBlock) where T : class, IContainer
+      {
+         buildingBlock.Each(convertAllParametersIn);
+      }
+
+      private void convertAllParametersIn(IContainer container)
+      {
+         container.GetAllChildren<IParameter>().Each(convertWithDefaultStateObjectToDefault);
+      }
+
+      private void convertWithDefaultStateObjectToDefault(IWithDefaultState withDefaultState)
+      {
+         withDefaultState.IsDefault = true;
+         _converted = true;
       }
    }
 }
