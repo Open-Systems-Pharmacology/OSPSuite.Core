0ed15f2f27542b8adcdb48dce4085a803b29f559
Michael Sevestre <michael@design2code.ca>
2019-02-05 14:09:40 -0500

    Fixes #483 move file logger from PKSim to Core (#484)
    * Fixes #483 move file logger from PKSim to Core
    
    * Fixes #483 move file logger from PKSim to Core

---
+++ src/OSPSuite.Infrastructure/Logging/OSPSuiteLogger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using ILogger = OSPSuite.Core.Services.ILogger;
+
+namespace OSPSuite.Infrastructure.Logging
+{
+   public class OSPSuiteLogger : ILogger
+   {
+      private readonly ILoggerFactory _loggerFactory;
+      private readonly string _defaultLoggerCategory;
+
+      public OSPSuiteLogger(ILoggerFactory loggerFactory, string defaultLoggerCategory = "OSPSuite")
+      {
+         _loggerFactory = loggerFactory;
+         _defaultLoggerCategory = defaultLoggerCategory;
+      }
+
+      public virtual void AddToLog(string message, LogLevel logLevel, string categoryName)
+      {
+         var logger = _loggerFactory.CreateLogger(string.IsNullOrEmpty(categoryName) ? _defaultLoggerCategory : categoryName);
+         switch (logLevel)
+         {
+            case LogLevel.Trace:
+               logger.LogTrace(message);
+               break;
+            case LogLevel.Debug:
+               logger.LogDebug(message);
+               break;
+            case LogLevel.Information:
+               logger.LogInformation(message);
+               break;
+            case LogLevel.Warning:
+               logger.LogWarning(message);
+               break;
+            case LogLevel.Error:
+               logger.LogError(message);
+               break;
+            case LogLevel.Critical:
+               logger.LogCritical(message);
+               break;
+         }
+      }
+   }
+}
