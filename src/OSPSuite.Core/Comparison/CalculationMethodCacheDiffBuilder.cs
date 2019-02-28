ec626ab87cc86f2390a9b5dddde83a696a2abd74
Michael Sevestre <michael@design2code.ca>
2019-02-28 14:27:18 -0500

    Fixes #499 add calculation method diff builder (#500)
    * Fixes #499 add calculation method diff builder
    
    * Add more test
    
    * Add more test
    
    * Add more test

---
+++ src/OSPSuite.Core/Comparison/CalculationMethodCacheDiffBuilder.cs
@@ -0,0 +1,20 @@
+﻿using OSPSuite.Assets;
+using OSPSuite.Core.Domain;
+
+namespace OSPSuite.Core.Comparison
+{
+   public class CalculationMethodCacheDiffBuilder : DiffBuilder<CalculationMethodCache>
+   {
+      private readonly EnumerableComparer _enumerableComparer;
+
+      public CalculationMethodCacheDiffBuilder(EnumerableComparer enumerableComparer)
+      {
+         _enumerableComparer = enumerableComparer;
+      }
+
+      public override void Compare(IComparison<CalculationMethodCache> comparison)
+      {
+         _enumerableComparer.CompareEnumerables(comparison, x => x, item => item.Category, missingItemType: Captions.CalculationMethod);
+      }
+   }
+}
