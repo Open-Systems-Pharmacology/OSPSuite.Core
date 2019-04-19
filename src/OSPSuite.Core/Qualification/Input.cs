3db2d1c33a48d82219581587406ce51ec0c20457
Michael Sevestre <michael@design2code.ca>
2019-04-19 16:44:28 -0400

    Fixes #545 add path to bb ref

--- src/OSPSuite.Core/Qualification/Input.cs
+++ src/OSPSuite.Core/Qualification/Input.cs
@@ -1,7 +1,12 @@
-﻿namespace OSPSuite.Core.Qualification
+﻿using OSPSuite.Core.Domain;
+
+namespace OSPSuite.Core.Qualification
 {
-   public class Input : BuildingBlockRef
+   public class Input : IWithName, IReferencingProject
    {
+      public PKSimBuildingBlockType Type { get; set; }
+      public string Name { get; set; }
+      public string Project { get; set; }
       public int SectionId { get; set; }
    }
 }
