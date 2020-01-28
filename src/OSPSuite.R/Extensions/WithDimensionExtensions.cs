444f21c73771a45d55dfe794c59b3a2ecc03f0fc
Michael Sevestre <michael@design2code.ca>
2020-01-28 14:46:57 -0500

    Fixes #707 add indexes unit (#708)

---
+++ src/OSPSuite.R/Extensions/WithDimensionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using OSPSuite.Core.Domain;
+
+namespace OSPSuite.R.Extensions
+{
+   /// <summary>
+   /// Required to support another logic for R since we have issue with unicode characters
+   /// See https://github.com/Open-Systems-Pharmacology/OSPSuite-R/issues/233
+   /// </summary>
+   public static class WithDimensionExtensions
+   {
+      /// <summary>
+      /// Converts the value given in the unit at index <paramref name="unitIndex"/> into base unit
+      /// </summary>
+      /// <param name="withDimension">Object with dimension used for conversion</param>
+      /// <param name="value">Value in unit defined at <paramref name="unitIndex"/></param>
+      /// <param name="unitIndex">1-based index of the unit in the units array</param>
+      /// <returns>The value converted in base unit</returns>
+      public static double ConvertToBaseUnit(this IWithDimension withDimension, double value, int unitIndex)
+      {
+         return withDimension.ConvertToBaseUnit(value, withDimension.Dimension.UnitAt(unitIndex - 1));
+      }
+
+
+      /// <summary>
+      /// Converts the value given in base unit to the unit at index  <paramref name="unitIndex"/> 
+      /// </summary>
+      /// <param name="withDimension">Object with dimension used for conversion</param>
+      /// <param name="valueInBaseUnit">Value in base unit to convert in unit defined at <paramref name="unitIndex"/></param>
+      /// <param name="unitIndex">1-based index of the unit in the units array</param>
+      /// <returns>The value converted in unit</returns>
+      public static double ConvertToUnit(this IWithDimension withDimension, double valueInBaseUnit, int unitIndex)
+      {
+         return withDimension.ConvertToUnit(valueInBaseUnit, withDimension.Dimension.UnitAt(unitIndex-1));
+      }
+   }
+}
---
+++ tests/OSPSuite.R.Tests/Extensions/WithDimensionExtensionsSpecs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.UnitSystem;
+using OSPSuite.Helpers;
+
+namespace OSPSuite.R.Extensions
+{
+   public abstract class concern_for_WithDimensionExtensions : StaticContextSpecification
+   {
+      protected IDimension _dimension;
+      protected Parameter _parameter;
+
+      protected override void Context()
+      {
+         //min = 1, h = 2
+         _dimension = DomainHelperForSpecs.TimeDimensionForSpecs();
+         _parameter = new Parameter().WithDimension(_dimension);
+      }
+   }
+
+   public class When_converting_a_value_to_base_unit_using_the_unit_index : concern_for_WithDimensionExtensions
+   {
+      [Observation]
+      public void should_set_the_expected_value()
+      {
+         _parameter.ConvertToBaseUnit(2, 2).ShouldBeEqualTo(120);
+      }
+
+      [Observation]
+      public void should_thrown_an_exception_if_the_index_is_out_of_range()
+      {
+         The.Action(() => _parameter.ConvertToBaseUnit(2, 3)).ShouldThrowAn<ArgumentOutOfRangeException>();
+         The.Action(() => _parameter.ConvertToBaseUnit(2, 0)).ShouldThrowAn<ArgumentOutOfRangeException>();
+         The.Action(() => _parameter.ConvertToBaseUnit(2, -1)).ShouldThrowAn<ArgumentOutOfRangeException>();
+      }
+   }
+
+
+   public class When_converting_a_value_to_unit_using_the_unit_index : concern_for_WithDimensionExtensions
+   {
+      [Observation]
+      public void should_set_the_expected_value()
+      {
+         _parameter.ConvertToUnit(120, 1).ShouldBeEqualTo(120);
+         _parameter.ConvertToUnit(120, 2).ShouldBeEqualTo(2);
+      }
+
+      [Observation]
+      public void should_thrown_an_exception_if_the_index_is_out_of_range()
+      {
+         The.Action(() => _parameter.ConvertToUnit(2, 3)).ShouldThrowAn<ArgumentOutOfRangeException>();
+         The.Action(() => _parameter.ConvertToUnit(2, 0)).ShouldThrowAn<ArgumentOutOfRangeException>();
+         The.Action(() => _parameter.ConvertToUnit(2, -1)).ShouldThrowAn<ArgumentOutOfRangeException>();
+      }
+   }
+}
