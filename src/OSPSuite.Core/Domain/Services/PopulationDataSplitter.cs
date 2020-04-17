6b5c79ca2233f1aafd099dd519c7b83596385cdd
Michael Sevestre <michael@design2code.ca>
2020-04-17 13:48:08 -0400

    Fixes #765 ensure that we can support NaN in population table (#766)
    * WIP #765 ensure that we can support NaN in population table
    
    * WIP #765 ensure that we can support NaN in population table
    
    * WIP #765 ensure that we can support NaN in population table

--- src/OSPSuite.Core/Domain/Services/PopulationDataSplitter.cs
+++ src/OSPSuite.Core/Domain/Services/PopulationDataSplitter.cs
@@ -15,7 +15,7 @@ namespace OSPSuite.Core.Domain.Services
       private readonly int _numberOfCores;
       private readonly int _numberOfSimulationsPerCore;
 
-      public PopulationDataSplitter(int numberOfCores, DataTable populationData, DataTable agingData = null, DataTable initialValues=null)
+      public PopulationDataSplitter(int numberOfCores, DataTable populationData, DataTable agingData = null, DataTable initialValues = null)
       {
          _populationData = populationData;
          _agingData = agingData ?? undefinedAgingData();
@@ -39,9 +39,10 @@ namespace OSPSuite.Core.Domain.Services
          return table;
       }
 
-      public void UpdateParametersAndInitialValuesForIndividual(int individualId, IReadOnlyList<ParameterProperties> parameterProperties, IReadOnlyList<SpeciesProperties> speciesProperties)
+      public void UpdateParametersAndSpeciesValuesForIndividual(int individualId, IReadOnlyList<ParameterProperties> parameterProperties,
+         IReadOnlyList<SpeciesProperties> speciesProperties, PathCache<IParameter> parameterCache)
       {
-         fillParameterAndInitialValuesFor(individualId, parameterProperties, speciesProperties);
+         fillParameterAndSpeciesInitialValuesFor(individualId, parameterProperties, speciesProperties, parameterCache);
       }
 
       public IReadOnlyList<int> GetIndividualIdsFor(int coreIndex)
@@ -104,28 +105,33 @@ namespace OSPSuite.Core.Domain.Services
          return dataTable.Rows.Cast<DataRow>().FirstOrDefault(r => individualIdFrom(r) == individualId);
       }
 
-      private void fillParameterAndInitialValuesFor(int individualId, IReadOnlyList<ParameterProperties> parameters, IReadOnlyList<SpeciesProperties> speciesProperties)
+      private void fillParameterAndSpeciesInitialValuesFor(int individualId, IReadOnlyList<ParameterProperties> parameters,
+         IReadOnlyList<SpeciesProperties> speciesProperties, PathCache<IParameter> parameterCache)
       {
          var nonTableParameterValues = parameterDataRowFromIndividualId(individualId);
 
-         foreach (var parameterProperties in parameters)
+         foreach (var parameter in parameters)
          {
             // first, assign all table points for the parameter from the aging data (if any)
-            fillTableParameterValuesFor(parameterProperties, individualId);
+            fillTableParameterValuesFor(parameter, individualId);
 
             // if no aging data found, parameter value is stored in the population data table
             // (single value)
-            if (!parameterProperties.TablePoints.Any())
-               parameterProperties.Value = nonTableParameterValues[parameterProperties.Path].ConvertedTo<double>();
+            if (parameter.TablePoints.Any())
+               continue;
+
+            var parameterValue = nonTableParameterValues[parameter.Path].ConvertedTo<double>();
+            //reset the parameter to its default value if the value in the table is NaN
+            parameter.Value = double.IsNaN(parameterValue) ? parameterCache[parameter.Path].Value : parameterValue;
          }
 
          var initialValues = initialValuesDataRowFromIndividualId(individualId);
          if (initialValues == null)
             return;
 
-         foreach (var initialValue in speciesProperties)
+         foreach (var species in speciesProperties)
          {
-            initialValue.InitialValue = initialValues[initialValue.Path].ConvertedTo<double>();
+            species.InitialValue = initialValues[species.Path].ConvertedTo<double>();
          }
       }
 
@@ -156,14 +162,14 @@ namespace OSPSuite.Core.Domain.Services
       /// </summary>
       public IReadOnlyList<string> ParameterPathsToBeVaried()
       {
-         var nonTableParametersToBeVaried = from DataColumn dc in _populationData.Columns
+         var nonTableParameterPathsToBeVaried = from DataColumn dc in _populationData.Columns
             where !dc.ColumnName.Equals(Constants.Population.INDIVIDUAL_ID_COLUMN)
             select dc.ColumnName;
 
-         var tableParametersToBeVaried = (from DataRow dr in _agingData.Rows
+         var tableParameterPathsToBeVaried = (from DataRow dr in _agingData.Rows
             select dr[Constants.Population.PARAMETER_PATH_COLUMN].ToString()).Distinct();
 
-         return nonTableParametersToBeVaried.Union(tableParametersToBeVaried).ToList();
+         return nonTableParameterPathsToBeVaried.Union(tableParameterPathsToBeVaried).ToList();
       }
 
       /// <summary>
@@ -171,11 +177,11 @@ namespace OSPSuite.Core.Domain.Services
       /// </summary>
       public IReadOnlyList<string> InitialValuesPathsToBeVaried()
       {
-         var speciesToBeVaried =  from DataColumn dc in _initialValues.Columns
+         var speciesPathsToBeVaried = from DataColumn dc in _initialValues.Columns
             where !dc.ColumnName.Equals(Constants.Population.INDIVIDUAL_ID_COLUMN)
             select dc.ColumnName;
 
-         return speciesToBeVaried.ToList();
+         return speciesPathsToBeVaried.ToList();
       }
    }
 }
