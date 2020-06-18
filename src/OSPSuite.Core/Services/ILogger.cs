808b0acc6d26b7878a6caa9073f88ec8e0d694c6
Abdel Rodríguez <abdelr1982@gmail.com>
2020-06-18 12:05:14 +0200

    Removing duplicates

--- src/OSPSuite.Core/Services/ILogger.cs
+++ src/OSPSuite.Core/Services/ILogger.cs
@@ -10,17 +10,5 @@ namespace OSPSuite.Core.Services
       ///    <paramref name="categoryName" />
       /// </summary>
       void AddToLog(string message, LogLevel logLevel, string categoryName);
-
-      /// <summary>
-      /// Adds a new configuration to the loggingBuilder
-      /// </summary>
-      /// <param name="configuration">New configuration</param>
-      void AddLoggingBuilderConfiguration(Func<ILoggingBuilder, ILoggingBuilder> configuration);
-
-      /// <summary>
-      /// Adds a loggerProvider
-      /// </summary>
-      /// <param name="loggerprovider"></param>
-      void AddLoggerProvider(ILoggerProvider loggerprovider);
    }
 }
