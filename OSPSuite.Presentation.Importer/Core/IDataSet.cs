27656e402f6856dc90e7d4195102ac9fc2286aea
Abdel Rodríguez <abdelr1982@gmail.com>
2020-07-09 13:59:16 +0200

    Some basic interfaces

---
+++ OSPSuite.Presentation.Importer/Core/IDataSet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace OSPSuite.Presentation.Importer.Core
+{
+   /// <summary>
+   /// Data from a single experiment
+   /// </summary>
+   public interface IDataSet
+   {
+      Dictionary<IColumn, IList<double>> Data { get; set; }
+   }
+
+   public class DataSet : IDataSet
+   {
+      public Dictionary<IColumn, IList<double>> Data { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+   }
+}
---
+++ OSPSuite.Presentation.Importer/Core/IDataTable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace OSPSuite.Presentation.Importer.Core
+{
+   public interface IColumn
+   {
+      string Name { get; set; }
+      string Unit { get; set; }
+   }
+
+   public class Column : IColumn
+   {
+      public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+      public string Unit { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+   }
+
+   /// <summary>
+   /// e.g. a sheet in an excel file
+   /// </summary>
+   public interface IDataTable
+   {
+      Dictionary<string, IList<string>> RawData { get; set; }
+      IFormat Format { get; set; }
+   }
+
+   public class DataTable : IDataTable
+   {
+      public Dictionary<string, IList<string>> RawData { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+      public IFormat Format { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+   }
+}
