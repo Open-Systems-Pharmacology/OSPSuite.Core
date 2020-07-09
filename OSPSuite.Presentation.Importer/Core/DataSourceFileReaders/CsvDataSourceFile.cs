c940e0fa30c030e66f32b19da946266437b3791a
Abdel Rodríguez <abdelr1982@gmail.com>
2020-07-09 14:18:15 +0200

    Create CsvDataSourceFile.cs

---
+++ OSPSuite.Presentation.Importer/Core/DataSourceFileReaders/CsvDataSourceFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPSuite.Presentation.Importer.Core.Readers
+{
+   public class CsvDataSourceFile : DataSourceFile
+   {
+      public CsvDataSourceFile(string path) : base(path) { }
+
+      override protected Dictionary<string, IDataTable> LoadFromFile(string path)
+      {
+         throw new System.NotImplementedException();
+      }
+   }
+}
