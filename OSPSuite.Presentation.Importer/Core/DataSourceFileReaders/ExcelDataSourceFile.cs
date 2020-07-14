e88cedeef478edf352a83c46d579e674acdc2d9f
georgeDaskalakis <geodas13@gmail.com>
2020-07-14 15:50:25 +0200

    IUnformatedData created
    CURRENT STATE DOES NOT BUILD

--- OSPSuite.Presentation.Importer/Core/DataSourceFileReaders/ExcelDataSourceFile.cs
+++ OSPSuite.Presentation.Importer/Core/DataSourceFileReaders/ExcelDataSourceFile.cs
@@ -25,10 +25,20 @@ namespace OSPSuite.Presentation.Importer.Core.DataSourceFileReaders
             {
                ISheet sheet = book.GetSheetAt(i);
                var sheetName = sheet.SheetName;
+               //sheet.GetRow(i);
 
                var tableStart = reader.DetermineFirstColumn(sheet);
                var headers = reader.GetExcelRowAsListOfStrings(sheet, tableStart);
                var rows = reader.ReaDataTable(sheet, tableStart, headers.Count);
+
+               /*
+               var rows = new List<List<string>>(headers.Count);
+               while (csv.ReadNextRecord())
+               {
+                  for (var i = 0; i < headers.Length; i++)
+                     rows[i].Add(csv[i]);
+               }
+*/
 
                IDataSheet dataSheet = new DataSheet(); //DataSheet also exists (under Microsoft.Graph.Application - but is by far less common).
                                                       //if we really want to we could even do it OSPSuiteDataSheet
---
+++ OSPSuite.Presentation.Importer/Core/IUnformatedData.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OSPSuite.Presentation.Importer.Core
+{
+   public interface IUnformatedData
+   {
+      //UnformatedData
+      IList<string> GetRow(int index);
+      IList<string> GetColumn(int index);
+      IList<string> GetColumn(string columnName);
+
+      IList<IList<string>> GetRows(Func<IList<string>, bool> filter);
+
+
+      void AddRow(IList<string> row);
+      void AddColumn(IList<string> row, string columnName);
+
+   }
+
+   public class UnformatedData : IUnformatedData
+   {
+      private List<List<string>> rawData;
+
+      //private Dictionary<string, ColumnDescription> headers; //we have to ensure headers and RawData sizes match
+      //unparsed name   //datatype, List<strings> values, 
+
+      public void AddColumn(IList<string> row, string columnName)
+      {
+         throw new NotImplementedException();
+      }
+
+      public void AddRow(IList<string> row)
+      {
+         //if (headers.Count != 0) //do not add Data to a DataSheet without column names
+            
+
+         throw new NotImplementedException();
+      }
+
+      public IList<string> GetColumn(int index)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IList<string> GetColumn(string columnName)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IList<string> GetRow(int index)
+      {
+         throw new NotImplementedException();
+      }
+
+      public IList<IList<string>> GetRows(Func<IList<string>, bool> filter)
+      {
+         throw new NotImplementedException();
+      }
+   }
+}
