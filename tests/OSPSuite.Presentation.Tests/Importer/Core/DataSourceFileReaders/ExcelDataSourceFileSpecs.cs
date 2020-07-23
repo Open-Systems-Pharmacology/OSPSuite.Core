d3bdcef5d66b4b3e9e54831a8646365ee8dbcd21
georgeDaskalakis <geodas13@gmail.com>
2020-07-23 12:12:25 +0200

    IList to IEnumerable initial commit

--- tests/OSPSuite.Presentation.Tests/Importer/Core/DataSourceFileReaders/ExcelDataSourceFileSpecs.cs
+++ tests/OSPSuite.Presentation.Tests/Importer/Core/DataSourceFileReaders/ExcelDataSourceFileSpecs.cs
@@ -61,8 +61,8 @@ namespace OSPSuite.Presentation.Importer.Core.DataSourceFileReaders
       [TestCase]
       public void body_is_read_first_sheet()
       {
-         sut.DataSheets.ElementAt(0).Value.RawData.GetRow(0).Count.ShouldBeEqualTo(3);
-         sut.DataSheets.ElementAt(0).Value.RawData.GetColumn("header1").Count.ShouldBeEqualTo(3);
+         sut.DataSheets.ElementAt(0).Value.RawData.GetRow(0).Count().ShouldBeEqualTo(3);
+         sut.DataSheets.ElementAt(0).Value.RawData.GetColumn("header1").Count().ShouldBeEqualTo(3);
          for (var i = 0; i < 3; i++)
          {
             sut.DataSheets.ElementAt(0).Value.RawData.GetColumn(i).ShouldBeEqualTo(new[] { "str" + (i + 1), "str" + (i + 4), "str" + (i + 7) });
@@ -72,8 +72,8 @@ namespace OSPSuite.Presentation.Importer.Core.DataSourceFileReaders
       [TestCase]
       public void body_is_read_second_sheet()
       {
-         sut.DataSheets.ElementAt(1).Value.RawData.GetRow(0).Count.ShouldBeEqualTo(3);
-         sut.DataSheets.ElementAt(1).Value.RawData.GetColumn("sheet2_header2").Count.ShouldBeEqualTo(2);
+         sut.DataSheets.ElementAt(1).Value.RawData.GetRow(0).Count().ShouldBeEqualTo(3);
+         sut.DataSheets.ElementAt(1).Value.RawData.GetColumn("sheet2_header2").Count().ShouldBeEqualTo(2);
          for (var i = 0; i < 3; i++)
          {
             sut.DataSheets.ElementAt(1).Value.RawData.GetColumn(i).ShouldBeEqualTo(new[] { "str" + (i + 7), "str" + (i + 10) });
