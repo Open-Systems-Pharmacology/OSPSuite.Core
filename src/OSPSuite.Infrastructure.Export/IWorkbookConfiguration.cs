0c44608c01af62d39c6c8ac884b4ecb13b036b97
georgeDaskalakis <geodas13@gmail.com>
2020-08-06 16:37:55 +0200

    workbookConfiguration introduced

--- src/OSPSuite.Infrastructure.Export/IWorkbookConfiguration.cs
+++ src/OSPSuite.Infrastructure.Export/IWorkbookConfiguration.cs
@@ -3,26 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 
 namespace OSPSuite.Infrastructure.Export
 {
    public interface IWorkbookConfiguration
    {
+       XSSFWorkbook WorkBook { get; set; }
+       ICellStyle HeadersStyle { get; set; }
+       ICellStyle BodyStyle { get; set; }
+
+
       void SetHeadersBold();
    }
 
    public class WorkbookConfiguration : IWorkbookConfiguration
    {
-      private XSSFWorkbook _workBook;
+      public XSSFWorkbook WorkBook { get; set; }
+      public ICellStyle HeadersStyle {get; set;}
+      public ICellStyle BodyStyle {get; set;}
+
+      private IFont getDefaultFont( )
+      {
+         var font = WorkBook.CreateFont();
+         font.FontHeightInPoints = 11;
+         font.FontName = "Times New Roman";
+         font.IsBold = false;
+
+         return font;
+      }
       public WorkbookConfiguration()
       {
-         _workBook = new XSSFWorkbook();
+         WorkBook = new XSSFWorkbook();
+
+         var font = getDefaultFont();
+
+         HeadersStyle = WorkBook.CreateCellStyle();
+         HeadersStyle.SetFont(font);
+
+         BodyStyle = WorkBook.CreateCellStyle();
+         BodyStyle.SetFont(font);
       }
 
       public void SetHeadersBold()
       {
-         throw new NotImplementedException();
+         var font = WorkBook.CreateFont();
+         font.FontHeightInPoints = 11;
+         font.FontName = "Arial";
+         font.IsBold = true;
+
+         HeadersStyle = WorkBook.CreateCellStyle();
+         HeadersStyle.SetFont(font);
       }
    }
 }
