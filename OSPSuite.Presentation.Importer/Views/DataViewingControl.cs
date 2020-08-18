535dba3aead75378e9c4dfda10483b22dfbeacfc
georgeDaskalakis <geodas13@gmail.com>
2020-08-18 23:46:17 +0200

    hard coded first sheet name fixed

--- OSPSuite.Presentation.Importer/Views/DataViewingControl.cs
+++ OSPSuite.Presentation.Importer/Views/DataViewingControl.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data;
+using System.Linq;
 using OSPSuite.UI.Controls;
 using OSPSuite.Presentation.Importer.Presenters;
 using OSPSuite.Presentation.Importer.Core.DataSourceFileReaders;
@@ -18,29 +19,16 @@ namespace OSPSuite.Presentation.Importer.Views
       {
          base.InitializeResources();
 
-         /*
-          *         //     tempDataSourceFile.Path = "C:/Users/GeorgiosDaskalakis/Documents/GitHub/OSPSuite.Core/tests/OSPSuite.Presentation.Tests/Data/sample1.xlsx";
-         // tempDataSourceFile.Path = "C:/Users/GeorgiosDaskalakis/Downloads/Midazolam.xlsx";
-         tempDataSourceFile.Path = "C:/Users/GeorgiosDaskalakis/Downloads/Book1 (1).xlsx";
-         DataTable temp = tempDataSourceFile.DataSheets["Sheet1"].RawData.AsDataTable();
-         gridControl1.DataSource = temp;
-         // gridView1.DataSource =temp();
-
-          *
-          */
-
          var tempDataSourceFile = new ExcelDataSourceFile(null);
 
-         //tempDataSourceFile.Path = "C:/Users/GeorgiosDaskalakis/Documents/GitHub/OSPSuite.Core/tests/OSPSuite.Presentation.Tests/Data/sample1.xlsx";
-         //tempDataSourceFile.Path = "C:/Users/GeorgiosDaskalakis/Downloads/Midazolam.xlsx";  iv, 0.15 mgkg(Heizmann 1983)
          var file = new OpenFileDialog();
          if (file.ShowDialog() == DialogResult.OK)
          {
             tempDataSourceFile.Path = file.FileName;
          }
 
-         //tempDataSourceFile.Path = "C:/Users/GeorgiosDaskalakis/Downloads/Book1 (1).xlsx";
-         DataTable temp = tempDataSourceFile.DataSheets["Sheet1"].RawData.AsDataTable();
+         var keys = tempDataSourceFile.DataSheets.Keys;
+         DataTable temp = tempDataSourceFile.DataSheets[keys.ElementAt(0)].RawData.AsDataTable();
          gridControl1.DataSource = temp;
          // gridView1.DataSource =temp();
       }
