dec399578c741d14c3c76845e2dc6391f7025438
georgeDaskalakis <geodas13@gmail.com>
2020-08-19 11:19:52 +0200

    first implementation of importer presenter wrapper

---
+++ OSPSuite.Presentation.Importer/Presenters/ImporterPresenter.cs
@@ -0,0 +1,26 @@
+﻿using OSPSuite.Presentation.Importer.Views;
+using OSPSuite.Presentation.Presenters;
+
+namespace OSPSuite.Presentation.Importer.Presenters
+{
+   //internal - because of the accessibility of AbstractCommandCollectorPresenter
+   internal class ImporterPresenter : AbstractCommandCollectorPresenter<IImporterPresenterView, IImporterPresenter>, IImporterPresenter
+   {
+      private readonly IImporterPresenterView _importerPresenterView;
+      public IDataViewingPresenter DataViewingPresenter { get; }
+      public IColumnMappingPresenter ColumnMappingPresenter { get; }
+
+
+      public ImporterPresenter(IImporterPresenterView view, IDataViewingPresenter dataViewingPresenter, IColumnMappingPresenter columnMappingPresenter ) : base(view)
+      {
+         _importerPresenterView = view;
+         _view.AddDataViewingControl(dataViewingPresenter.View);
+         _view.AddColumnMappingControl(columnMappingPresenter.View);
+
+         DataViewingPresenter = dataViewingPresenter;
+         ColumnMappingPresenter = columnMappingPresenter;
+
+         AddSubPresenters(DataViewingPresenter, ColumnMappingPresenter);
+      }
+   }
+}
---
+++ OSPSuite.Presentation.Importer/Views/IImporterPresenterView.cs
@@ -0,0 +1,13 @@
+﻿using OSPSuite.Presentation.Importer.Presenters;
+using OSPSuite.Presentation.Views;
+
+namespace OSPSuite.Presentation.Importer.Views
+{
+   interface IImporterPresenterView : IView<IImporterPresenter>
+   {
+      void AddDataViewingControl(IDataViewingControl dataViewingControl);
+      void AddColumnMappingControl(IColumnMappingControl columnMappingControl);
+   }
+
+  
+}
