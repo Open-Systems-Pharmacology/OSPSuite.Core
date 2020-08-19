ee05eca10ddeed60223e4cc117bf5564c117051f
georgeDaskalakis <geodas13@gmail.com>
2020-08-19 11:19:52 +0200

    first implementation of importer presenter wrapper

---
+++ OSPSuite.Presentation.Importer/Presenters/IImporterPresenter.cs
@@ -0,0 +1,12 @@
+﻿using OSPSuite.Presentation.Importer.Views;
+using OSPSuite.Presentation.Presenters;
+
+namespace OSPSuite.Presentation.Importer.Presenters
+{
+   interface IImporterPresenter : IPresenter<IImporterPresenterView>, ICommandCollectorPresenter
+   {
+      IDataViewingPresenter DataViewingPresenter { get; }
+      IColumnMappingPresenter ColumnMappingPresenter { get; }
+
+   }
+}
