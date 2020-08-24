d24261253f1b1d0db104b37dcd08c160f01c44af
Michael Sevestre <michael@design2code.ca>
2020-08-24 17:17:38 -0400

    Possible fix for refresh issue plus suggestion for more optimistic code

--- OSPSuite.Presentation.Importer/Presenters/ColumnMappingPresenter.cs
+++ OSPSuite.Presentation.Importer/Presenters/ColumnMappingPresenter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using DevExpress.DataProcessing;
 using OSPSuite.Assets;
 using OSPSuite.Core.Importer;
 using OSPSuite.Presentation.Core;
