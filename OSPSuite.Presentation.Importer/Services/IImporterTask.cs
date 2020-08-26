8a7160fe8060e8cdea4c1d5838aa237e9b711b33
Abdel Rodríguez <abdelr1982@gmail.com>
2020-08-26 11:05:56 +0200

    Removing unnecessary events

--- OSPSuite.Presentation.Importer/Services/IImporterTask.cs
+++ OSPSuite.Presentation.Importer/Services/IImporterTask.cs
@@ -21,13 +21,7 @@ namespace OSPSuite.Presentation.Importer.Services
          switch (parameter)
          {
             case MetaDataFormatParameter mp:
-               if (mappings == null)
-                  return ApplicationIcons.IconIndex(ApplicationIcons.MetaData);
-               return ApplicationIcons.IconIndex(
-                  mappings
-                     .Any(m => (m is MetaDataFormatParameter) && (m as MetaDataFormatParameter).MetaDataId == mp.MetaDataId)
-                     ? ApplicationIcons.MetaData
-                     : ApplicationIcons.MissingMetaData);
+               return ApplicationIcons.IconIndex(ApplicationIcons.MetaData);
             case MappingDataFormatParameter mp:
                if (mappings == null)
                   return ApplicationIcons.IconIndex(ApplicationIcons.UnitInformation);
@@ -52,11 +46,11 @@ namespace OSPSuite.Presentation.Importer.Services
             if
             (
                mappings
+                  .OfType<MappingDataFormatParameter>()
                   .Where
                   (
                      cm =>
-                     cm is MappingDataFormatParameter &&
-                     (cm as MappingDataFormatParameter).MappedColumn.Name.ToString() == col.Name
+                     cm.MappedColumn.Name.ToString() == col.Name
                   ).FirstOrDefault() != null
             )
                return col.Name;
