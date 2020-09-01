7949870c9e4951779285868d13fe711c3973e201
Abdel Rodríguez <abdelr1982@gmail.com>
2020-09-01 15:36:32 +0200

    Initial IImportConfirmation component

---
+++ OSPSuite.Presentation.Importer/Presenters/IImportConfirmationPresenter.cs
@@ -0,0 +1,15 @@
+﻿using OSPSuite.Presentation.Importer.Core;
+using OSPSuite.Presentation.Presenters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPSuite.Presentation.Importer.Presenters
+{
+   public interface IImportConfirmationPresenter : IDisposablePresenter
+   {
+      void Show(IDataSource dataSource);
+   }
+}
