9a5c555a12eae5685e81012ad84f85f75d854be5
Abdel Rodríguez <abdelr1982@gmail.com>
2020-09-30 14:53:23 +0200

    adding an exception on invalid excel file

---
+++ OSPSuite.Presentation.Importer/Core/InvalidFileException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPSuite.Presentation.Importer.Core
+{
+   public class InvalidFileException : Exception
+   {
+      private const string MESSAGE = "An error occurred while reading the file. Please check the content";
+
+      public InvalidFileException() : base(MESSAGE)
+      {
+      }
+   }
+}
