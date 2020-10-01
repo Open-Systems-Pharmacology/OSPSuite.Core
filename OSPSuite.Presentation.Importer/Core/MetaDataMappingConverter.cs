6916b6258678672e9079c4aa817c2c84f5df2dec
Abdel Rodríguez <abdelr1982@gmail.com>
2020-10-01 20:56:07 +0200

    Adding some comments on the code
    Still not very readable. It will be better once the Parse function is refactored

--- OSPSuite.Presentation.Importer/Core/MetaDataMappingConverter.cs
+++ OSPSuite.Presentation.Importer/Core/MetaDataMappingConverter.cs
@@ -2,6 +2,10 @@
 
 namespace OSPSuite.Presentation.Importer.Core
 {
+   /**
+    * Helper class to keep track of the id of the data stored
+    * and the column index it refers to
+    */
    public class MetaDataMappingConverter
    {
       public string Id { get; set; }
