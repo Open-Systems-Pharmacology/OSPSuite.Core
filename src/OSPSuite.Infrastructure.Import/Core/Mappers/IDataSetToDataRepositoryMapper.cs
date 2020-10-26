bb3c6e3f1712398e24dce6a68e5baab07d4bc7d0
georgeDaskalakis <geodas13@gmail.com>
2020-10-26 13:37:22 +0100

    temp importer mapper +  extendedColumn added

--- src/OSPSuite.Infrastructure.Import/Core/Mappers/IDataSetToDataRepositoryMapper.cs
+++ src/OSPSuite.Infrastructure.Import/Core/Mappers/IDataSetToDataRepositoryMapper.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.Data;
+using OSPSuite.Core.Domain.UnitSystem;
 using OSPSuite.Infrastructure.Import.Core.DataFormat;
 
 namespace OSPSuite.Infrastructure.Import.Core.Mappers
@@ -15,6 +16,12 @@ namespace OSPSuite.Infrastructure.Import.Core.Mappers
 
    class DataSetToDataRepositoryMapper : IDataSetToDataRepositoryMapper
    {
+      private readonly IDimensionFactory _dimensionFactory; //TO BE INITIALIZED IN THE CONSTRUCTOR
+
+      public DataSetToDataRepositoryMapper(IDimensionFactory dimensionFactory)
+      {
+         _dimensionFactory = dimensionFactory;
+      }
       public DataRepository ConvertImportDataSet(IDataSource dataSource, int dataSetIndex, string dataSetName)
       {
          //var dataSet = dataSource.DataSets.ElementAt(dataSetIndex);
@@ -26,16 +33,22 @@ namespace OSPSuite.Infrastructure.Import.Core.Mappers
          var dataRepository = new DataRepository { Name = dataSetName };
          var configuration = dataSource.GetImporterConfiguration();
 
+        // configuration.Format.Parameters
+
 
          //var colInfos = columnInfos as IList<ColumnInfo> ?? columnInfos.ToList();
 
          //do we actually need to sort our IDataSet somehow?
          //importDataTable.DefaultView.Sort = getSortString(colInfos);
 
-         addExtendedPropertyForSource(configuration.FileName, sheetName, dataRepository);
+         //addExtendedPropertyForSource(configuration.FileName, sheetName, dataRepository);
 
-         addExtendedPropertiesForMetaData(configuration.Format, dataSet, dataRepository);
+         //addExtendedPropertiesForMetaData(configuration.Format, dataSet, dataRepository);
 
+         foreach (var column in ParsedDataSet.Data)
+         {
+            convertParsedDataColumn(dataRepository, column, configuration.FileName);
+         }
 
          /*
 
@@ -46,7 +59,7 @@ namespace OSPSuite.Infrastructure.Import.Core.Mappers
                   foreach (ImportDataColumn importDataColumn in importDataTable.Columns)
                   {
                      bool columnErrorNaN;
-                     convertImportDataColumn(dataRepository, importDataColumn, colInfos, out columnErrorNaN);
+                     convertParsedDataColumn(dataRepository, importDataColumn, colInfos, out columnErrorNaN);
                      errorNaN |= columnErrorNaN;
                   }
 
@@ -54,6 +67,48 @@ namespace OSPSuite.Infrastructure.Import.Core.Mappers
                   associateColumns(colInfos, dataRepository);
          */
          return dataRepository;
+      }
+
+      private void convertParsedDataColumn(DataRepository dataRepository, KeyValuePair<ExtendedColumn, IList<SimulationPoint>> column, string fileName)
+      {
+         DataColumn dataColumn;
+
+
+         //here use colInfo.Name instead!!!
+         var dimension = _dimensionFactory.DimensionForUnit(column.Key.Column.Unit.SelectedUnit);
+
+         //var baseGrid = findColumnByName(dataRepository.Columns, colInfo.BaseGridName) as BaseGrid;
+         //dataColumn = new DataColumn(column.Key.Unit.ColumnName, dimension, baseGrid);
+
+         //maybe colInfo.Name...!!!!
+         dataColumn = new BaseGrid(column.Key.Column.Name, dimension); //it is not good that we have column 
+
+         var dataInfo = new DataInfo(ColumnOrigins.Undefined);
+         dataColumn.DataInfo = dataInfo;
+
+         //this should actually be the name of the file or the sheet
+         if (!string.IsNullOrEmpty(fileName))
+            dataInfo.Source = fileName;
+
+         
+         //not 100% sure this is correct
+         var unit = dimension.Unit(column.Key.Column.Unit.SelectedUnit);
+         var values = new float[column.Value.Count];
+         var i = 0;
+         //loop over view rows to get the sorted values.
+         foreach (var value in column.Value)
+         {
+            if (value == null) //but we actually should not be allowing this at all right?
+               values[i++] = float.NaN;
+            else
+               values[i++] = (float)dataColumn.Dimension.UnitValueToBaseUnitValue(unit, (double)value.Value);
+         }
+
+         dataInfo.DisplayUnitName = unit.Name; //or column.Key.Unit.Name?
+         dataColumn.Values = values;
+
+         //meta data information and input parameters currently not handled
+         dataRepository.Add(dataColumn);
       }
 
       private static void addExtendedPropertyForSource(string fileName, string sheetName,  DataRepository dataRepository)
