ec39fa1c883d292584457cf8d6ec1dc24c42111b
Abdel Rodríguez <abdelr1982@gmail.com>
2020-10-27 16:35:01 +0100

    Showing confirmation dialog on opening a new file when already imported data

---
+++ src/OSPSuite.Presentation/Core/ConfirmationDialog.cs
@@ -0,0 +1,27 @@
+﻿using OSPSuite.Presentation.Presenters;
+
+namespace OSPSuite.Presentation.Core
+{
+   public interface IConfirmationDialog
+   {
+      bool Confirm(string message);
+   }
+
+   public class ConfirmationDialog : IConfirmationDialog
+   {
+      private readonly IApplicationController _applicationController;
+      public ConfirmationDialog(IApplicationController applicationController)
+      {
+         _applicationController = applicationController;
+      }
+
+      public bool Confirm(string message)
+      {
+         using (var dialog = _applicationController.Start<IConfirmationDialogPresenter>())
+         {
+            dialog.ShowMessage(message);
+            return !dialog.Canceled;
+         }
+      }
+   }
+}
