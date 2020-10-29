56cab61650bc8c2ae3f7cf88207993f745b75a76
Abdel Rodríguez <abdelr1982@gmail.com>
2020-10-29 10:55:36 +0100

    removing unused classes

--- src/OSPSuite.Presentation/Views/IConfirmationDialogView.cs
+++
@@ -1,9 +0,0 @@
-﻿using OSPSuite.Presentation.Presenters;
-
-namespace OSPSuite.Presentation.Views
-{
-   public interface IConfirmationDialogView : IModalView<IConfirmationDialogPresenter>
-   {
-      string Message { set; }
-   }
-}
