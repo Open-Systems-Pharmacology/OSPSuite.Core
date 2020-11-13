09745a5d3e644b76d281d9d8825de4badbc5914a
georgeDaskalakis <geodas13@gmail.com>
2020-11-13 22:53:35 +0100

    _dataSource moved from confirmationPresenter to parent

--- src/OSPSuite.Presentation/Presenters/Importer/IImportConfirmationPresenter.cs
+++ src/OSPSuite.Presentation/Presenters/Importer/IImportConfirmationPresenter.cs
@@ -1,23 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using OSPSuite.Infrastructure.Import.Core;
+using OSPSuite.Core.Domain.Data;
 using OSPSuite.Presentation.Views.Importer;
 
 namespace OSPSuite.Presentation.Presenters.Importer
 {
-   public class ImportDataEventArgs : EventArgs
+   public class DataSetSelectedEventArgs : EventArgs
    {
-      public IDataSource DataSource { get; set; }
+      public string Key { get; set; }
+      public int Index { get; set; }
+   }
+
+   public class NamingConventionChangedEventArgs : EventArgs
+   {
+      public string NamingConvention { get; set; }
    }
    public interface IImportConfirmationPresenter : IPresenter<IImportConfirmationView>
    {
       void TriggerNamingConventionChanged(string namingConvention);
-      void SetDataSource(IDataSource dataSource);
+      void SetKeys(IEnumerable<string> keys);
       void SetNamingConventions(IEnumerable<string> namingConventions);
       void ImportData();
       void Refresh();
-      void DataSetToDataRepository(string key, int index); 
+      void DataSetSelected(string key, int index);
+      void PlotDataRepository(DataRepository dataRepository);
+      void SetDataSetNames(IEnumerable<string> names);
 
-      event EventHandler<ImportDataEventArgs> OnImportData;
+      event EventHandler<EventArgs> OnImportData;
+
+      event EventHandler<DataSetSelectedEventArgs> OnDataSetSelected;
+
+      event EventHandler<NamingConventionChangedEventArgs> OnNamingConventionChanged;
+      
    }
 }
--- src/OSPSuite.Presentation/Presenters/Importer/ImportConfirmationPresenter.cs
+++ src/OSPSuite.Presentation/Presenters/Importer/ImportConfirmationPresenter.cs
@@ -2,69 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using OSPSuite.Infrastructure.Import.Core;
-using OSPSuite.Infrastructure.Import.Services;
 using OSPSuite.Presentation.Views.Importer;
-using OSPSuite.Core.Domain;
-using OSPSuite.Infrastructure.Import.Core.Mappers;
-using OSPSuite.Presentation.Presenters.Charts;
+using OSPSuite.Core.Domain.Data;
 using OSPSuite.Presentation.Presenters.ObservedData;
 
 namespace OSPSuite.Presentation.Presenters.Importer
 {
    public class ImportConfirmationPresenter : AbstractPresenter<IImportConfirmationView, IImportConfirmationPresenter>, IImportConfirmationPresenter
    {
-      private IImporter _importer;
-      private IDataSource _dataSource;
-      private readonly IDataSetToDataRepositoryMapper _dataRepositoryMapper;
       private readonly IDataRepositoryChartPresenter _chartPresenter;
       private readonly IDataRepositoryDataPresenter _dataPresenter;
       private string _lastNamingPattern = "";
 
-      //maybe better seperate concerns here
-      public void DataSetToDataRepository(string key, int index)
-      {
-         var dataRepository = _dataRepositoryMapper.ConvertImportDataSet(_dataSource, index, key);
-         //View.ShowSelectedDataSet(dataRepository);
-         _chartPresenter.EditObservedData(dataRepository);
-         _dataPresenter.EditObservedData(dataRepository);
-      }
-
-      public event EventHandler<ImportDataEventArgs> OnImportData = delegate { };
-
-      public ImportConfirmationPresenter(IImportConfirmationView view, IImporter importer, IDataSetToDataRepositoryMapper dataRepositoryMapper,
+      public ImportConfirmationPresenter(IImportConfirmationView view,
          IDataRepositoryChartPresenter chartPresenter, IDataRepositoryDataPresenter dataPresenter) : base(view)
       {
-         _importer = importer;
-         _dataSource = new DataSource(_importer); //we re just initializing to empty...
-         _dataRepositoryMapper = dataRepositoryMapper;
          _chartPresenter = chartPresenter;
          _dataPresenter = dataPresenter;
          View.AddChartView(_chartPresenter.BaseView);
          View.AddDataView(_dataPresenter.View);
          _dataPresenter.DisableEdition();
       }
+      public void DataSetSelected(string key, int index)
+      {
+         OnDataSetSelected.Invoke(this, new DataSetSelectedEventArgs { Key = key, Index = index });
+      }
+
+      public void PlotDataRepository(DataRepository dataRepository)
+      {
+         _chartPresenter.EditObservedData(dataRepository);
+         _dataPresenter.EditObservedData(dataRepository);
+      }
 
       public void TriggerNamingConventionChanged(string namingConvention)
       {
          _lastNamingPattern = namingConvention;
-         setNames(namingConvention);
+         OnNamingConventionChanged.Invoke(this, new NamingConventionChangedEventArgs {NamingConvention = namingConvention});
       }
 
       public void Refresh()
       {
          if (!string.IsNullOrEmpty(_lastNamingPattern))
-            setNames(_lastNamingPattern);
+            OnNamingConventionChanged.Invoke(this, new NamingConventionChangedEventArgs { NamingConvention = _lastNamingPattern });
       }
 
-      public void SetDataSource(IDataSource dataSource)
+      public void SetKeys(IEnumerable<string> keys)
       {
-         _dataSource = dataSource;
-         var keys = new List<string>()
-         {
-            Constants.FILE,
-            Constants.SHEET
-         };
-         keys.AddRange(_dataSource.GetMappings().Select(m => m.Id));
          View.SetNamingConventionKeys(keys);
       }
 
@@ -80,18 +63,23 @@ namespace OSPSuite.Presentation.Presenters.Importer
 
          _view.SetNamingConventions(conventions);
          _lastNamingPattern = conventions.First();
-         setNames(_lastNamingPattern);
+         OnNamingConventionChanged.Invoke(this, new NamingConventionChangedEventArgs { NamingConvention = _lastNamingPattern });
       }
 
       public void ImportData()
       {
-         OnImportData.Invoke(this, new ImportDataEventArgs { DataSource = _dataSource });
+         OnImportData.Invoke(this, new EventArgs());
       }
 
-      private void setNames(string namingConvention)
+      public void SetDataSetNames(IEnumerable<string> names)
       {
-         _dataSource.SetNamingConvention(namingConvention);
-         _view.SetDataSetNames(_dataSource.NamesFromConvention());
+         _view.SetDataSetNames(names);
       }
+
+      public event EventHandler<EventArgs> OnImportData = delegate { };
+
+      public event EventHandler<DataSetSelectedEventArgs> OnDataSetSelected = delegate { };
+
+      public event EventHandler<NamingConventionChangedEventArgs> OnNamingConventionChanged = delegate { };
    }
 }
