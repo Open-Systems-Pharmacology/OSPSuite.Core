57e9591af63dc64349646af8b9bbdb27d7ad4eb0
Michael Sevestre <michael@design2code.ca>
2020-12-11 09:14:41 -0500

    Move interface of ILoggerCreator to infrastructure to see if one can delay loading of binaries to solve issues with PKSim.R

--- src/OSPSuite.Infrastructure/Services/LoggerCreator.cs
+++ src/OSPSuite.Infrastructure/Services/LoggerCreator.cs
@@ -4,15 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Logging;
 using OSPSuite.Core.Extensions;
-using OSPSuite.Core.Services;
 
 namespace OSPSuite.Infrastructure.Services
 {
+   public interface ILoggerCreator
+   {
+      ILogger GetOrCreateLogger(string categoryName);
+
+      ILoggerCreator AddLoggingBuilderConfiguration(Func<ILoggingBuilder, ILoggingBuilder> configuration);
+   }
+
    public class LoggerCreator : ILoggerCreator
    {
       private readonly ConcurrentDictionary<string, ILogger> _loggerDict = new ConcurrentDictionary<string, ILogger>();
 
-      private readonly List<Func<ILoggingBuilder, ILoggingBuilder>> _loggingBuilderConfigurations = new List<Func<ILoggingBuilder, ILoggingBuilder>> {builder => builder};
+      private readonly List<Func<ILoggingBuilder, ILoggingBuilder>> _loggingBuilderConfigurations = new List<Func<ILoggingBuilder, ILoggingBuilder>>
+         {builder => builder};
 
       public ILoggerCreator AddLoggingBuilderConfiguration(Func<ILoggingBuilder, ILoggingBuilder> configuration)
       {
