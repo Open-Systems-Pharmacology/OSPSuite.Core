b6f5d93cdffe0f906ba7e2bd14c788ce361bad5a
georgeDaskalakis <geodas13@gmail.com>
2021-01-11 10:48:07 +0100

    annotation include removed

--- src/OSPSuite.Infrastructure.Import/Core/IDataSet.cs
+++ src/OSPSuite.Infrastructure.Import/Core/IDataSet.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace OSPSuite.Infrastructure.Import.Core
