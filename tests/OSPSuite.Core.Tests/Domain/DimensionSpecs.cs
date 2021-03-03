1539822d706fedc8c53d2f1d712adccde6fc41bb
Michael Sevestre <michael@design2code.ca>
2021-03-03 16:55:29 -0500

    Add some more test"

--- tests/OSPSuite.Core.Tests/Domain/DimensionSpecs.cs
+++ tests/OSPSuite.Core.Tests/Domain/DimensionSpecs.cs
@@ -16,6 +16,7 @@ namespace OSPSuite.Core.Domain
          sut.AddUnit("KM", 0.01, 0);
          _unitWithSynonym = sut.AddUnit("dm", 0.1, 0);
          _unitWithSynonym.AddUnitSynonym("dm_2");
+         _unitWithSynonym.AddUnitSynonym("DM_3");
       }
    }
 
@@ -41,6 +42,46 @@ namespace OSPSuite.Core.Domain
       }
    }
 
+   public class When_checking_if_a_dimension_supports_a_unit : concern_for_Dimension
+   {
+      [Observation]
+      public void should_return_true_if_the_unit_by_name_exists()
+      {
+         sut.SupportsUnit("KM").ShouldBeTrue();
+         sut.SupportsUnit("DM_3").ShouldBeTrue();
+      }
+
+      [Observation]
+      public void should_return_true_if_the_unit_by_name_exists_in_and_the_case_is_ignored()
+      {
+         sut.SupportsUnit("km", ignoreCase: true).ShouldBeTrue();
+         sut.SupportsUnit("dM_3", ignoreCase: true).ShouldBeTrue();
+      }
+   }
+
+   public class When_finding_a_unit_by_name : concern_for_Dimension
+   {
+      [Observation]
+      public void should_return_the_unit_if_the_unit_by_name_exists()
+      {
+         sut.FindUnit("KM").ShouldNotBeNull();
+         sut.FindUnit("DM_3").ShouldBeEqualTo(_unitWithSynonym);
+      }
+
+      [Observation]
+      public void should_return_the_unit_if_the_unit_by_name_exists_and_the_case_is_ignored()
+      {
+         sut.FindUnit("kM", ignoreCase: true).ShouldNotBeNull();
+         sut.FindUnit("dM_3", ignoreCase: true).ShouldBeEqualTo(_unitWithSynonym);
+      }
+
+      [Observation]
+      public void should_return_null_otherwise()
+      {
+         sut.FindUnit("toto").ShouldBeNull();
+      }
+   }
+
    public class When_retrieving_a_unit_by_name : concern_for_Dimension
    {
       [Observation]
@@ -60,7 +101,7 @@ namespace OSPSuite.Core.Domain
       [Observation]
       public void should_throw_an_exception_if_it_does_not_exist()
       {
-         The.Action(()=> sut.Unit("toto")).ShouldThrowAn<Exception>();
+         The.Action(() => sut.Unit("toto")).ShouldThrowAn<Exception>();
       }
    }
 
