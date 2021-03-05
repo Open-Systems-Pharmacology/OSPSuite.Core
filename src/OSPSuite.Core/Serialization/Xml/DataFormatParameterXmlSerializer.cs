b0d2698f288b1ef848183909dcc851907ad18529
Abdel Rodríguez <abdelr1982@gmail.com>
2021-03-05 10:13:33 +0100

    Creating a generic formatter for DataFormatParameter that does ColumnName and derives other serializers from it

--- src/OSPSuite.Core/Serialization/Xml/DataFormatParameterXmlSerializer.cs
+++ src/OSPSuite.Core/Serialization/Xml/DataFormatParameterXmlSerializer.cs
@@ -2,11 +2,35 @@
 
 namespace OSPSuite.Core.Serialization.Xml
 {
-   public class DataFormatParameterXmlSerializer : OSPSuiteXmlSerializer<DataFormatParameter>
+   public class DataFormatParameterXmlSerializer<T> : OSPSuiteXmlSerializer<T>
+      where T : DataFormatParameter
    {
       public override void PerformMapping()
       {
          Map(x => x.ColumnName);
       }
+   }
+
+   public class MetaDataFormatParameterXmlSerializer : DataFormatParameterXmlSerializer<MetaDataFormatParameter>
+   {
+      public override void PerformMapping()
+      {
+         base.PerformMapping();
+         Map(x => x.MetaDataId);
+         Map(x => x.IsColumn);
+      }
+   }
+
+   public class MappingDataFormatParameterXmlSerializer : DataFormatParameterXmlSerializer<MappingDataFormatParameter>
+   {
+      public override void PerformMapping()
+      {
+         base.PerformMapping();
+         Map(x => x.MappedColumn);
+      }
+   }
+
+   public class GroupByDataFormatParameterXmlSerializer : DataFormatParameterXmlSerializer<GroupByDataFormatParameter>
+   {
    }
 }
