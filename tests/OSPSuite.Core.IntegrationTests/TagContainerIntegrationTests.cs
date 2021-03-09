988fb8d6e9e186b817fa1f19ed0be8fe5632a80e
Michael Sevestre <michael@design2code.ca>
2021-03-09 15:29:39 -0500

    Fixes #907 repdoruce the bug first (#908)
    * WIP #907 repdoruce the bug first
    
    * Fixes #907 in container tag

---
+++ tests/OSPSuite.Core.IntegrationTests/TagContainerIntegrationTests.cs
@@ -0,0 +1,25 @@
+﻿using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+
+namespace OSPSuite.Core
+{
+   public class When_loading_a_simulation_with_reaction_tags_defined_as_in_container : ContextForModelConstructorIntegration
+   {
+      private IModel _model;
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _model = CreateFrom("tag_reactions").Model;
+      }
+
+      [Observation]
+      public void should_create_the_simulation_at_the_expected_location()
+      {
+         var intracellular = _model.Root.EntityAt<Container>("Organism", "Liver", "Intracellular");
+         intracellular.Container("MatchTag").ShouldNotBeNull();
+         intracellular.Container("InContainer").ShouldNotBeNull();
+      }
+   }
+}
