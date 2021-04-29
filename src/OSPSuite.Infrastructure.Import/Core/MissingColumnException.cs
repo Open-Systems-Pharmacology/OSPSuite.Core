3a279951ce3ee9c291dbed59f82d658d09499a03
georgeDaskalakis <geodas13@gmail.com>
2021-04-29 22:56:24 +0200

    fixes #950

---
+++ src/OSPSuite.Infrastructure.Import/Core/MissingColumnException.cs
@@ -0,0 +1,12 @@
+﻿using OSPSuite.Assets;
+using System;
+
+namespace OSPSuite.Infrastructure.Import.Core
+{
+   public class MissingColumnException : Exception
+   {
+      public MissingColumnException(string missingColumn) : base(Error.MissingColumnException(missingColumn))
+      {
+      }
+   }
+}
