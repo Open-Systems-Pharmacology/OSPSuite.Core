d6fc1aea71ac8149b8872ca321f4461b6759f7a9
Abdel Rodríguez <abdelr1982@gmail.com>
2021-04-30 10:03:37 +0200

    Fixing build

--- src/OSPSuite.Presentation/Presenters/Importer/ImporterPresenter.cs
+++ src/OSPSuite.Presentation/Presenters/Importer/ImporterPresenter.cs
@@ -238,7 +238,7 @@ namespace OSPSuite.Presentation.Presenters.Importer
 
          keys.AddRange(_dataSource.GetMappings().Select(m => m.Id));
          _confirmationPresenter.SetKeys(keys);
-         _confirmationPresenter.SetNamingConventions(_dataImporterSettings.NamingConventions);
+         _confirmationPresenter.SetNamingConventions(_dataImporterSettings.NamingConventions.ToList());
          View.EnableConfirmationView();
       }
 
