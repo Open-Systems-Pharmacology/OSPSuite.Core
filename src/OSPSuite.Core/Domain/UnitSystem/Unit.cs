8a12a58d1859c1569354939e4d297a639013a8c1
Michael Sevestre <michael@design2code.ca>
2021-05-18 14:47:34 -0400

    Fixes #1043 unit or default (#1044)
    * Fixes #1043 unit or default
    
    * Fixes #1043 unit or default
    
    * Fixes #1043 unit or default

--- src/OSPSuite.Core/Domain/UnitSystem/Unit.cs
+++ src/OSPSuite.Core/Domain/UnitSystem/Unit.cs
@@ -96,7 +96,7 @@ namespace OSPSuite.Core.Domain.UnitSystem
          FactorFormula = factor.ToString(NumberFormatInfo.InvariantInfo);
       }
 
-      public virtual bool HasSynonym(string name) => _unitSynonyms.Contains(name);
+      public virtual bool HasSynonym(string name) => name != null && _unitSynonyms.Contains(name);
 
       internal virtual void AddUnitSynonym(string name) => AddUnitSynonym(new UnitSynonym(name));
 
--- tests/OSPSuite.Core.Tests/Domain/DimensionSpecs.cs
+++ tests/OSPSuite.Core.Tests/Domain/DimensionSpecs.cs
@@ -48,6 +48,21 @@ namespace OSPSuite.Core.Domain
       }
    }
 
+   public class When_retrieving_the_units_or_default_for_a_dimension : concern_for_Dimension
+   {
+      [Observation]
+      public void should_return_the_expected_unit_if_defined()
+      {
+         sut.UnitOrDefault("dm").ShouldBeEqualTo(_unitWithSynonym);
+      }
+
+      [Observation]
+      public void should_return_the_default_unit_if_the_unit_is_null()
+      {
+         sut.UnitOrDefault(null).ShouldBeEqualTo(sut.DefaultUnit);
+      }
+   }
+
    public class When_checking_if_a_dimension_supports_a_unit : concern_for_Dimension
    {
       [Observation]
@@ -76,7 +91,6 @@ namespace OSPSuite.Core.Domain
          sut.FindUnit("KM").ShouldNotBeNull();
          sut.FindUnit("DM_3").ShouldBeEqualTo(_unitWithSynonym);
          sut.FindUnit("min2/m3").ShouldNotBeNull();
-
       }
 
       [Observation]
