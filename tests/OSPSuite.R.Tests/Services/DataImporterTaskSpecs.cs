04318c22017bf341e1b5bdd27207e4d3e0f44101
Abdel Rodríguez <abdelr1982@gmail.com>
2021-07-13 12:07:08 +0200

    Could also load from an already existing configuration object

--- tests/OSPSuite.R.Tests/Services/DataImporterTaskSpecs.cs
+++ tests/OSPSuite.R.Tests/Services/DataImporterTaskSpecs.cs
@@ -24,6 +24,7 @@ namespace OSPSuite.R.Services
          sut.ImportXslxFromConfiguration(getFileFullName("importerConfiguration1.xml"), getFileFullName("sample1.xlsx")).Count.ShouldBeEqualTo(1);
       }
 
+
       [Observation]
       public void should_import_simple_data_from_csv()
       {
@@ -34,6 +35,25 @@ namespace OSPSuite.R.Services
       public void should_return_empty_on_invalid_file_name()
       {
          sut.ImportXslxFromConfiguration(getFileFullName("importerConfiguration1.xml"), "").Count.ShouldBeEqualTo(0);
+      }
+
+      [Observation]
+      public void should_read_configuration()
+      {
+         sut.GetConfiguration(getFileFullName("importerConfiguration1.xml")).ShouldNotBeNull();
+      }
+
+      [Observation]
+      public void should_import_simple_data_with_configuration_object()
+      {
+         sut.ImportXslxFromConfiguration(sut.GetConfiguration(getFileFullName("importerConfiguration1.xml")), getFileFullName("sample1.xlsx")).Count.ShouldBeEqualTo(1);
+      }
+
+
+      [Observation]
+      public void should_import_simple_data_from_csv_with_configuration_object()
+      {
+         sut.ImportCsvFromConfiguration(sut.GetConfiguration(getFileFullName("importerConfiguration1.csv.xml")), getFileFullName("sample1.csv"), ';').Count.ShouldBeEqualTo(1);
       }
    }
 }
