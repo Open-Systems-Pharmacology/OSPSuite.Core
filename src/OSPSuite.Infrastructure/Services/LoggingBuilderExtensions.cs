a512c7956f4a5def02e43ac44482ed37e6a6d33e
georgeDaskalakis <geodas13@gmail.com>
2021-09-21 08:51:21 +0200

    comment added

--- src/OSPSuite.Infrastructure/Services/LoggingBuilderExtensions.cs
+++ src/OSPSuite.Infrastructure/Services/LoggingBuilderExtensions.cs
@@ -28,9 +28,15 @@ namespace OSPSuite.Infrastructure.Services
                serilogLevel = LogEventLevel.Fatal;
                break;
          }
+
+         //this is a workaround to avoid having the date automatically appended to the log file name
+         //(eg log_20210921.txt, when input name is simply log.txt).
+         //Setting rollOnFileSizeLimit disables that, since the first log is log.txt, the next log_001.txt and so on
+         //since we do not expect to ever reach the log limit (in the qualificationRunner fe), this results in one file
+         //1073741824 is as far as I understand the maximum possible fileSizeLimitBytes
          builder.AddSerilog(
            new LoggerConfiguration()
-             .WriteTo.File( logFileFullPath, fileSizeLimitBytes: 1073741824, rollOnFileSizeLimit: true)
+             .WriteTo.File( logFileFullPath, fileSizeLimitBytes: 1073741824, rollOnFileSizeLimit: true, restrictedToMinimumLevel: serilogLevel, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {SourceContext:l} {Level:u}] {Message:l} {Exception}")
              .CreateLogger()
          );
          return builder;
