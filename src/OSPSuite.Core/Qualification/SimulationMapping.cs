74c7208db589c9c0a3564aae11079a36b987bf55
Michael Sevestre <michael@design2code.ca>
2021-09-23 10:43:51 -0400

    Fixes #1308 export simulation file

--- src/OSPSuite.Core/Qualification/SimulationMapping.cs
+++ src/OSPSuite.Core/Qualification/SimulationMapping.cs
@@ -2,8 +2,25 @@
 {
    public class SimulationMapping : IReferencingSimulation
    {
+      /// <summary>
+      /// Name of project
+      /// </summary>
       public string Project { get; set; }
+
+      /// <summary>
+      /// Name of simulation
+      /// </summary>
       public string Simulation { get; set; }
+
+      /// <summary>
+      /// Path where simulation artifacts will be exported
+      /// </summary>
       public string Path { get; set; }
+
+      /// <summary>
+      /// Name of simulation file. It is not always the name of the simulation as the simulation file is stripped from all invalid characters
+      /// </summary>
+      public string SimulationFile { get; set; }
+
    }
 }
