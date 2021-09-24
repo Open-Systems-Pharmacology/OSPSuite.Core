c5f32ed743e0c7a59ff1a8f8ee91baa42303637e
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2021-09-24 15:21:12 +0200

    1307 multiple log files (#1310)
    * test for shared file
    
    * support for multiple log files added
    
    * restoring .yml
    
    * removing code duplication
    
    * parameter for central QR log added
    
    * triggering build
    
    * -yml corrected
    
    * building again
    
    * restoring .yml
    
    * code review changes

--- src/OSPSuite.Infrastructure/Services/LoggingBuilderExtensions.cs
+++ src/OSPSuite.Infrastructure/Services/LoggingBuilderExtensions.cs
@@ -1,16 +1,23 @@
-﻿
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
 using Serilog.Events;
+using OSPSuite.Core.Domain;
 
 namespace OSPSuite.Infrastructure.Services
 {
    public static class LoggingBuilderExtensions
    {
-      public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string logFileFullPath, LogLevel level)
+      public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string logFileNameFileFullPath, LogLevel level, bool shared= true,
+         string outputTemplate = Core.Domain.Constants.LoggerConstants.DEFAULT_LOG_ENTRY_TEMPLATE)
       {
-         LogEventLevel serilogLevel = LogEventLevel.Verbose;
+         return builder.AddFile(new string[] { logFileNameFileFullPath }, level, shared, outputTemplate);
+      }
+
+      public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string[] logFileFullPaths, LogLevel level, bool shared = true,
+         string outputTemplate = Core.Domain.Constants.LoggerConstants.DEFAULT_LOG_ENTRY_TEMPLATE)
+      {
+         var serilogLevel = LogEventLevel.Verbose;
          switch (level)
          {
             case LogLevel.Debug:
@@ -30,18 +37,23 @@ namespace OSPSuite.Infrastructure.Services
                break;
          }
 
-         //this is a workaround to avoid having the date automatically appended to the log file name
-         //(eg log_20210921.txt, when input name is simply log.txt).
-         //Setting rollOnFileSizeLimit disables that, since the first log is log.txt, the next log_001.txt and so on
-         //since we do not expect to ever reach the log limit (in the qualificationRunner fe), this results in one file
-         //1073741824 is as far as I understand the maximum possible fileSizeLimitBytes
-         builder.AddSerilog(
-           new LoggerConfiguration()
-              .MinimumLevel.ControlledBy(new LoggingLevelSwitch { MinimumLevel = serilogLevel })
-              .WriteTo.File( logFileFullPath, fileSizeLimitBytes: 1073741824, rollOnFileSizeLimit: true, restrictedToMinimumLevel: serilogLevel, outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {SourceContext:l} {Level:u}] {Message:l} {NewLine:l} {Exception}")
-             .CreateLogger()
-         );
+         var loggerConfiguration = new LoggerConfiguration().MinimumLevel.ControlledBy(new LoggingLevelSwitch { MinimumLevel = serilogLevel });
+
+         foreach (var logFile in logFileFullPaths)
+         {
+            //this is a workaround to avoid having the date automatically appended to the log file name
+            //(eg log_20210921.txt, when input name is simply log.txt).
+            //Setting rollOnFileSizeLimit disables that, since the first log is log.txt, the next log_001.txt and so on
+            //since we do not expect to ever reach the log limit (in the qualificationRunner fe), this results in one file
+            //1073741824 is as far as I understand the maximum possible fileSizeLimitBytes
+            loggerConfiguration.WriteTo.File(logFile, fileSizeLimitBytes: 1073741824, rollOnFileSizeLimit: true,
+               restrictedToMinimumLevel: serilogLevel,
+               shared: shared, outputTemplate: outputTemplate);
+         }
+
+         builder.AddSerilog(loggerConfiguration.CreateLogger());
          return builder;
       }
+
    }
 }
