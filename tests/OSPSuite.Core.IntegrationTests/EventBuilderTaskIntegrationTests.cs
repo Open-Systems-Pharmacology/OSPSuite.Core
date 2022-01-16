480fe768195bf354b0767094c2f15cb8036c29c5
Michael Sevestre <michael@design2code.ca>
2022-01-16 12:32:35 +0100

    Update dimension to latest

--- tests/OSPSuite.Core.IntegrationTests/EventBuilderTaskIntegrationTests.cs
+++ tests/OSPSuite.Core.IntegrationTests/EventBuilderTaskIntegrationTests.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Core.Domain;
-using OSPSuite.Helpers;
 
 namespace OSPSuite.Core
 {
@@ -11,15 +10,13 @@ namespace OSPSuite.Core
    {
       protected CreationResult _creationResult;
 
-      public override void GlobalContext()
-      {
-         base.GlobalContext();
-         _creationResult = CreateFrom("EventsAppKeywordReplacement");
-      }
    }
 
    public class When_creating_simulation_with_many_floating_molecules_and_nested_applications_and_nonapplication_eventgroups : concern_for_EventBuilderTask
    {
+
+
+
       private IList<IEventGroup> _allEventGroups;
       private const string C1 = "C1";
       private const string C2 = "C2";
@@ -27,11 +24,14 @@ namespace OSPSuite.Core
       private const string AppC2 = "AppC2";
       private const string AppC3 = "AppC3";
 
-      protected override void Context()
+
+      public override void GlobalContext()
       {
-         base.Context();
+         base.GlobalContext();
+         _creationResult = CreateFrom("EventsAppKeywordReplacement");
          _allEventGroups = _creationResult.Model.Root.GetAllChildren<IEventGroup>().ToList();
       }
+
 
       [Observation]
       public void should_replace_keywords_in_applications_with_molecule_name_of_parent_application()
@@ -52,4 +52,42 @@ namespace OSPSuite.Core
          }
       }
    }
-}	
+
+   public class When_creating_simulation_using_an_event_with_a_keyword_all_floating : concern_for_EventBuilderTask
+   {
+      private IList<IEventGroup> _allEventGroups;
+      private const string C1 = "C1";
+      private const string C2 = "C2";
+      private const string C3 = "C3";
+      private const string AppC2 = "AppC2";
+      private const string AppC3 = "AppC3";
+
+
+      public override void GlobalContext()
+      {
+         base.GlobalContext();
+         _creationResult = CreateFrom("simulation_with_urine_emptying");
+         _allEventGroups = _creationResult.Model.Root.GetAllChildren<IEventGroup>().ToList();
+      }
+
+      [Observation]
+      public void should_replace_keyword_with_the_name_of_all_floating_molecules()
+      {
+         foreach (var eventGroup in _allEventGroups)
+         {
+            var param = eventGroup.GetSingleChildByName<IParameter>("P1");
+            var lastPathEntry = param.Formula.ObjectPaths.ToArray()[0].Last();
+
+            if (eventGroup.Name.StartsWith(AppC2))
+            {
+               lastPathEntry.ShouldBeEqualTo(C2);
+            }
+            else if (eventGroup.Name.StartsWith(AppC3))
+            {
+               lastPathEntry.ShouldBeEqualTo(C3);
+            }
+         }
+      }
+   }
+
+}
