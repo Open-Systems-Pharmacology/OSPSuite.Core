7695cefb23bc3775cec559e5dcca22f5a7704d76
Michael Sevestre <michael@design2code.ca>
2022-01-16 13:21:47 +0100

    Fixes #1447 all floating molecule keyword

--- tests/OSPSuite.Core.IntegrationTests/EventBuilderTaskIntegrationTests.cs
+++ tests/OSPSuite.Core.IntegrationTests/EventBuilderTaskIntegrationTests.cs
@@ -9,21 +9,16 @@ namespace OSPSuite.Core
    public abstract class concern_for_EventBuilderTask : ContextForModelConstructorIntegration
    {
       protected CreationResult _creationResult;
-
    }
 
    public class When_creating_simulation_with_many_floating_molecules_and_nested_applications_and_nonapplication_eventgroups : concern_for_EventBuilderTask
    {
-
-
-
-      private IList<IEventGroup> _allEventGroups;
+      private List<IEventGroup> _allEventGroups;
       private const string C1 = "C1";
       private const string C2 = "C2";
       private const string C3 = "C3";
       private const string AppC2 = "AppC2";
       private const string AppC3 = "AppC3";
-
 
       public override void GlobalContext()
       {
@@ -31,7 +26,6 @@ namespace OSPSuite.Core
          _creationResult = CreateFrom("EventsAppKeywordReplacement");
          _allEventGroups = _creationResult.Model.Root.GetAllChildren<IEventGroup>().ToList();
       }
-
 
       [Observation]
       public void should_replace_keywords_in_applications_with_molecule_name_of_parent_application()
@@ -55,13 +49,7 @@ namespace OSPSuite.Core
 
    public class When_creating_simulation_using_an_event_with_a_keyword_all_floating : concern_for_EventBuilderTask
    {
-      private IList<IEventGroup> _allEventGroups;
-      private const string C1 = "C1";
-      private const string C2 = "C2";
-      private const string C3 = "C3";
-      private const string AppC2 = "AppC2";
-      private const string AppC3 = "AppC3";
-
+      private List<IEventGroup> _allEventGroups;
 
       public override void GlobalContext()
       {
@@ -73,21 +61,11 @@ namespace OSPSuite.Core
       [Observation]
       public void should_replace_keyword_with_the_name_of_all_floating_molecules()
       {
-         foreach (var eventGroup in _allEventGroups)
-         {
-            var param = eventGroup.GetSingleChildByName<IParameter>("P1");
-            var lastPathEntry = param.Formula.ObjectPaths.ToArray()[0].Last();
-
-            if (eventGroup.Name.StartsWith(AppC2))
-            {
-               lastPathEntry.ShouldBeEqualTo(C2);
-            }
-            else if (eventGroup.Name.StartsWith(AppC3))
-            {
-               lastPathEntry.ShouldBeEqualTo(C3);
-            }
-         }
+         var urineEmptyingEventGroup = _allEventGroups.FindByName("PIPI_1");
+         var urineEmptyingStartEvent = urineEmptyingEventGroup.Events.FindByName("Urine_Emptying_StartEvent");
+         urineEmptyingStartEvent.Assignments.Count().ShouldBeEqualTo(2);
+         urineEmptyingStartEvent.Assignments.Find(x => x.ObjectPath.PathAsString.Contains("C1")).ShouldNotBeNull();
+         urineEmptyingStartEvent.Assignments.Find(x => x.ObjectPath.PathAsString.Contains("C2")).ShouldNotBeNull();
       }
    }
-
 }
