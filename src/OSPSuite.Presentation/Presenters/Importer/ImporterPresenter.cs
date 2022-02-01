f06bf00f1860b1fb3f97d2e4ebcba6cd5a2b9183
Abdel Rodríguez <abdelr1982@gmail.com>
2022-02-01 12:27:05 +0100

    Reset mapping before loading configuration

--- src/OSPSuite.Presentation/Presenters/Importer/ImporterPresenter.cs
+++ src/OSPSuite.Presentation/Presenters/Importer/ImporterPresenter.cs
@@ -232,7 +232,7 @@ namespace OSPSuite.Presentation.Presenters.Importer
          _columnMappingPresenter.SetRawData(e.TabData);
       }
 
-      private void onResetMappingBasedOnCurrentSheet(object sender, EventArgs e)
+      protected virtual void onResetMappingBasedOnCurrentSheet(object sender, EventArgs e)
       {
          if (confirmDroppingOfLoadedSheets())
             return;
@@ -387,7 +387,7 @@ namespace OSPSuite.Presentation.Presenters.Importer
          _importerDataPresenter.DisableImportedSheets();
       }
 
-      private bool confirmDroppingOfLoadedSheets()
+      protected virtual bool confirmDroppingOfLoadedSheets()
       {
          return _dataSource.DataSets.Count != 0 && _dialogCreator.MessageBoxYesNo(Captions.Importer.ActionWillEraseLoadedData) != ViewResult.Yes;
       }
@@ -403,6 +403,8 @@ namespace OSPSuite.Presentation.Presenters.Importer
       {
          if (confirmDroppingOfLoadedSheets())
                return;
+
+         onResetMappingBasedOnCurrentSheet(this, null);
 
          var fileName = _dialogCreator.AskForFileToOpen(Captions.Importer.ApplyConfiguration, Constants.Filter.XML_FILE_FILTER,
             Constants.DirectoryKey.OBSERVED_DATA);
