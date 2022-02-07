3ab30bf0989fca814cffc6a82103e94429ae815a
Michael Sevestre <michael@design2code.ca>
2022-02-07 12:00:01 +0100

    Fixes #1457 gc for R

---
+++ src/OSPSuite.Core/Services/GarbageCollectionTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime;
+
+namespace OSPSuite.Core.Services
+{
+   public static class GarbageCollectionTask
+   {
+      /// <summary>
+      ///    Force GC Collection compacting the Large Object Heap. Implementation very similar to what DotMemory
+      ///    is doing according to one of their post
+      /// </summary>
+      public static void ForceGC()
+      {
+         GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+         for (int i = 0; i < 4; i++)
+         {
+            GC.Collect(2, GCCollectionMode.Forced, blocking: true);
+            GC.WaitForPendingFinalizers();
+         }
+      }
+   }
+}
