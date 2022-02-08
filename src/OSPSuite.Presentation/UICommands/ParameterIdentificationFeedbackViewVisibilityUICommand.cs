a5e525eca2c420dcc9ee6a68bfd6464659065fad
Abdel Rodríguez <abdelr1982@gmail.com>
2022-02-08 16:08:26 +0100

    Update ParameterIdentificationFeedbackViewVisibilityUICommand.cs (#1486)
    * Update ParameterIdentificationFeedbackViewVisibilityUICommand.cs
    
    * Implementing ActiveObjectUICommand
    
    * Missing changes
    
    * ActiveObjectUICommand on proper domain

--- src/OSPSuite.Presentation/UICommands/ParameterIdentificationFeedbackViewVisibilityUICommand.cs
+++ src/OSPSuite.Presentation/UICommands/ParameterIdentificationFeedbackViewVisibilityUICommand.cs
@@ -1,31 +1,24 @@
 ﻿using OSPSuite.Core.Domain.ParameterIdentifications;
-using OSPSuite.Core.Events;
+using OSPSuite.Core.Services;
 using OSPSuite.Presentation.Presenters.ParameterIdentifications;
 using OSPSuite.Presentation.Services;
-using OSPSuite.Utility.Events;
 
 namespace OSPSuite.Presentation.UICommands
 {
-   public class ParameterIdentificationFeedbackViewVisibilityUICommand : ObjectUICommand<IParameterIdentificationFeedbackPresenter>, IListener<ParameterIdentificationSelectedEvent>
+   public class ParameterIdentificationFeedbackViewVisibilityUICommand : ActiveObjectUICommand<ParameterIdentification>
    {
       private readonly ISingleStartPresenterTask _singleStartPresenterTask;
-      private ParameterIdentification _parameterIdentification;
       private readonly IParameterIdentificationFeedbackManager _parameterIdentificationFeedbackManager;
 
-      public ParameterIdentificationFeedbackViewVisibilityUICommand(ISingleStartPresenterTask singleStartPresenterTask, IParameterIdentificationFeedbackManager parameterIdentificationFeedbackManager)
+      public ParameterIdentificationFeedbackViewVisibilityUICommand(ISingleStartPresenterTask singleStartPresenterTask, IParameterIdentificationFeedbackManager parameterIdentificationFeedbackManager, IActiveSubjectRetriever activeSubjectRetriever) : base(activeSubjectRetriever)
       {
          _singleStartPresenterTask = singleStartPresenterTask;
          _parameterIdentificationFeedbackManager = parameterIdentificationFeedbackManager;
       }
 
-      public void Handle(ParameterIdentificationSelectedEvent eventToHandle)
-      {
-         _parameterIdentification = eventToHandle.ParameterIdentification;
-      }
-
       protected override void PerformExecute()
       {
-         _singleStartPresenterTask.StartForSubject(_parameterIdentificationFeedbackManager.GetFeedbackFor(_parameterIdentification));
+         _singleStartPresenterTask.StartForSubject(_parameterIdentificationFeedbackManager.GetFeedbackFor(Subject));
       }
    }
 }
