66ff32b2f72c7f8cb6feb19ea1062ddec340bc31
Abdel Rodríguez <abdelr1982@gmail.com>
2022-03-01 12:07:18 +0100

    Adding normal flow test

---
+++ tests/OSPSuite.Infrastructure.Tests/Import/MixColumnsDataFormatSpecs.cs
@@ -0,0 +1,75 @@
+﻿using FakeItEasy;
+using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Import;
+using OSPSuite.Helpers;
+using OSPSuite.Infrastructure.Import.Core;
+using OSPSuite.Infrastructure.Import.Core.DataFormat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSPSuite.Infrastructure.Import
+{
+   public abstract class concern_for_MixColumnsDataFormat : ContextSpecification<MixColumnsDataFormat>
+   {
+      protected override void Context()
+      {
+         sut = new MixColumnsDataFormat();
+      }
+   }
+
+   public class When_setting_parameters : concern_for_MixColumnsDataFormat
+   {
+      protected IUnformattedData _rawData;
+      protected ColumnInfoCache _columnInfos;
+      protected IReadOnlyList<MetaDataCategory> _metaDataCategories;
+
+      protected override void Context()
+      {
+         base.Context();
+         _rawData = A.Fake<IUnformattedData>();
+         A.CallTo(() => _rawData.GetHeaders()).Returns(new[] { "Time [h]", "Concentration [mol]", "Error [mol]" });
+         A.CallTo(() => _rawData.GetColumnDescription(A<string>.Ignored)).Returns(new ColumnDescription(0) { Level = ColumnDescription.MeasurementLevel.Numeric });
+         _columnInfos = new ColumnInfoCache
+         {
+            new ColumnInfo() { DisplayName = "Time", Name ="Time" },
+            new ColumnInfo() { DisplayName = "Concentration", Name = "Concentration"},
+            new ColumnInfo() { DisplayName = "Error", Name = "Error", IsMandatory = false, RelatedColumnOf = "Concentration"}
+         };
+         _columnInfos["Time"].SupportedDimensions.Add(DimensionFactoryForSpecs.TimeDimension);
+         _columnInfos["Concentration"].SupportedDimensions.Add(DimensionFactoryForSpecs.ConcentrationDimension);
+         _columnInfos["Error"].SupportedDimensions.Add(DimensionFactoryForSpecs.ConcentrationDimension);
+         _metaDataCategories = new List<MetaDataCategory>()
+         {
+            new MetaDataCategory() { Name = "Organ" },
+            new MetaDataCategory() { Name = "Compartment" },
+            new MetaDataCategory() { Name = "Species" },
+            new MetaDataCategory() { Name = "Dose" },
+            new MetaDataCategory() { Name = "Molecule" },
+            new MetaDataCategory() { Name = "Route" }
+         };
+      }
+
+      protected override void Because()
+      {
+         sut.SetParameters(_rawData, _columnInfos, _metaDataCategories);
+      }
+
+      [Observation]
+      public void infers_unit_from_header_for_measurement_and_error()
+      {
+         var concentrationMappingColumn = sut.Parameters.OfType<MappingDataFormatParameter>().First(x => x.ColumnName == "Concentration [mol]").MappedColumn;
+         concentrationMappingColumn.Dimension.Name.ShouldBeEqualTo("Concentration");
+         concentrationMappingColumn.Unit.SelectedUnit.ShouldBeEqualTo("mol");
+
+         var errorMappingColumn = sut.Parameters.OfType<MappingDataFormatParameter>().First(x => x.ColumnName == "Error [mol]").MappedColumn;
+         errorMappingColumn.Dimension.Name.ShouldBeEqualTo("Concentration");
+         errorMappingColumn.Unit.SelectedUnit.ShouldBeEqualTo("mol");
+         errorMappingColumn.ErrorStdDev.ShouldBeEqualTo(Constants.STD_DEV_ARITHMETIC);
+      }
+   }
+}
