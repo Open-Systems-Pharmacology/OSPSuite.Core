8cad41f83d2d281a41dd12548a986a532c519eb8
Abdel Rodríguez <abdelr1982@gmail.com>
2022-03-01 12:34:14 +0100

    Adding extra testcase

--- tests/OSPSuite.Infrastructure.Tests/Import/MixColumnsDataFormatSpecs.cs
+++ tests/OSPSuite.Infrastructure.Tests/Import/MixColumnsDataFormatSpecs.cs
@@ -16,23 +16,16 @@ namespace OSPSuite.Infrastructure.Import
 {
    public abstract class concern_for_MixColumnsDataFormat : ContextSpecification<MixColumnsDataFormat>
    {
-      protected override void Context()
-      {
-         sut = new MixColumnsDataFormat();
-      }
-   }
-
-   public class When_setting_parameters : concern_for_MixColumnsDataFormat
-   {
       protected IUnformattedData _rawData;
       protected ColumnInfoCache _columnInfos;
       protected IReadOnlyList<MetaDataCategory> _metaDataCategories;
+      protected virtual IEnumerable<string> _headers { get => new[] { "Time [h]", "Concentration [mol]", "Error [mol]" }; }
 
       protected override void Context()
       {
-         base.Context();
+         sut = new MixColumnsDataFormat();
          _rawData = A.Fake<IUnformattedData>();
-         A.CallTo(() => _rawData.GetHeaders()).Returns(new[] { "Time [h]", "Concentration [mol]", "Error [mol]" });
+         A.CallTo(() => _rawData.GetHeaders()).ReturnsLazily(() => _headers);
          A.CallTo(() => _rawData.GetColumnDescription(A<string>.Ignored)).Returns(new ColumnDescription(0) { Level = ColumnDescription.MeasurementLevel.Numeric });
          _columnInfos = new ColumnInfoCache
          {
@@ -53,7 +46,10 @@ namespace OSPSuite.Infrastructure.Import
             new MetaDataCategory() { Name = "Route" }
          };
       }
+   }
 
+   public class When_setting_parameters : concern_for_MixColumnsDataFormat
+   {
       protected override void Because()
       {
          sut.SetParameters(_rawData, _columnInfos, _metaDataCategories);
@@ -70,6 +66,27 @@ namespace OSPSuite.Infrastructure.Import
          errorMappingColumn.Dimension.Name.ShouldBeEqualTo("Concentration");
          errorMappingColumn.Unit.SelectedUnit.ShouldBeEqualTo("mol");
          errorMappingColumn.ErrorStdDev.ShouldBeEqualTo(Constants.STD_DEV_ARITHMETIC);
+      }
+   }
+
+   public class When_setting_parameters_without_error_unit_on_headers : concern_for_MixColumnsDataFormat
+   {
+      protected override IEnumerable<string> _headers { get => new[] { "Time [h]", "Concentration [mol]", "Error" }; }
+
+      protected override void Because()
+      {
+         sut.SetParameters(_rawData, _columnInfos, _metaDataCategories);
+      }
+
+      [Observation]
+      public void infers_unit_from_header_for_measurement_and_error()
+      {
+         var concentrationMappingColumn = sut.Parameters.OfType<MappingDataFormatParameter>().First(x => x.ColumnName == "Concentration [mol]").MappedColumn;
+         concentrationMappingColumn.Dimension.Name.ShouldBeEqualTo("Concentration");
+         concentrationMappingColumn.Unit.SelectedUnit.ShouldBeEqualTo("mol");
+
+         var errorMappingColumn = sut.Parameters.OfType<MappingDataFormatParameter>().First(x => x.ColumnName == "Error").MappedColumn;
+         errorMappingColumn.ErrorStdDev.ShouldBeEqualTo(Constants.STD_DEV_GEOMETRIC);
       }
    }
 }
