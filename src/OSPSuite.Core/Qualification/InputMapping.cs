6519cf161ef3814f52dbc5522c6adc0c1bcded5e
Michael Sevestre <michael@design2code.ca>
2022-04-13 17:29:25 -0400

    Fixes #1586 difference in qualification plan and configuration plan field

--- src/OSPSuite.Core/Qualification/InputMapping.cs
+++ src/OSPSuite.Core/Qualification/InputMapping.cs
@@ -1,8 +1,9 @@
 ﻿namespace OSPSuite.Core.Qualification
 {
-   public class InputMapping
+   public class InputMapping: IWithSectionReference
    {
-      public int SectionId { get; set; }
       public string Path { get; set; }
+      public int? SectionId { get; set; }
+      public string SectionReference { get; set; }
    }
 }
--- src/OSPSuite.Core/Qualification/SimulationPlot.cs
+++ src/OSPSuite.Core/Qualification/SimulationPlot.cs
@@ -1,9 +1,10 @@
 ﻿namespace OSPSuite.Core.Qualification
 {
-   public class SimulationPlot : IReferencingSimulation
+   public class SimulationPlot : IReferencingSimulation, IWithSectionReference
    {
       public string Simulation { get; set; }
       public string Project { get; set; }
-      public int SectionId { get; set; }
+      public int? SectionId { get; set; }
+      public string SectionReference { get; set; }
    }
 }
