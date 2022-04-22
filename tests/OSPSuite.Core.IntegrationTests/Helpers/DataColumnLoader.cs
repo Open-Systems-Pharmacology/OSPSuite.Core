2406bb23ee8798fd1457fb3458c8a2fb1a11b519
Abdel Rodríguez <abdelr1982@gmail.com>
2022-04-22 16:04:34 +0200

    Restoring tests (#1599)
    * Restoring tests
    
    * Using culture invariant on tests
    
    * Update OSPSuite.Core.IntegrationTests.csproj

---
+++ tests/OSPSuite.Core.IntegrationTests/Helpers/DataColumnLoader.cs
@@ -0,0 +1,43 @@
+﻿using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Data;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace OSPSuite.Core.Helpers
+{
+   internal static class DataColumnLoader
+   {
+      public static DataColumn GetDataColumnFrom(string csvName)
+      {
+         var fileName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"Data\\{csvName}.csv");
+         var timeValues = new List<float>();
+         var concValues = new List<float>();
+         foreach (string line in File.ReadLines(fileName))
+         {
+            var cells = line.Split(';');
+            timeValues.Add(float.Parse(cells[0], CultureInfo.InvariantCulture));
+            concValues.Add(float.Parse(cells[1], CultureInfo.InvariantCulture));
+         }
+
+         var baseGrid = new BaseGrid(
+            "Time", 
+            "Time", 
+            Constants.Dimension.NO_DIMENSION
+         ) { 
+            Values = timeValues.ToArray() 
+         };
+
+         return
+            new DataColumn(
+               "Value",
+               csvName, 
+               Constants.Dimension.NO_DIMENSION, 
+               baseGrid
+            ) { 
+               Values = concValues.ToArray() 
+            };
+      }
+   }
+}
