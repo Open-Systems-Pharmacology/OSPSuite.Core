90a03292794e92602cf073b6890ea27ffbbf096a
Michael Sevestre <michael@design2code.ca>
2022-07-01 16:09:36 -0400

    Add missing constants

--- src/OSPSuite.Infrastructure.Import/Extensions/ImporterDataTableExtensions.cs
+++ src/OSPSuite.Infrastructure.Import/Extensions/ImporterDataTableExtensions.cs
@@ -6,11 +6,10 @@ namespace OSPSuite.Infrastructure.Import.Extensions
    public static class ImporterDataTableExtensions
    {
       /// <summary>
-      /// Adds a list of values as a row to a DataTable
+      ///    Adds a list of values as a row to a DataTable
       /// </summary>
       /// <param name="dataTable">Data Table to populate</param>
       /// <param name="rowValues">The values to be added to the DataTable</param>
-      /// <param name="dataTable">DataTable to add values to</param>
       public static void AddRowToDataTable(this DataTable dataTable, IReadOnlyList<string> rowValues)
       {
          var dataRow = dataTable.NewRow();
