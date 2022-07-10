39b880491817ff5f81295a9c7d7aaea77853b3fc
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-07-10 21:26:42 +0200

    initial implementation

---
+++ src/OSPSuite.UI/Views/SimulationAnalysisView.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace OSPSuite.UI.Views
+{
+   partial class SimulationAnalysisView
+   {
+      /// <summary> 
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary> 
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing)
+      {
+         if (disposing && (components != null))
+         {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Component Designer generated code
+
+      /// <summary> 
+      /// Required method for Designer support - do not modify 
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+         this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+         this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+         this.panelChart = new DevExpress.XtraEditors.PanelControl();
+         this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+         this.layoutControl1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.panelChart)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // layoutControl1
+         // 
+         this.layoutControl1.Controls.Add(this.panelChart);
+         this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+         this.layoutControl1.Location = new System.Drawing.Point(82, 54);
+         this.layoutControl1.Name = "layoutControl1";
+         this.layoutControl1.Root = this.Root;
+         this.layoutControl1.Size = new System.Drawing.Size(1917, 1548);
+         this.layoutControl1.TabIndex = 0;
+         this.layoutControl1.Text = "layoutControl1";
+         // 
+         // Root
+         // 
+         this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+         this.Root.GroupBordersVisible = false;
+         this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+         this.Root.Name = "Root";
+         this.Root.Size = new System.Drawing.Size(1917, 1548);
+         this.Root.TextVisible = false;
+         // 
+         // panelChart
+         // 
+         this.panelChart.Location = new System.Drawing.Point(12, 12);
+         this.panelChart.Name = "panelChart";
+         this.panelChart.Size = new System.Drawing.Size(1893, 1524);
+         this.panelChart.TabIndex = 4;
+         // 
+         // layoutControlItem1
+         // 
+         this.layoutControlItem1.Control = this.panelChart;
+         this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+         this.layoutControlItem1.Name = "layoutControlItem1";
+         this.layoutControlItem1.Size = new System.Drawing.Size(1897, 1528);
+         this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+         this.layoutControlItem1.TextVisible = false;
+         // 
+         // SimulationAnalysisView
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 33F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.Controls.Add(this.layoutControl1);
+         this.Name = "SimulationAnalysisView";
+         this.Size = new System.Drawing.Size(2259, 1781);
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+         this.layoutControl1.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.panelChart)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+         this.ResumeLayout(false);
+
+      }
+
+      #endregion
+
+      private DevExpress.XtraLayout.LayoutControl layoutControl1;
+      private DevExpress.XtraLayout.LayoutControlGroup Root;
+      private DevExpress.XtraEditors.PanelControl panelChart;
+      private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+   }
+}
---
+++ src/OSPSuite.UI/Views/SimulationAnalysisView.cs
@@ -0,0 +1,19 @@
+﻿using OSPSuite.Presentation.Views;
+using OSPSuite.UI.Controls;
+using OSPSuite.UI.Extensions;
+
+namespace OSPSuite.UI.Views
+{
+   public partial class SimulationAnalysisView : BaseUserControl, ISimulationAnalysisView
+   {
+      public SimulationAnalysisView()
+      {
+         InitializeComponent();
+      }
+
+      public void SetAnalysisView(IView view)
+      {
+         panelChart.FillWith(view);
+      }
+   }
+}
