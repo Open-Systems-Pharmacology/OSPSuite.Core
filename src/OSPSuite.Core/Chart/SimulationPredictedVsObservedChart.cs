26113d7d82b92fbf82868ae6d29439ae2f875d2e
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-07-13 13:12:50 +0200

    residuals first test

--- src/OSPSuite.Core/Chart/SimulationPredictedVsObservedChart.cs
+++ src/OSPSuite.Core/Chart/SimulationPredictedVsObservedChart.cs
@@ -1,15 +1,45 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using OSPSuite.Assets;
 using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Data;
 using OSPSuite.Core.Domain.UnitSystem;
 using OSPSuite.Utility.Extensions;
 
 namespace OSPSuite.Core.Chart
 {
-   public class SimulationPredictedVsObservedChart : ChartWithObservedData, ISimulationAnalysis
-   { 
+   public abstract class SimulationAnalysisChart : ChartWithObservedData, ISimulationAnalysis
+   {
       public IAnalysable Analysable { get; set; }
+   }
 
+   public abstract class SimulationAnalysisChartWithLocalRepositories : SimulationAnalysisChart
+   {
+      private readonly List<DataRepository> _dataRepositories;
+      public virtual IReadOnlyList<DataRepository> DataRepositories => _dataRepositories;
+
+      protected SimulationAnalysisChartWithLocalRepositories()
+      {
+         _dataRepositories = new List<DataRepository>();
+      }
+
+      public virtual void AddRepository(DataRepository dataRepository)
+      {
+         _dataRepositories.Add(dataRepository);
+      }
+
+      public virtual void ClearDataRepositories()
+      {
+         _dataRepositories.Clear();
+      }
+
+      public virtual void AddRepositories(IEnumerable<DataRepository> dataRepositories)
+      {
+         dataRepositories.Each(AddRepository);
+      }
+   }
+   public class SimulationPredictedVsObservedChart : SimulationAnalysisChart
+   {
       public void UpdateAxesVisibility()
       {
          var visibleAxes = Axes.Where(x => x.Dimension != null && x.Dimension.HasSharedUnitNamesWith(XAxis.Dimension)).ToList();
@@ -17,4 +47,10 @@ namespace OSPSuite.Core.Chart
          Axes.Except(visibleAxes).Each(axis => axis.Visible = false);
       }
    }
+
+   public class SimulationResidualVsTimeChart : SimulationAnalysisChartWithLocalRepositories
+   {
+   }
+
+
 }
---
+++ src/OSPSuite.Presentation/Presenters/SimulationResidualVsTimeChartPresenter.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MathNet.Numerics;
+using OSPSuite.Assets;
+using OSPSuite.Core.Chart;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Data;
+using OSPSuite.Core.Domain.ParameterIdentifications;
+using OSPSuite.Core.Domain.Repositories;
+using OSPSuite.Core.Domain.Services.ParameterIdentifications;
+using OSPSuite.Core.Events;
+using OSPSuite.Core.Extensions;
+using OSPSuite.Core.Services;
+using OSPSuite.Core.Services.ParameterIdentifications;
+using OSPSuite.Presentation.Presenters.Charts;
+using OSPSuite.Presentation.Services.Charts;
+using OSPSuite.Presentation.Views;
+using OSPSuite.Utility.Events;
+using OSPSuite.Utility.Extensions;
+using Constants = OSPSuite.Core.Domain.Constants;
+
+namespace OSPSuite.Presentation.Presenters
+{
+   public interface ISimulationResidualVsTimeChartPresenter : IChartPresenter<SimulationResidualVsTimeChart>,
+      ISimulationAnalysisPresenter
+      /*,
+      IListener<RenamedEvent>,
+      IListener<ObservedDataAddedToAnalysableEvent>,
+      IListener<ObservedDataRemovedFromAnalysableEvent>,
+      IListener<SimulationResultsUpdatedEvent>*/
+   {
+   }
+
+   //: SimulationAnalysisChartPresenter<SimulationPredictedVsObservedChart, ISimulationPredictedVsObservedAnalysisChartView, ISimulationPredictedVsObservedChartPresenter>,
+   //this should actually implement SimulationRunAnalysisPresenter<SimulationPredictedVsObservedChart>
+   //and the view and presenter interfaces should actually be part of SimulationRunAnalysisPresenter
+   //ISimulationRunAnalysisView should actually get an implementation instead of  ISimulationPredictedVsObservedAnalysisChartView
+   public class SimulationResidualVsTimeChartPresenter : SimulationRunAnalysisPresenter<SimulationResidualVsTimeChart>,
+      ISimulationResidualVsTimeChartPresenter
+   {
+      private readonly ISimulationPredictedVsObservedChartService _predictedVsObservedChartService;
+      private readonly List<DataRepository> _identityRepositories;
+      private readonly IObservedDataRepository _observedDataRepository;
+      private readonly ResidualCalculator _residualCalculator;
+      private IReadOnlyCollection<OutputResiduals> AllOutputResiduals;
+      private string _markerCurveId = string.Empty;
+      private const string ZERO = "Zero";
+
+      public SimulationResidualVsTimeChartPresenter(ISimulationRunAnalysisView view, ChartPresenterContext chartPresenterContext, ISimulationPredictedVsObservedChartService predictedVsObservedChartService, IObservedDataRepository observedDataRepository) 
+         : base(view, chartPresenterContext, ApplicationIcons.PredictedVsObservedAnalysis, PresenterConstants.PresenterKeys.SimulationPredictedVsActualChartPresenter)
+      {
+         _predictedVsObservedChartService = predictedVsObservedChartService;
+         _identityRepositories = new List<DataRepository>();
+         _observedDataRepository = observedDataRepository;
+         //OK this is not working, but we still have the:_
+         //_simulation.ResultRepository;
+         //we could use this for the calculation, but again this is something we have actually added
+         var simulationResidual = _residualCalculator.CalculateForSimulation(_simulation.ResultRepository, _simulation.OutputMappings.All);
+         AllOutputResiduals = simulationResidual.AllOutputResiduals;
+      }
+
+      protected override void UpdateAnalysisBasedOn(IReadOnlyList<IndividualResults> simulationResults)
+      {
+         base.UpdateAnalysisBasedOn(simulationResults);
+         if (!getAllAvailableObservedData().Any())
+            return;
+
+         addZeroMarkerCurveToChart();
+
+         if (ChartIsBeingCreated)
+         {
+            Chart.AxisBy(AxisTypes.Y).Caption = Captions.ParameterIdentification.Residuals;
+            Chart.AxisBy(AxisTypes.Y).Scaling = Scalings.Linear;
+         }
+
+         UpdateChartFromTemplate();
+
+      }
+
+      protected override void AddRunResultToChart()
+      {
+         AllOutputResiduals.GroupBy(x => x.FullOutputPath).Each(addOutputToScatter);
+      }
+
+      protected void AddResultRepositoryToEditor(DataRepository dataRepository)
+      {
+         AddResultRepositoriesToEditor(new[] { dataRepository });
+      }
+
+      protected void AddResultRepositoriesToEditor(IReadOnlyList<DataRepository> dataRepositories)
+      {
+         _resultsRepositories.AddRange(dataRepositories);
+         AddDataRepositoriesToEditor(dataRepositories);
+      }
+
+      private void addOutputToScatter(IGrouping<string, OutputResiduals> outputMappingsByOutput)
+      {
+         var fullOutputPath = outputMappingsByOutput.Key;
+         bool shouldShowInLegend = true;
+         foreach (var outputMapping in outputMappingsByOutput)
+         {
+            var dataRepository = getOrCreateScatterDataRepositoryFor(outputMapping);
+            AddResultRepositoryToEditor(dataRepository);
+            var visibleInLegend = shouldShowInLegend;
+
+            AddCurvesFor(dataRepository, (column, curve) =>
+            {
+               SelectColorForPath(fullOutputPath);
+               UpdateColorForPath(curve, fullOutputPath);
+               curve.Name = fullOutputPath;
+               curve.Description = "description";
+               curve.Symbol = Symbols.Circle;
+               curve.LineStyle = LineStyles.None;
+               curve.VisibleInLegend = visibleInLegend;
+            });
+
+            shouldShowInLegend = false;
+         }
+      }
+
+      private void addZeroMarkerCurveToChart()
+      {
+         var markerRepository = createMarkerRepository();
+         AddDataRepositoriesToEditor(new[] { markerRepository });
+         AddCurvesFor(markerRepository, (column, curve) =>
+         {
+            curve.UpdateMarkerCurve(ZERO);
+            _markerCurveId = curve.Id;
+         });
+      }
+
+      private DataRepository createMarkerRepository()
+      {
+         var id = $"{Chart.Id}-{ZERO}";
+         var dataRepository = createEmptyRepository(id, ZERO, ZERO);
+         dataRepository.BaseGrid.Values = new[] { minObservedDataTime(), MaxObservedDataTime() };
+         dataRepository.FirstDataColumn().Values = new[] { 0f, 0f };
+         return dataRepository;
+      }
+
+      private float minObservedDataTime()
+      { 
+         return getAllAvailableObservedData().Select(x => x.BaseGrid.Values.First()).Min();
+      }
+
+      private float MaxObservedDataTime()
+      {
+         return getAllAvailableObservedData().Select(x => x.BaseGrid.Values.Last()).Max();
+      }
+
+      private DataRepository getOrCreateScatterDataRepositoryFor( OutputResiduals outputResidual)
+      {
+         var repositoryName = "test";//(Captions.ParameterIdentification.SimulationResultsForRun(runIndex);
+         var id = $"{Chart.Id}-{outputResidual.FullOutputPath}-{outputResidual.ObservedData.Id}";
+
+         var timeValues = outputResidual.Residuals.Select(x => x.Time).ToList();
+         var outputValues = outputResidual.Residuals.Select(x => x.Value).ToList();
+
+         var dataRepository = Chart.DataRepositories.FindById(id);
+         if (dataRepository == null)
+         {
+            dataRepository = createScatterDataRepository(id, repositoryName, outputResidual);
+            Chart.AddRepository(dataRepository);
+         }
+
+         dataRepository.BaseGrid.Values = timeValues.ToFloatArray();
+         dataRepository.FirstDataColumn().Values = outputValues.ToFloatArray();
+
+         return dataRepository;
+      }
+
+      private IEnumerable<DataRepository> getAllAvailableObservedData()
+      {
+         return _observedDataRepository.AllObservedDataUsedBy(_simulation)
+            .Distinct()
+            .OrderBy(x => x.Name);
+      }
+
+      private DataRepository createScatterDataRepository(string id, string repositoryName, OutputResiduals outputResidual)
+      {
+         var dataRepository = createEmptyRepository(id, repositoryName, "Values");
+         var scatterColumn = dataRepository.FirstDataColumn();
+         var outputPath = new List<string>(outputResidual.FullOutputPath.ToPathArray());
+         //need to create a unique path containing the observed data name. Since we want to keep the entity name and simulation name, we have to insert before the name
+         if (outputPath.Count > 0)
+            outputPath.Insert(outputPath.Count - 1, outputResidual.ObservedDataName);
+
+         scatterColumn.QuantityInfo.Path = outputPath;
+         return dataRepository;
+      }
+
+      private DataRepository createEmptyRepository(string id, string name, string valueName)
+      {
+         var dataRepository = new DataRepository(id) { Name = name };
+         var baseGrid = new BaseGrid($"{id}-Time", "Time", _chartPresenterContext.DimensionFactory.Dimension(Constants.Dimension.TIME));
+         var values = new DataColumn($"{id}-{valueName}", valueName, _chartPresenterContext.DimensionFactory.NoDimension, baseGrid) { DataInfo = { Origin = ColumnOrigins.CalculationAuxiliary } };
+         dataRepository.Add(values);
+         return dataRepository;
+      }
+
+      public override void Clear()
+      {
+         base.Clear();
+         Chart.RemoveCurve(_markerCurveId);
+      }
+
+      public SimulationResidualVsTimeChart Chart { get; }
+   }
+}
