882bd0c41b26dd76449771c8e2bf2635a6afee0a
Michael Sevestre <michael@design2code.ca>
2022-08-03 20:59:42 -0400

    Fixes #1676 more fix (#1678)

--- src/OSPSuite.UI/Extensions/LayoutControlItemExtensions.cs
+++ src/OSPSuite.UI/Extensions/LayoutControlItemExtensions.cs
@@ -3,6 +3,7 @@ using DevExpress.LookAndFeel;
 using DevExpress.Skins;
 using DevExpress.XtraLayout;
 using OSPSuite.Presentation.Extensions;
+using static OSPSuite.UI.UIConstants.Size;
 
 namespace OSPSuite.UI.Extensions
 {
@@ -17,78 +18,78 @@ namespace OSPSuite.UI.Extensions
          layoutControlItem.Text = text.FormatForLabel();
       }
 
-      public static void AdjustLargeButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl)
+      public static void AdjustLargeButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl = null)
       {
-         layoutControlItem.AdjustSize(layoutControl, UIConstants.Size.LARGE_BUTTON_WIDTH, UIConstants.Size.LARGE_BUTTON_HEIGHT);
+         layoutControlItem.AdjustControlSize(LARGE_BUTTON_WIDTH, LARGE_BUTTON_HEIGHT, layoutControl);
       }
 
-      public static void AdjustLongButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl)
+      public static void AdjustLongButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl = null)
       {
-         layoutControlItem.AdjustSize(layoutControl, UIConstants.Size.LARGE_BUTTON_WIDTH, UIConstants.Size.BUTTON_HEIGHT);
+         layoutControlItem.AdjustControlSize(LARGE_BUTTON_WIDTH, BUTTON_HEIGHT, layoutControl);
       }
 
-      public static void AdjustTallButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl)
+      public static void AdjustTallButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl = null)
       {
-         layoutControlItem.AdjustSize(layoutControl, UIConstants.Size.BUTTON_WIDTH, UIConstants.Size.LARGE_BUTTON_HEIGHT);
+         layoutControlItem.AdjustControlSize(BUTTON_WIDTH, LARGE_BUTTON_HEIGHT, layoutControl);
       }
 
       /// <summary>
-      /// Sets the layout control item size to custom and define the min and max width
+      ///    Sets the layout control item size to custom and define the min and max width
       /// </summary>
-      public static void AdjustSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl, int width, int height)
+      public static void AdjustSize(this LayoutControlItem layoutControlItem, int width, int height, LayoutControl layoutControl = null)
       {
          var size = new Size(width, height);
-         layoutControl.BeginUpdate();
+         layoutControl?.BeginUpdate();
          layoutControlItem.SizeConstraintsType = SizeConstraintsType.Custom;
          layoutControlItem.MaxSize = size;
          layoutControlItem.MinSize = size;
          //In case the control size became smaller that the previous min size, the max size is ignored. So we set it again 
          layoutControlItem.MaxSize = size;
          layoutControlItem.Size = size;
-         layoutControl.EndUpdate();
+         layoutControl?.EndUpdate();
       }
 
       /// <summary>
-      /// Only sets the control item height. Width will be calculated dynamically
+      ///    Only sets the control item height. Width will be calculated dynamically
       /// </summary>
       public static void AdjustControlHeight(this LayoutControlItem layoutControlItem, LayoutControl layoutControl, int height)
       {
          //using 0 allows the control width to be calculated dynamically
-         layoutControlItem.AdjustControlSize(layoutControl, 0, height);
+         layoutControlItem.AdjustControlSize(0, height, layoutControl);
       }
 
       /// <summary>
-      /// Only sets the control item width. Height will be calculated dynamically
+      ///    Only sets the control item width. Height will be calculated dynamically
       /// </summary>
       public static void AdjustControlWidth(this LayoutControlItem layoutControlItem, LayoutControl layoutControl, int width)
       {
          //using 0 allows the control width to be calculated dynamically
-         layoutControlItem.AdjustControlSize(layoutControl, width, 0);
+         layoutControlItem.AdjustControlSize(width, 0, layoutControl);
       }
 
       /// <summary>
-      /// Sets the size of the inner control 
+      ///    Sets the size of the inner control
       /// </summary>
-      public static void AdjustControlSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl, int width, int height)
+      public static void AdjustControlSize(this LayoutControlItem layoutControlItem, int width, int height, LayoutControl layoutControl = null)
       {
          var size = new Size(width, height);
-         layoutControl.BeginUpdate();
+         layoutControl?.BeginUpdate();
          layoutControlItem.SizeConstraintsType = SizeConstraintsType.Custom;
          layoutControlItem.ControlMaxSize = size;
          layoutControlItem.ControlMinSize = size;
          //In case the control size became smaller that the previous min size, the max size is ignored. So we set it again 
          layoutControlItem.ControlMaxSize = size;
-         layoutControl.EndUpdate();
+         layoutControl?.EndUpdate();
       }
 
       public static void AdjustButtonSize(this LayoutControlItem layoutControlItem, LayoutControl layoutControl)
       {
-         layoutControlItem.AdjustSize(layoutControl, UIConstants.Size.BUTTON_WIDTH, UIConstants.Size.BUTTON_HEIGHT);
+         layoutControlItem.AdjustControlSize(BUTTON_WIDTH, BUTTON_HEIGHT, layoutControl);
       }
 
       public static void AdjustButtonSizeWithImageOnly(this LayoutControlItem layoutControlItem, LayoutControl layoutControl)
       {
-         layoutControlItem.AdjustSize(layoutControl, UIConstants.Size.BUTTON_HEIGHT, UIConstants.Size.BUTTON_HEIGHT);
+         layoutControlItem.AdjustControlSize(BUTTON_HEIGHT, BUTTON_HEIGHT, layoutControl);
       }
    }
 }
