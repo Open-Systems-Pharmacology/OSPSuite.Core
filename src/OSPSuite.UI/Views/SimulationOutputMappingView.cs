9385d65532c539ba37df05863334a6b0747f7e23
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-11-14 16:36:02 +0100

    temp

--- src/OSPSuite.UI/Views/SimulationOutputMappingView.cs
+++ src/OSPSuite.UI/Views/SimulationOutputMappingView.cs
@@ -117,6 +117,7 @@ namespace OSPSuite.UI.Views
       private RepositoryItem allOutputsRepository(SimulationOutputMappingDTO dto)
       {
          _outputRepository.FillComboBoxRepositoryWith(_presenter.AllAvailableOutputs);
+         //_outputRepository.Items.Add(null);
          _outputRepository.Items.Add(Captions.SimulationUI.NoneEditorNullText);
          return _outputRepository;
       }
