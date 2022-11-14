0b1eb871b38d097e8f7083082a47629ac4802421
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-11-14 18:52:34 +0100

    adding comment

--- src/OSPSuite.Presentation/Presenters/Charts/DataBrowserPresenter.cs
+++ src/OSPSuite.Presentation/Presenters/Charts/DataBrowserPresenter.cs
@@ -271,6 +271,7 @@ namespace OSPSuite.Presentation.Presenters.Charts
 
       protected override void SetDefaultColumnSettings()
       {
+         //-1 actively removes the grouping for this column if there was one till now - specifying none leaves everything as is.
          AddColumnSettings(BrowserColumns.RepositoryName).WithCaption(Captions.Chart.DataBrowser.RepositoryName).GroupIndex = -1;
          AddColumnSettings(BrowserColumns.Simulation).WithCaption(Captions.SimulationPath);
          AddColumnSettings(BrowserColumns.TopContainer).WithCaption(Captions.TopContainerPath);
