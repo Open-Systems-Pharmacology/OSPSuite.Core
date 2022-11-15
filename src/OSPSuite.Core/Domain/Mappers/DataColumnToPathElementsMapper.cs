221a73452c68216de6fc4716cbe18d1b5df7e899
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2022-11-15 16:01:31 -0500

    Fixes #1792 Deviation and Identity columns are not named in data browser

--- src/OSPSuite.Core/Domain/Mappers/DataColumnToPathElementsMapper.cs
+++ src/OSPSuite.Core/Domain/Mappers/DataColumnToPathElementsMapper.cs
@@ -36,16 +36,18 @@ namespace OSPSuite.Core.Domain.Mappers
          if (dataColumn.IsObservation())
             return ObservedDataPathElementsFor(dataColumn, quantityPath);
 
-         if (dataColumn.IsBaseGrid())
-            return pathElementsForBaseGrid(dataColumn);
+         if (dataColumn.IsBaseGrid() || dataColumn.IsDeviation())
+            return pathElementsForNameOnlyColumn(dataColumn);
 
          return pathElementsForUnknownColumns(dataColumn, quantityPath);
       }
 
-      private static PathElements pathElementsForBaseGrid(DataColumn dataColumn)
+      private static PathElements pathElementsForNameOnlyColumn(DataColumn dataColumn)
       {
-         var pathColumnValues = new PathElements();
-         pathColumnValues[PathElementId.Name] = new PathElement {DisplayName = dataColumn.Name};
+         var pathColumnValues = new PathElements
+         {
+            [PathElementId.Name] = new PathElement {DisplayName = dataColumn.Name}
+         };
          return pathColumnValues;
       }
 
