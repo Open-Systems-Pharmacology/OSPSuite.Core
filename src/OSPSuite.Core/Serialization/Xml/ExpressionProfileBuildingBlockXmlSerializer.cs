c2147c0c61945af7e3021109f9143e7cb00a6e4c
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-11-25 12:01:40 +0100

    adding the serializer initial implementation

---
+++ src/OSPSuite.Core/Serialization/Xml/ExpressionProfileBuildingBlockXmlSerializer.cs
@@ -0,0 +1,15 @@
+﻿using System.Xml.Linq;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
+
+namespace OSPSuite.Core.Serialization.Xml
+{
+   public class ExpressionProfileBuildingBlockXmlSerializer : BuildingBlockXmlSerializer<ExpressionProfileBuildingBlock>
+   {
+      public override void PerformMapping()
+      {
+         Map(x => x.Id);
+         Map(x => x.Name);
+      }
+   }
+}
