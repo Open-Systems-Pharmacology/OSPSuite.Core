cfac6336a20a371ac8127b1be7d5e81359c72177
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-11-28 11:01:14 +0100

    trying to update expression profile

--- src/OSPSuite.Core/Domain/Builder/ExpressionTypes.cs
+++ src/OSPSuite.Core/Domain/Builder/ExpressionTypes.cs
@@ -4,24 +4,41 @@ using System.Text;
 
 namespace OSPSuite.Core.Domain.Builder
 {
+   public static class TempAssets
+   {
+      public static class Captions
+      {
+         public static readonly string Species = "Species";
+         public static readonly string Category = "Category";
+
+         public static string Transporter = "Transporter";
+         public static string Protein = "Protein";
+         public static string Enzyme = "Enzyme";
+      }
+
+      public static class IconNames
+      {
+         public static string Transporter = "Transporter";
+         public static string Protein = "Protein";
+         public static string Enzyme = "Enzyme";
+      }
+   }
+
    public static class ExpressionTypes
    {
-      public static ExpressionType TransportProtein = new ExpressionType(_transporter);
-      public static ExpressionType MetabolizingEnzyme = new ExpressionType(_enzyme);
-      public static ExpressionType ProteinBindingPartner = new ExpressionType(_protein);
-
-      private const string _transporter = "Transporter";
-      private const string _protein = "Protein";
-      private const string _enzyme = "Enzyme";
+      public static ExpressionType TransportProtein = new ExpressionType(TempAssets.IconNames.Transporter, TempAssets.Captions.Transporter);
+      public static ExpressionType MetabolizingEnzyme = new ExpressionType(TempAssets.IconNames.Enzyme, TempAssets.Captions.Enzyme);
+      public static ExpressionType ProteinBindingPartner = new ExpressionType(TempAssets.IconNames.Protein, TempAssets.Captions.Protein);
    }
 
    public class ExpressionType
    {
       public string IconName { get; }
-
-      public ExpressionType(string iconName)
+      public string DisplayName { get; }
+      public ExpressionType(string iconName, string displayName)
       {
          IconName = iconName;
+         DisplayName = displayName;
       }
    }
 }
