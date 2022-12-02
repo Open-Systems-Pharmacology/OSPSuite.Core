5d1e03a5def5e2714537607a169a62ad0eb20492
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-12-02 13:31:32 +0100

    fifth try

--- src/OSPSuite.Core/Serialization/Xml/ExpressionParameterXmlSerializer.cs
+++ src/OSPSuite.Core/Serialization/Xml/ExpressionParameterXmlSerializer.cs
@@ -1,13 +1,41 @@
 using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Serialization.Xml.Extensions;
+using System.Xml.Linq;
 
 namespace OSPSuite.Core.Serialization.Xml
 {
+   /*
    public abstract class ExpressionParameterXmlSerialize : PathAndValueEntityXmlSerializer<ExpressionParameter>
    {
       public override void PerformMapping()
       {
          base.PerformMapping();
          Map(x => x.StartValue);
+      }
+   }
+   */
+
+   public abstract class ExpressionParameterXmlSerialize<T> : EntityXmlSerializer<T> where T : ExpressionParameter
+   {
+      public override void PerformMapping()
+      {
+         base.PerformMapping();
+         Map(x => x.Dimension);
+         Map(x => x.ContainerPath);
+         Map(x => x.StartValue);
+         MapReference(x => x.Formula);
+      }
+
+      protected override void TypedDeserialize(T startValue, XElement startValueElement, SerializationContext serializationContext)
+      {
+         base.TypedDeserialize(startValue, startValueElement, serializationContext);
+         startValueElement.UpdateDisplayUnit(startValue);
+      }
+
+      protected override XElement TypedSerialize(T startValue, SerializationContext serializationContext)
+      {
+         var startValueElement = base.TypedSerialize(startValue, serializationContext);
+         return startValueElement.AddDisplayUnitFor(startValue);
       }
    }
 }
