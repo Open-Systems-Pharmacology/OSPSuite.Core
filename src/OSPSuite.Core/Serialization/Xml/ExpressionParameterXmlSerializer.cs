2f11ce958642ad0200cd7d1beac1517a29cdc18d
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-12-02 13:13:27 +0100

    fourth try

--- src/OSPSuite.Core/Serialization/Xml/ExpressionParameterXmlSerializer.cs
+++ src/OSPSuite.Core/Serialization/Xml/ExpressionParameterXmlSerializer.cs
@@ -1,10 +1,8 @@
-using System.Xml.Linq;
 using OSPSuite.Core.Domain.Builder;
-using OSPSuite.Core.Serialization.Xml.Extensions;
 
 namespace OSPSuite.Core.Serialization.Xml
 {
-   public abstract class ExpressionParameterXmlSerializer : PathAndValueEntityXmlSerializer<ExpressionParameter>
+   public abstract class ExpressionParameterXmlSerialize : PathAndValueEntityXmlSerializer<ExpressionParameter>
    {
       public override void PerformMapping()
       {
--- src/OSPSuite.Core/Serialization/Xml/PathAndValueEntityXmlSerializer.cs
+++ src/OSPSuite.Core/Serialization/Xml/PathAndValueEntityXmlSerializer.cs
@@ -1,10 +1,8 @@
-using System.Xml.Linq;
 using OSPSuite.Core.Domain.Builder;
-using OSPSuite.Core.Serialization.Xml.Extensions;
 
 namespace OSPSuite.Core.Serialization.Xml
 {
-   public abstract class PathAndValueEntityXmlSerializer<T> : EntityXmlSerializer<T> where T : ExpressionParameter
+   public abstract class PathAndValueEntityXmlSerializer<T> : EntityXmlSerializer<T> where T : PathAndValueEntity
    {
       public override void PerformMapping()
       {
@@ -12,18 +10,6 @@ namespace OSPSuite.Core.Serialization.Xml
          Map(x => x.Dimension);
          Map(x => x.ContainerPath);
          MapReference(x => x.Formula);
-      }
-
-      protected override void TypedDeserialize(T startValue, XElement startValueElement, SerializationContext serializationContext)
-      {
-         base.TypedDeserialize(startValue, startValueElement, serializationContext);
-         startValueElement.UpdateDisplayUnit(startValue);
-      }
-
-      protected override XElement TypedSerialize(T startValue, SerializationContext serializationContext)
-      {
-         var startValueElement = base.TypedSerialize(startValue, serializationContext);
-         return startValueElement.AddDisplayUnitFor(startValue);
       }
    }
 }
