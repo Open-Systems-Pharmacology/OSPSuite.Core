963a13cf52f4b5ac4e2b5af734219a77cb073df8
georgeDaskalakis <37107428+georgeDaskalakis@users.noreply.github.com>
2022-12-02 12:05:06 +0100

    ExpressionParameter serializer test

---
+++ src/OSPSuite.Core/Serialization/Xml/ExpressionParameterXmlSerializer.cs
@@ -0,0 +1,30 @@
+using System.Xml.Linq;
+using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Serialization.Xml.Extensions;
+
+namespace OSPSuite.Core.Serialization.Xml
+{
+   public abstract class ExpressionParameterXmlSerializer<T> : EntityXmlSerializer<T> where T : ExpressionParameter
+   {
+      public override void PerformMapping()
+      {
+         base.PerformMapping();
+         Map(x => x.Dimension);
+         Map(x => x.ContainerPath);
+         Map(x => x.StartValue);
+         MapReference(x => x.Formula);
+      }
+
+      protected override void TypedDeserialize(T startValue, XElement startValueElement, SerializationContext serializationContext)
+      {
+         base.TypedDeserialize(startValue, startValueElement, serializationContext);
+         startValueElement.UpdateDisplayUnit(startValue);
+      }
+
+      protected override XElement TypedSerialize(T startValue, SerializationContext serializationContext)
+      {
+         var startValueElement = base.TypedSerialize(startValue, serializationContext);
+         return startValueElement.AddDisplayUnitFor(startValue);
+      }
+   }
+}
