3dfd12050275af1f41974f9335f3b757dc8ba3f8
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2022-12-14 11:49:46 -0500

    PR feedback

--- src/OSPSuite.Core/Domain/Builder/IndividualBuildingBlock.cs
+++ src/OSPSuite.Core/Domain/Builder/IndividualBuildingBlock.cs
@@ -1,31 +1,9 @@
-﻿using OSPSuite.Core.Domain.Services;
+﻿using OSPSuite.Utility.Collections;
 
 namespace OSPSuite.Core.Domain.Builder
 {
    public class IndividualBuildingBlock : PathAndValueEntityBuildingBlockFromPKSim<IndividualParameter>
    {
-      public CoreOriginData OriginData { get; set; }
-
-      public string Species => OriginData.Species;
-
-      public double Age => OriginData.Age?.Value ?? 0;
-
-      public double Weight => OriginData.Weight.Value;
-
-      public double Height => OriginData.Height?.Value ?? 0;
-
-      public string Comment => OriginData.Comment;
-
-      public ValueOrigin ValueOrigin => OriginData.ValueOrigin;
-
-      public override void UpdatePropertiesFrom(IUpdatable source, ICloneManager cloneManager)
-      {
-         base.UpdatePropertiesFrom(source, cloneManager);
-         var sourceIndividual = source as IndividualBuildingBlock;
-         if (sourceIndividual == null) 
-            return;
-
-         OriginData = sourceIndividual.OriginData.Clone();
-      }
+      public Cache<string, string> OriginData { set; get; } = new Cache<string, string>();
    }
 }
