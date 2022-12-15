f5108ad72b96ca90fd3a5f9fd43dd3e578886cb9
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2022-12-15 09:08:27 -0500

    Adding ValueOrigin and CalculationMethodCache

--- src/OSPSuite.Core/Domain/Builder/IndividualBuildingBlock.cs
+++ src/OSPSuite.Core/Domain/Builder/IndividualBuildingBlock.cs
@@ -5,5 +5,7 @@ namespace OSPSuite.Core.Domain.Builder
    public class IndividualBuildingBlock : PathAndValueEntityBuildingBlockFromPKSim<IndividualParameter>
    {
       public Cache<string, string> OriginData { set; get; } = new Cache<string, string>();
+      public ValueOrigin ValueOrigin { set; get; }
+      public CalculationMethodCache CalculationMethodCache { set; get; }
    }
 }
