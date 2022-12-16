6170cc3e22674acad193aa812a8799e9510a3b16
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2022-12-16 09:17:34 -0500

    Fixes #1357 individual building block for mobi (#1831)
    * wip on creating a individual building block in core
    
    * PR feedback
    
    * PR feedback
    
    * reverse scripts submodule changes
    
    * Adding ValueOrigin and CalculationMethodCache

---
+++ src/OSPSuite.Core/Domain/Builder/IndividualBuildingBlock.cs
@@ -0,0 +1,11 @@
+﻿using OSPSuite.Utility.Collections;
+
+namespace OSPSuite.Core.Domain.Builder
+{
+   public class IndividualBuildingBlock : PathAndValueEntityBuildingBlockFromPKSim<IndividualParameter>
+   {
+      public Cache<string, string> OriginData { set; get; } = new Cache<string, string>();
+      public ValueOrigin ValueOrigin { set; get; }
+      public CalculationMethodCache CalculationMethodCache { set; get; }
+   }
+}
