1a36e35a811c6a00243859b4a5aa7ffbbc1479b6
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-01-25 08:11:16 -0500

    Fixes #835 Comparison of MSV BBs - list "Molecule name" and "Path" separately

---
+++ src/OSPSuite.Core/Domain/Mappers/PathAndValueEntityToPathElementsMapper.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Utility;
+using OSPSuite.Utility.Extensions;
+
+namespace OSPSuite.Core.Domain.Mappers
+{
+   public interface IPathAndValueEntityToPathElementsMapper : IMapper<PathAndValueEntity, PathElements>
+   {
+   }
+
+   public class PathAndValueEntityToPathElementsMapper : IPathAndValueEntityToPathElementsMapper
+   {
+      public PathElements MapFrom(PathAndValueEntity input)
+      {
+         switch (input)
+         {
+            case ParameterStartValue psv:
+               return mapAsParameterStartValue(psv);
+            case MoleculeStartValue msv:
+               return mapAsMoleculeStartValue(msv);
+            case ExpressionParameter expressionParameter:
+               return mapAsExpressionParameter(expressionParameter);
+            case IndividualParameter individualParameter:
+               return mapAsIndividualParameter(individualParameter);
+            default:
+               return new PathElements();
+         }
+      }
+
+      private PathElements mapAsIndividualParameter(IndividualParameter individualParameter)
+      {
+         return mapAsPathAndValueEntity(individualParameter, secondLastPathElementIndex(individualParameter));
+      }
+
+      private PathElements mapAsExpressionParameter(ExpressionParameter expressionParameter)
+      {
+         var bottomCompartmentIndex = thirdLastPathElementIndex(expressionParameter);
+         var pathElements = mapAsPathAndValueEntity(expressionParameter, bottomCompartmentIndex);
+         addMolecule(expressionParameter, pathElements, bottomCompartmentIndex);
+
+         return pathElements;
+      }
+
+      private PathElements mapAsMoleculeStartValue(MoleculeStartValue msv)
+      {
+         var bottomCompartmentIndex = secondLastPathElementIndex(msv);
+         var pathElements = mapAsPathAndValueEntity(msv, bottomCompartmentIndex);
+         addMolecule(msv, pathElements, bottomCompartmentIndex);
+
+         return pathElements;
+      }
+
+      private void addMolecule(ExpressionParameter expressionParameter, PathElements pathElements, int bottomCompartmentIndex)
+      {
+         addMolecule(pathElements, expressionParameter.Path.ElementAt(bottomCompartmentIndex + 1));
+      }
+
+      private void addMolecule(MoleculeStartValue msv, PathElements pathElements, int bottomCompartmentIndex)
+      {
+         addMolecule(pathElements, msv.Path.ElementAt(bottomCompartmentIndex + 1));
+      }
+
+      private static void addMolecule(PathElements pathElements, string displayName)
+      {
+         pathElements.Add(PathElementId.Molecule, new PathElement { DisplayName = displayName });
+      }
+
+      private PathElements mapAsParameterStartValue(ParameterStartValue psv)
+      {
+         return mapAsPathAndValueEntity(psv, secondLastPathElementIndex(psv));
+      }
+
+      private PathElements mapAsPathAndValueEntity(PathAndValueEntity pathAndValueEntity, int bottomCompartmentIndex)
+      {
+         var pathElements = new PathElements();
+
+
+         if (pathAndValueEntity.Path.Count > bottomCompartmentIndex)
+            addContainerPaths(pathAndValueEntity, pathElements, bottomCompartmentIndex);
+         if (pathAndValueEntity.Path.Count >= bottomCompartmentIndex)
+            addBottomCompartment(pathAndValueEntity, pathElements, bottomCompartmentIndex);
+         if (pathAndValueEntity.Path.Count >= 2)
+            addNameAndTopContainer(pathAndValueEntity, pathElements);
+
+         return pathElements;
+      }
+
+      private static int secondLastPathElementIndex(PathAndValueEntity pathAndValueEntity) => pathAndValueEntity.Path.Count - 2;
+      private static int thirdLastPathElementIndex(PathAndValueEntity pathAndValueEntity) => pathAndValueEntity.Path.Count - 3;
+
+      private void addContainerPaths(PathAndValueEntity pathAndValueEntity, PathElements pathElements, int bottomCompartmentIndex)
+      {
+         // Skip 1 (the top container) and take until one before the bottom compartment to create a combined ContainerPathElement
+         var containerPath = pathAndValueEntity.Path.Skip(1).Take(bottomCompartmentIndex - 1).ToString(Constants.DISPLAY_PATH_SEPARATOR);
+         if (string.IsNullOrEmpty(containerPath))
+            return;
+
+         pathElements.Add(PathElementId.Container, new PathElement { DisplayName = containerPath });
+      }
+
+      private void addBottomCompartment(PathAndValueEntity pathAndValueEntity, PathElements pathElements, int bottomCompartmentIndex)
+      {
+         pathElements.Add(PathElementId.BottomCompartment, new PathElement { DisplayName = pathAndValueEntity.Path.ElementAt(bottomCompartmentIndex) });
+      }
+
+      private void addNameAndTopContainer(PathAndValueEntity pathAndValueEntity, PathElements pathElements)
+      {
+         pathElements.Add(PathElementId.Name, new PathElement { DisplayName = pathAndValueEntity.Path.Last() });
+         pathElements.Add(PathElementId.TopContainer, new PathElement { DisplayName = pathAndValueEntity.Path.First() });
+      }
+   }
+}
---
+++ tests/OSPSuite.Core.Tests/Domain/PathAndValueEntityToPathElementsMapperSpecs.cs
@@ -0,0 +1,142 @@
+﻿using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Domain.Mappers;
+
+namespace OSPSuite.Core.Domain
+{
+   public class concern_for_PathAndValueEntityToPathElementsMapper : ContextSpecification<PathAndValueEntityToPathElementsMapper>
+   {
+      protected const string _topContainer = "TopContainer";
+      protected const string _container1 = "Container1";
+      protected const string _container2 = "Container2";
+      protected const string _bottomCompartment = "BottomCompartment";
+      protected const string _moleculeName = "MoleculeName";
+      protected const string _parameterName = "ParameterName";
+
+      protected override void Context()
+      {
+         sut = new PathAndValueEntityToPathElementsMapper();
+      }
+   }
+
+   public class When_mapping_molecule_start_values_to_path_elements : concern_for_PathAndValueEntityToPathElementsMapper
+   {
+      private PathElements _result;
+      private MoleculeStartValue _msv;
+
+      protected override void Context()
+      {
+         base.Context();
+         _msv = new MoleculeStartValue
+         {
+            Path = new ObjectPath(_topContainer, _container1, _container2, _bottomCompartment, _moleculeName)
+         };
+      }
+
+      protected override void Because()
+      {
+         _result = sut.MapFrom(_msv);
+      }
+
+      [Observation]
+      public void the_result_should_map_PathElementId_correctly()
+      {
+         _result[PathElementId.Name].DisplayName.ShouldBeEqualTo(_moleculeName);
+         _result[PathElementId.Molecule].DisplayName.ShouldBeEqualTo(_moleculeName);
+         _result[PathElementId.BottomCompartment].DisplayName.ShouldBeEqualTo(_bottomCompartment);
+         _result[PathElementId.TopContainer].DisplayName.ShouldBeEqualTo(_topContainer);
+         _result[PathElementId.Container].DisplayName.ShouldBeEqualTo($"{_container1}{Constants.DISPLAY_PATH_SEPARATOR}{_container2}");
+      }
+   }
+
+   public class When_mapping_expression_parameter_to_path_elements : concern_for_PathAndValueEntityToPathElementsMapper
+   {
+      private PathElements _result;
+      private ExpressionParameter _individualParameter;
+
+      protected override void Context()
+      {
+         base.Context();
+         _individualParameter = new ExpressionParameter
+         {
+            Path = new ObjectPath(_topContainer, _container1, _container2, _bottomCompartment, _moleculeName, _parameterName)
+         };
+      }
+
+      protected override void Because()
+      {
+         _result = sut.MapFrom(_individualParameter);
+      }
+
+      [Observation]
+      public void the_result_should_map_PathElementId_correctly()
+      {
+         _result[PathElementId.Name].DisplayName.ShouldBeEqualTo(_parameterName);
+         _result[PathElementId.Molecule].DisplayName.ShouldBeEqualTo(_moleculeName);
+         _result[PathElementId.BottomCompartment].DisplayName.ShouldBeEqualTo(_bottomCompartment);
+         _result[PathElementId.TopContainer].DisplayName.ShouldBeEqualTo(_topContainer);
+         _result[PathElementId.Container].DisplayName.ShouldBeEqualTo($"{_container1}{Constants.DISPLAY_PATH_SEPARATOR}{_container2}");
+      }
+   }
+
+   public class When_mapping_individual_parameter_to_path_elements : concern_for_PathAndValueEntityToPathElementsMapper
+   {
+      private PathElements _result;
+      private IndividualParameter _individualParameter;
+
+      protected override void Context()
+      {
+         base.Context();
+         _individualParameter = new IndividualParameter
+         {
+            Path = new ObjectPath(_topContainer, _container1, _container2, _bottomCompartment, _parameterName)
+         };
+      }
+
+      protected override void Because()
+      {
+         _result = sut.MapFrom(_individualParameter);
+      }
+
+      [Observation]
+      public void the_result_should_map_PathElementId_correctly()
+      {
+         _result[PathElementId.Name].DisplayName.ShouldBeEqualTo(_parameterName);
+         _result[PathElementId.Molecule].DisplayName.ShouldBeNullOrEmpty();
+         _result[PathElementId.BottomCompartment].DisplayName.ShouldBeEqualTo(_bottomCompartment);
+         _result[PathElementId.TopContainer].DisplayName.ShouldBeEqualTo(_topContainer);
+         _result[PathElementId.Container].DisplayName.ShouldBeEqualTo($"{_container1}{Constants.DISPLAY_PATH_SEPARATOR}{_container2}");
+      }
+   }
+
+   public class When_mapping_parameter_start_values_to_path_elements : concern_for_PathAndValueEntityToPathElementsMapper
+   {
+      private PathElements _result;
+      private ParameterStartValue _psv;
+
+      protected override void Context()
+      {
+         base.Context();
+         _psv = new ParameterStartValue
+         {
+            Path = new ObjectPath(_topContainer, _container1, _container2, _bottomCompartment, _parameterName)
+         };
+      }
+
+      protected override void Because()
+      {
+         _result = sut.MapFrom(_psv);
+      }
+
+      [Observation]
+      public void the_result_should_map_PathElementId_correctly()
+      {
+         _result[PathElementId.Name].DisplayName.ShouldBeEqualTo(_parameterName);
+         _result[PathElementId.Molecule].DisplayName.ShouldBeNullOrEmpty();
+         _result[PathElementId.BottomCompartment].DisplayName.ShouldBeEqualTo(_bottomCompartment);
+         _result[PathElementId.TopContainer].DisplayName.ShouldBeEqualTo(_topContainer);
+         _result[PathElementId.Container].DisplayName.ShouldBeEqualTo($"{_container1}{Constants.DISPLAY_PATH_SEPARATOR}{_container2}");
+      }
+   }
+}
