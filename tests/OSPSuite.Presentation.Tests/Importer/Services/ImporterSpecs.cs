b4549c57a65091c8ceec550d12f5420bdfce3620
Michael Sevestre <michael@design2code.ca>
2023-04-02 16:55:16 -0400

    Remove fake to use real dimensions

--- tests/OSPSuite.Presentation.Tests/Importer/Services/ImporterSpecs.cs
+++ tests/OSPSuite.Presentation.Tests/Importer/Services/ImporterSpecs.cs
@@ -7,6 +7,7 @@ using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Core.Domain.UnitSystem;
 using OSPSuite.Core.Import;
 using OSPSuite.Core.Services;
+using OSPSuite.Helpers;
 using OSPSuite.Infrastructure.Import.Core;
 using OSPSuite.Infrastructure.Import.Core.DataFormat;
 using OSPSuite.Infrastructure.Import.Core.Mappers;
@@ -42,17 +43,11 @@ namespace OSPSuite.Presentation.Importer.Services
       protected IDataSetToDataRepositoryMapper _dataRepositoryMapper;
       protected IDialogCreator _dialogCreator;
       protected ColumnInfoCache _columnInfos;
-      protected IDimension _fakedTimeDimension;
-      protected IDimension _fakedConcentrationDimension;
-      protected IDimension _fakedErrorDimension;
       protected IDimensionFactory _fakedDimensionFactory;
 
       public override void GlobalContext()
       {
          base.GlobalContext();
-         _fakedTimeDimension = A.Fake<IDimension>();
-         _fakedConcentrationDimension = A.Fake<IDimension>();
-         _fakedErrorDimension = A.Fake<IDimension>();
          _fakedDimensionFactory = A.Fake<IDimensionFactory>();
          _basicFormat = A.Fake<DataSheet>();
          _container = A.Fake<IContainer>();
@@ -65,18 +60,15 @@ namespace OSPSuite.Presentation.Importer.Services
             new ColumnInfo() {DisplayName = "Error"}
          };
 
-         _columnInfos["Time"].SupportedDimensions.Add(_fakedTimeDimension);
-         _columnInfos["Concentration"].SupportedDimensions.Add(_fakedConcentrationDimension);
-         _columnInfos["Error"].SupportedDimensions.Add(_fakedErrorDimension);
+         _columnInfos["Time"].SupportedDimensions.Add(DimensionFactoryForSpecs.TimeDimension);
+         _columnInfos["Concentration"].SupportedDimensions.Add(DimensionFactoryForSpecs.ConcentrationDimension);
+         _columnInfos["Error"].SupportedDimensions.Add(DimensionFactoryForSpecs.ConcentrationDimension);
 
          A.CallTo(() => dataFormat.SetParameters(_basicFormat, _columnInfos, null)).Returns(1);
          A.CallTo(() => _container.ResolveAll<IDataFormat>()).Returns(new List<IDataFormat>() {dataFormat});
          _parser = A.Fake<IDataSourceFileParser>();
          _dataRepositoryMapper = A.Fake<IDataSetToDataRepositoryMapper>();
          A.CallTo(() => _container.Resolve<IDataSourceFileParser>()).Returns(_parser);
-         A.CallTo(() => _fakedTimeDimension.HasUnit("min")).Returns(true);
-         A.CallTo(() => _fakedConcentrationDimension.HasUnit("pmol/l")).Returns(true);
-         A.CallTo(() => _fakedErrorDimension.HasUnit("pmol/l")).Returns(true);
          sut = new OSPSuite.Infrastructure.Import.Services.Importer(_container, _parser, _dataRepositoryMapper, _fakedDimensionFactory);
       }
    }
@@ -224,17 +216,11 @@ namespace OSPSuite.Presentation.Importer.Services
       protected IDialogCreator _dialogCreator;
       protected ColumnInfoCache _columnInfos;
       protected IReadOnlyList<MetaDataCategory> _metaDataCategories;
-      protected IDimension _fakedTimeDimension;
-      protected IDimension _fakedConcentrationDimension;
-      protected IDimension _fakedErrorDimension;
       protected IDimensionFactory _fakedDimensionFactory;
 
       public override void GlobalContext()
       {
          base.GlobalContext();
-         _fakedTimeDimension = A.Fake<IDimension>();
-         _fakedConcentrationDimension = A.Fake<IDimension>();
-         _fakedErrorDimension = A.Fake<IDimension>();
          _container = A.Fake<IContainer>();
          _dialogCreator = A.Fake<IDialogCreator>();
          _fakedDimensionFactory = A.Fake<IDimensionFactory>();
@@ -255,15 +241,16 @@ namespace OSPSuite.Presentation.Importer.Services
             new MetaDataCategory() {Name = "Route"}
          };
 
-         _columnInfos["Time"].SupportedDimensions.Add(_fakedTimeDimension);
-         _columnInfos["Concentration"].SupportedDimensions.Add(_fakedConcentrationDimension);
-         _columnInfos["Error"].SupportedDimensions.Add(_fakedErrorDimension);
+         _columnInfos["Time"].SupportedDimensions.Add(DimensionFactoryForSpecs.TimeDimension);
+         _columnInfos["Concentration"].SupportedDimensions.Add(DimensionFactoryForSpecs.ConcentrationDimension);
+         _columnInfos["Error"].SupportedDimensions.Add(DimensionFactoryForSpecs.ConcentrationDimension);
 
-         A.CallTo(() => _fakedTimeDimension.HasUnit("min")).Returns(true);
-         A.CallTo(() => _fakedConcentrationDimension.HasUnit("pmol/l")).Returns(true);
-         A.CallTo(() => _fakedErrorDimension.HasUnit("pmol/l")).Returns(true);
-         A.CallTo(() => _container.ResolveAll<IDataFormat>()).Returns(new List<IDataFormat>()
-            {new DataFormatHeadersWithUnits(), new DataFormatNonmem(), new MixColumnsDataFormat()});
+         A.CallTo(() => _container.ResolveAll<IDataFormat>()).Returns(new List<IDataFormat>
+         {
+            new DataFormatHeadersWithUnits(),
+            new DataFormatNonmem(),
+            new MixColumnsDataFormat()
+         });
          _parser = A.Fake<IDataSourceFileParser>();
          _dataRepositoryMapper = A.Fake<IDataSetToDataRepositoryMapper>();
          A.CallTo(() => _container.Resolve<IDataSourceFileParser>()).Returns(_parser);
@@ -288,11 +275,11 @@ namespace OSPSuite.Presentation.Importer.Services
                   new ColumnDescription(5, null, ColumnDescription.MeasurementLevel.Numeric)
                },
                {
-                  "Concentration [pmol/l]",
+                  "Concentration [mol]",
                   new ColumnDescription(6, null, ColumnDescription.MeasurementLevel.Numeric)
                },
                {
-                  "Error [pmol/l]",
+                  "Error [mol]",
                   new ColumnDescription(7, null, ColumnDescription.MeasurementLevel.Numeric)
                }
             }
@@ -337,7 +324,7 @@ namespace OSPSuite.Presentation.Importer.Services
                },
                {
                   "Concentration_unit",
-                  new ColumnDescription(5, new List<string>() {"pmol/l"}, ColumnDescription.MeasurementLevel.Discrete)
+                  new ColumnDescription(5, new List<string>() {"mol"}, ColumnDescription.MeasurementLevel.Discrete)
                },
                {
                   "Error",
@@ -345,7 +332,7 @@ namespace OSPSuite.Presentation.Importer.Services
                },
                {
                   "Error_unit",
-                  new ColumnDescription(5, new List<string>() {"pmol/l"}, ColumnDescription.MeasurementLevel.Discrete)
+                  new ColumnDescription(5, new List<string>() {"mol"}, ColumnDescription.MeasurementLevel.Discrete)
                }
             }
          );
@@ -385,7 +372,7 @@ namespace OSPSuite.Presentation.Importer.Services
                },
                {
                   "Concentration_unit",
-                  new ColumnDescription(5, new List<string>() {"pmol/l"}, ColumnDescription.MeasurementLevel.Discrete)
+                  new ColumnDescription(5, new List<string>() {"mol"}, ColumnDescription.MeasurementLevel.Discrete)
                },
                {
                   "Error",
@@ -393,7 +380,7 @@ namespace OSPSuite.Presentation.Importer.Services
                },
                {
                   "Error_unit",
-                  new ColumnDescription(5, new List<string>() {"pmol/l"}, ColumnDescription.MeasurementLevel.Discrete)
+                  new ColumnDescription(5, new List<string>() {"mol"}, ColumnDescription.MeasurementLevel.Discrete)
                }
             }
          );
@@ -425,7 +412,7 @@ namespace OSPSuite.Presentation.Importer.Services
                },
                {
                   "Concentration_unit",
-                  new ColumnDescription(5, new List<string>() {"pmol/l"}, ColumnDescription.MeasurementLevel.Discrete)
+                  new ColumnDescription(5, new List<string>() {"mol"}, ColumnDescription.MeasurementLevel.Discrete)
                },
                {
                   "Error",
@@ -433,7 +420,7 @@ namespace OSPSuite.Presentation.Importer.Services
                },
                {
                   "Error_unit",
-                  new ColumnDescription(5, new List<string>() {"pmol/l"}, ColumnDescription.MeasurementLevel.Discrete)
+                  new ColumnDescription(5, new List<string>() {"mol"}, ColumnDescription.MeasurementLevel.Discrete)
                }
             }
          );
@@ -456,11 +443,11 @@ namespace OSPSuite.Presentation.Importer.Services
          _parameterList = new List<DataFormatParameter>
          {
             new MappingDataFormatParameter("time  [h]",
-               new Column() {Name = "Time", Dimension = _fakedTimeDimension, Unit = new UnitDescription()}),
+               new Column() {Name = "Time", Dimension = DimensionFactoryForSpecs.TimeDimension, Unit = new UnitDescription()}),
             new MappingDataFormatParameter("conc  [mg/l]",
                new Column()
                {
-                  Name = "Concentration", Dimension = _fakedConcentrationDimension, Unit = new UnitDescription("mg/l")
+                  Name = "Concentration", Dimension = DimensionFactoryForSpecs.ConcentrationDimension, Unit = new UnitDescription("mg/l")
                })
          };
       }
