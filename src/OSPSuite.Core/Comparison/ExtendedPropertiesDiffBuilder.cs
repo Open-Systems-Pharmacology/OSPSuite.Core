58c9540653e52626870c85c8ff1497650d926a8c
Michael Sevestre <michael@design2code.ca>
2023-04-03 13:37:46 -0400

    Fixes 1954-remove-pk-sim-project-and-add-extension-properties

---
+++ src/OSPSuite.Core/Comparison/ExtendedPropertiesDiffBuilder.cs
@@ -0,0 +1,19 @@
+﻿using OSPSuite.Core.Domain;
+
+namespace OSPSuite.Core.Comparison
+{
+   public class ExtendedPropertiesDiffBuilder : DiffBuilder<ExtendedProperties>
+   {
+      private readonly EnumerableComparer _enumerableComparer;
+
+      public ExtendedPropertiesDiffBuilder(EnumerableComparer enumerableComparer)
+      {
+         _enumerableComparer = enumerableComparer;
+      }
+
+      public override void Compare(IComparison<ExtendedProperties> comparison)
+      {
+         _enumerableComparer.CompareEnumerables(comparison, x => x, x => x.Name);
+      }
+   }
+}
