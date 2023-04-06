30da055b0c9b3ef44bf2cbd1607f4b8a1f76f64d
Michael Sevestre <michael@design2code.ca>
2023-04-06 15:28:00 -0400

    Fixes #1967 add origin

--- src/OSPSuite.Core/Domain/Builder/ExpressionParameter.cs
+++ src/OSPSuite.Core/Domain/Builder/ExpressionParameter.cs
@@ -1,13 +1,6 @@
-﻿using OSPSuite.Core.Domain.Formulas;
-
-namespace OSPSuite.Core.Domain.Builder
+﻿namespace OSPSuite.Core.Domain.Builder
 {
    public class ExpressionParameter : PathAndValueEntity
    {
-      /// <summary>
-      /// Only set for a parameter that is a distributed parameter.
-      /// This is required in order to be able to create distributed parameter dynamically in the simulation
-      /// </summary>
-      public DistributionType? DistributionType { get; set; }
    }
 }
