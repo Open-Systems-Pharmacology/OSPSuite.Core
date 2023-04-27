af779d809fd8f695a064e7574deda4a0b19ef322
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-04-27 08:03:51 -0400

    Fixes #1989 Implement clone of SimulationConfiguration (#1990)
    * Fixes #1989 Implement clone of SimulationConfiguration
    
    * remove spaces

--- src/OSPSuite.Core/Domain/Builder/SimulationConfiguration.cs
+++ src/OSPSuite.Core/Domain/Builder/SimulationConfiguration.cs
@@ -1,18 +1,19 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using OSPSuite.Core.Domain.Services;
 using OSPSuite.Core.Serialization.SimModel.Services;
 using OSPSuite.Utility.Extensions;
 using OSPSuite.Utility.Visitor;
 
 namespace OSPSuite.Core.Domain.Builder
 {
-   public class SimulationConfiguration : IVisitable<IVisitor>
+   public class SimulationConfiguration : IVisitable<IVisitor>, IUpdatable
    {
       private readonly List<ExpressionProfileBuildingBlock> _expressionProfiles = new List<ExpressionProfileBuildingBlock>();
       private readonly List<ModuleConfiguration> _moduleConfigurations = new List<ModuleConfiguration>();
       private readonly List<CoreCalculationMethod> _allCalculationMethods = new List<CoreCalculationMethod>();
-      public SimModelExportMode SimModelExportMode { get; set; } = SimModelExportMode.Full;
 
+      public SimModelExportMode SimModelExportMode { get; set; } = SimModelExportMode.Full;
       public bool ShouldValidate { get; set; } = true;
       public bool ShowProgress { get; set; } = true;
       public bool PerformCircularReferenceCheck { get; set; } = true;
@@ -32,7 +33,7 @@ namespace OSPSuite.Core.Domain.Builder
 
       public IReadOnlyList<T> All<T>() where T : class, IBuildingBlock
       {
-         return ModuleConfigurations.Select(x=>x.BuildingBlock<T>()).Where(x=>x!=null).ToList();
+         return ModuleConfigurations.Select(x => x.BuildingBlock<T>()).Where(x => x != null).ToList();
       }
 
       public virtual void AcceptVisitor(IVisitor visitor)
@@ -40,6 +41,22 @@ namespace OSPSuite.Core.Domain.Builder
          ModuleConfigurations.Each(x => x.AcceptVisitor(visitor));
          Individual?.AcceptVisitor(visitor);
          _expressionProfiles.Each(x => x.AcceptVisitor(visitor));
+      }
+
+      public void UpdatePropertiesFrom(IUpdatable source, ICloneManager cloneManager)
+      {
+         if (!(source is SimulationConfiguration sourceConfiguration))
+            return;
+
+         SimModelExportMode = sourceConfiguration.SimModelExportMode;
+         ShouldValidate = sourceConfiguration.ShouldValidate;
+         ShowProgress = sourceConfiguration.ShowProgress;
+         PerformCircularReferenceCheck = sourceConfiguration.PerformCircularReferenceCheck;
+         sourceConfiguration.ExpressionProfiles.Each(x => AddExpressionProfile(cloneManager.Clone(x)));
+         sourceConfiguration.AllCalculationMethods.Each(x => AddCalculationMethod(cloneManager.Clone(x)));
+         sourceConfiguration.ModuleConfigurations.Each(x => AddModuleConfiguration(cloneManager.Clone(x)));
+         SimulationSettings = cloneManager.Clone(sourceConfiguration.SimulationSettings);
+         Individual = cloneManager.Clone(sourceConfiguration.Individual);
       }
    }
 }
