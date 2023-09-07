e2f773f27a4c20987f7bcc34e6240d64ba895aad
Michael Sevestre <michael@design2code.ca>
2023-09-07 14:41:56 -0400

    Fixes #2092 (#2093)

--- src/OSPSuite.Core/Commands/OSPSuiteLabelCommand.cs
+++ src/OSPSuite.Core/Commands/OSPSuiteLabelCommand.cs
@@ -2,7 +2,7 @@ using OSPSuite.Core.Commands.Core;
 
 namespace OSPSuite.Core.Commands
 {
-   public class OSPSuiteLabelCommand : LabelCommand, IOSPSuiteCommmand<IOSPSuiteExecutionContext>
+   public class OSPSuiteLabelCommand : LabelCommand, IOSPSuiteCommand<IOSPSuiteExecutionContext>
    {
       public string BuildingBlockType { get; set; }
       public string BuildingBlockName { get; set; }
