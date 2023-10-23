04d161740247665465f1e29de6729145e17ca138
Michael Sevestre <michael@design2code.ca>
2023-10-23 11:04:31 -0400

    Fixes #2108

--- src/OSPSuite.Core/Domain/EntityFormulaPath.cs
+++ src/OSPSuite.Core/Domain/EntityFormulaPath.cs
@@ -2,6 +2,14 @@
 
 namespace OSPSuite.Core.Domain
 {
+   /// <summary>
+   ///    Simple class to hold an entity (typically an IUsingFormula), the referenced formula and given path entry in the
+   ///    formula
+   ///    <remarks>
+   ///       We are using IEntity instead of IUsingFormula to ensure that the caller is not tempted to do
+   ///       usingFormula.Formula. In most cases, this would be right, except when dealing with RHSFormula in parameters.
+   ///    </remarks>
+   /// </summary>
    public class EntityFormulaPath
    {
       public IEntity Entity { get; }
