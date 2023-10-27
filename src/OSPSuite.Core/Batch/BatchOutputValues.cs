346c44a2358b1830a233034592ce288577232823
Michael Sevestre <michael@design2code.ca>
2023-10-27 11:53:29 -0400

    Fixes #2155 (#2156)

--- src/OSPSuite.Core/Batch/BatchOutputValues.cs
+++ src/OSPSuite.Core/Batch/BatchOutputValues.cs
@@ -1,10 +1,26 @@
 ﻿namespace OSPSuite.Core.Batch
 {
-   public class BatchOutputValues
+   public class BatchValues
+   {
+      /// <summary>
+      /// Unit in which the values are saved
+      /// </summary>
+      public string Unit { get; set; }
+
+      /// <summary>
+      /// Values saved in Unit
+      /// </summary>
+      public float[] Values { get; set; }
+
+      /// <summary>
+      /// Dimension in which the values are saved
+      /// </summary>
+      public string Dimension { get; set; }
+   }
+
+   public class BatchOutputValues: BatchValues
    {
       public string Path { get; set; }
-      public float[] Values { get; set; }
-      public string Dimension { get; set; }
       public double ComparisonThreshold { get; set; }
    }
 }
