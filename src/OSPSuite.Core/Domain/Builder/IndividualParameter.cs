e5c0270e29364744ce379434726d1af99cd00a96
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2023-11-23 06:21:58 -0500

    Fixes #2169

--- src/OSPSuite.Core/Domain/Builder/IndividualParameter.cs
+++ src/OSPSuite.Core/Domain/Builder/IndividualParameter.cs
@@ -1,5 +1,4 @@
-﻿using OSPSuite.Core.Domain.Formulas;
-using OSPSuite.Core.Domain.Services;
+﻿using OSPSuite.Core.Domain.Services;
 
 namespace OSPSuite.Core.Domain.Builder
 {
@@ -10,12 +9,6 @@ namespace OSPSuite.Core.Domain.Builder
       public ParameterInfo Info { get; set; }
 
       public bool IsDefault { get; set; }
-
-      /// <summary>
-      ///    Only set for a parameter that is a distributed parameter.
-      ///    This is required in order to be able to create distributed parameter dynamically in the simulation
-      /// </summary>
-      public DistributionType? DistributionType { get; set; }
 
       public override void UpdatePropertiesFrom(IUpdatable source, ICloneManager cloneManager)
       {
@@ -28,6 +21,5 @@ namespace OSPSuite.Core.Domain.Builder
          Origin = sourceIndividualParameter.Origin?.Clone();
          IsDefault = sourceIndividualParameter.IsDefault;
       }
-
    }
 }
