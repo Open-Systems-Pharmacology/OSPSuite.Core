b2bd65169e1ed5ceb3455af0a405b353bc7c5e28
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2024-01-08 09:30:29 -0500

    Fixes #2191 Create parameter values for spatial structure and molecules list (#2192)

--- tests/OSPSuite.Core.Tests/Domain/ParameterValuesCreatorSpecs.cs
+++ tests/OSPSuite.Core.Tests/Domain/ParameterValuesCreatorSpecs.cs
@@ -1,4 +1,5 @@
-﻿using FakeItEasy;
+﻿using System.Collections.Generic;
+using System.Linq;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Core.Domain.Builder;
@@ -9,12 +10,9 @@ namespace OSPSuite.Core.Domain
 {
    public abstract class concern_for_ParameterValuesCreator : ContextSpecification<IParameterValuesCreator>
    {
-      protected IObjectBaseFactory _objectBaseFactory;
-
       protected override void Context()
       {
-         _objectBaseFactory = A.Fake<IObjectBaseFactory>();
-         sut = new ParameterValuesCreator(_objectBaseFactory,new ObjectPathFactoryForSpecs(), new IdGenerator());
+         sut = new ParameterValuesCreator(new IdGenerator(), new EntityPathResolverForSpecs());
       }
    }
 
@@ -27,9 +25,10 @@ namespace OSPSuite.Core.Domain
       protected override void Context()
       {
          base.Context();
-         _objectPath =new ObjectPath("A", "B", "C");
+         _objectPath = new ObjectPath("A", "B", "C");
          _parameter = DomainHelperForSpecs.ConstantParameterWithValue(5).WithDimension(DomainHelperForSpecs.FractionDimensionForSpecs());
       }
+
       protected override void Because()
       {
          _psv = sut.CreateParameterValue(_objectPath, _parameter);
@@ -44,4 +43,42 @@ namespace OSPSuite.Core.Domain
       }
    }
 
-}	
+   public class When_creating_parameter_values_for_spatial_structure_and_molecules : concern_for_ParameterValuesCreator
+   {
+      private SpatialStructure _spatialStructure;
+      private List<MoleculeBuilder> _molecules;
+      private IReadOnlyList<ParameterValue> _parameterValues;
+
+      protected override void Context()
+      {
+         base.Context();
+         _spatialStructure = new SpatialStructure();
+         var topContainer = new Container().WithName("Top").WithMode(ContainerMode.Physical);
+         _spatialStructure.AddTopContainer(topContainer);
+         var physicalContainer = new Container().WithName("physicalContainer").WithMode(ContainerMode.Physical);
+         topContainer.Add(physicalContainer);
+         topContainer.Add(new Container().WithName("logicalContainer").WithMode(ContainerMode.Logical));
+
+         _molecules = new List<MoleculeBuilder>();
+
+         var constantFormulaParameter = DomainHelperForSpecs.ConstantParameterWithValue(5).WithDimension(DomainHelperForSpecs.FractionDimensionForSpecs()).WithName("constantFormulaParameterName");
+         var normalDistributionParameter = DomainHelperForSpecs.NormalDistributedParameter().WithDimension(DomainHelperForSpecs.FractionDimensionForSpecs()).WithName("normalDistributionParameterName");
+         var molecule = new MoleculeBuilder().WithName("Molecule1");
+         molecule.AddParameter(constantFormulaParameter);
+         molecule.AddParameter(normalDistributionParameter);
+
+         _molecules.Add(molecule);
+      }
+
+      protected override void Because()
+      {
+         _parameterValues = sut.CreateFrom(_spatialStructure, _molecules);
+      }
+
+      [Observation]
+      public void parameter_values_should_only_be_created_for_each_constant_formula_parameter_in_each_physical_container()
+      {
+         _parameterValues.Select(x => x.Path.ToString()).ShouldOnlyContain("Top|physicalContainer|Molecule1|constantFormulaParameterName", "Top|Molecule1|constantFormulaParameterName");
+      }
+   }
+}
