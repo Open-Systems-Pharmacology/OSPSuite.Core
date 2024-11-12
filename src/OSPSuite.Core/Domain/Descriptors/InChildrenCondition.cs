3749ea9599502cc1140a76f14c4be23b8b7be67c
Michael Sevestre <michael@design2code.ca>
2024-11-12 08:33:40 -0500

    Fixes #2359  (#2360)
    * WIP #2359 add criteria and test. Implementation missing
    
    * WIP #2359 add criteria and test. Implementation missing
    
    * Fixes #2359
    
    * Fixes formatting issue

---
+++ src/OSPSuite.Core/Domain/Descriptors/InChildrenCondition.cs
@@ -0,0 +1,22 @@
+﻿namespace OSPSuite.Core.Domain.Descriptors
+{
+   public class InChildrenCondition : TagCondition
+   {
+      public InChildrenCondition() : base(Constants.IN_CHILDREN, Constants.IN_CHILDREN)
+      {
+      }
+
+      public override string Condition { get; } = Constants.IN_CHILDREN.ToUpper();
+
+      //false because this condition should never be evaluated as it will need to be replaced
+      //by some InContainer conditions in the model
+      public override bool IsSatisfiedBy(EntityDescriptor item) => false;
+
+      public override ITagCondition CloneCondition() => new InChildrenCondition();
+
+      public override void Replace(string keyword, string replacement)
+      {
+         /*nothing to do*/
+      }
+   }
+}
