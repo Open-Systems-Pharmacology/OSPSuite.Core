efd25d00337e61568fb99f77bf71efb84f8f93a6
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-02-19 07:26:59 -0500

    consolidate packages

--- tests/OSPSuite.Core.Tests/Mappers/TransportBuilderToTransportMapperSpecs.cs
+++ tests/OSPSuite.Core.Tests/Mappers/TransportBuilderToTransportMapperSpecs.cs
@@ -6,7 +6,6 @@ using OSPSuite.Core.Domain;
 using OSPSuite.Core.Domain.Builder;
 using OSPSuite.Core.Domain.Formulas;
 using OSPSuite.Core.Domain.Mappers;
-using OSPSuite.Core.Domain.Mappers;
 using OSPSuite.Utility.Extensions;
 
 namespace OSPSuite.Core.Mappers
