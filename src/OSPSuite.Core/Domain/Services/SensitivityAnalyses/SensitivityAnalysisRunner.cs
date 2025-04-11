95d08c4e6a0cbd474748c21f4de599c67d5f7110
benjaperez1983 <benjamin.perez@gmail.com>
2025-04-11 10:17:01 +0200

    #2477 do not lock the ui when running multiple simulations on PK-Sim

--- src/OSPSuite.Core/Domain/Services/SensitivityAnalyses/SensitivityAnalysisRunner.cs
+++ src/OSPSuite.Core/Domain/Services/SensitivityAnalyses/SensitivityAnalysisRunner.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using OSPSuite.Assets;
-using OSPSuite.Utility;
-using OSPSuite.Utility.Exceptions;
 using OSPSuite.Core.Commands;
 using OSPSuite.Core.Domain.SensitivityAnalyses;
 using OSPSuite.Core.Extensions;
 using OSPSuite.Core.Services;
+using OSPSuite.Utility;
+using OSPSuite.Utility.Exceptions;
 
 namespace OSPSuite.Core.Domain.Services.SensitivityAnalyses
 {
@@ -25,16 +28,17 @@ namespace OSPSuite.Core.Domain.Services.SensitivityAnalyses
       private readonly IOSPSuiteExecutionContext _executionContext;
       private readonly ICoreUserSettings _coreUserSettings;
       private ISensitivityAnalysisEngine _sensitivityAnalysisEngine;
+      private readonly ConcurrentDictionary<SensitivityAnalysis, CancellationTokenSource> _cancellationTokenSources = new ConcurrentDictionary<SensitivityAnalysis, CancellationTokenSource>();
 
       public bool IsRunning => _sensitivityAnalysisEngine != null;
 
       public SensitivityAnalysisRunner(
-         ISensitivityAnalysisEngineFactory sensitivityAnalysisEngineFactory,  
+         ISensitivityAnalysisEngineFactory sensitivityAnalysisEngineFactory,
          IDialogCreator dialogCreator,
-         IEntityValidationTask entityValidationTask, 
+         IEntityValidationTask entityValidationTask,
          IOSPSuiteExecutionContext executionContext,
          ICoreUserSettings coreUserSettings
-         )
+      )
       {
          _sensitivityAnalysisEngineFactory = sensitivityAnalysisEngineFactory;
          _dialogCreator = dialogCreator;
@@ -50,14 +54,18 @@ namespace OSPSuite.Core.Domain.Services.SensitivityAnalyses
 
          try
          {
+            var cts = new CancellationTokenSource();
             if (IsRunning)
                throw new OSPSuiteException(Error.CannotStartTwoConcurrentSensitivityAnalyses);
 
-            var options = runOptions ?? new SensitivityAnalysisRunOptions { NumberOfCoresToUse = _coreUserSettings.MaximumNumberOfCoresToUse};
+            if (!_cancellationTokenSources.TryAdd(sensitivityAnalysis, cts)) //this will prevent from running one that is already running
+               throw new OSPSuiteException(Error.CannotStartTwoConcurrentSensitivityAnalyses);
+
+            var options = runOptions ?? new SensitivityAnalysisRunOptions { NumberOfCoresToUse = _coreUserSettings.MaximumNumberOfCoresToUse };
             using (_sensitivityAnalysisEngine = _sensitivityAnalysisEngineFactory.Create())
             {
                var begin = SystemTime.UtcNow();
-               await _sensitivityAnalysisEngine.StartAsync(sensitivityAnalysis, options);
+               await _sensitivityAnalysisEngine.StartAsync(sensitivityAnalysis, options, cts.Token);
                var end = SystemTime.UtcNow();
                var timeSpent = end - begin;
                _dialogCreator.MessageBoxInfo(Captions.SensitivityAnalysis.SensitivityAnalysisFinished(timeSpent.ToDisplay()));
@@ -76,7 +84,14 @@ namespace OSPSuite.Core.Domain.Services.SensitivityAnalyses
 
       public void Stop()
       {
-         _sensitivityAnalysisEngine?.Stop();
+         foreach (var sensitivityAnalysis in _cancellationTokenSources.Keys.ToList())
+         {
+            if (_cancellationTokenSources.TryRemove(sensitivityAnalysis, out var cts))
+            {
+               cts.Cancel();
+               cts.Dispose();
+            }
+         }
       }
    }
 }
