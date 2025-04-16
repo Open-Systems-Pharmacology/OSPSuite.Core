8645864fda6c27261cfb8a39aff1eeb94317d051
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-04-16 10:28:25 -0400

    Fixes #2485 Add the completion date/time to the PI result summary

--- src/OSPSuite.Assets/UIConstants.cs
+++ src/OSPSuite.Assets/UIConstants.cs
@@ -175,6 +175,7 @@ namespace OSPSuite.Assets
       public const string RemoveButtonText = "Remove";
       public static readonly string EnterAValue = "<enter a value>";
       public static readonly string NaN = "<NaN>";
+      public static readonly string NA = "<NA>";
       public static readonly string Analysis = "Analysis";
       public static readonly string CopyAsImage = "Copy as image";
       public static readonly string InvalidObject = "Invalid Object";
@@ -954,6 +955,7 @@ namespace OSPSuite.Assets
          public static readonly string ResidualCount = "# of Residuals";
          public static readonly string Results = "Results";
          public static readonly string NumberOfEvaluations = "Number of Evaluations";
+         public static readonly string CompletedDate = "Completed Date";
          public static readonly string TotalError = "Total Error";
          public static readonly string Status = "Status";
          public static readonly string RunMessage = "Message";
