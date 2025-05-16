d988903093f8fca72a03614142d5eb1259ab0906
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-05-16 13:31:45 -0400

    Fixes #2520 Add snapshot as string to a module (#2521)
    * Fixes #2520 Add snapshot as string to a module
    
    * add test for snapshot
    
    * wrong logic
    
    * PR feedback
    
    * remove pointless test

---
+++ src/OSPSuite.Core/Serialization/SimModel/Serializer/SimModelPrimitiveTypeSerializers.cs
@@ -0,0 +1,14 @@
+namespace OSPSuite.Core.Serialization.SimModel.Serializer
+{
+   public class DoubleSimModelSerializer : PrimitiveTypeSerializer<double, SimModelSerializationContext>, ISimModelSerializer
+   {
+   }
+
+   public class IntSimModelSerializer : PrimitiveTypeSerializer<int, SimModelSerializationContext>, ISimModelSerializer
+   {
+   }
+
+   public class StringSimModelSerializer : PrimitiveTypeSerializer<string, SimModelSerializationContext>, ISimModelSerializer
+   {
+   }
+}
