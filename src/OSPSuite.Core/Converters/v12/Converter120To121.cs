3bb7d47c7873d634d3401300e22c1a99dc6f6bb9
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 12:08:55 -0400

    Fixes #2607 Project conversion (#2608)
    * Fixes #2607 Project conversion
    
    * pr feedback

---
+++ src/OSPSuite.Core/Converters/v12/Converter120To121.cs
@@ -0,0 +1,22 @@
+﻿using OSPSuite.Core.Serialization;
+using System.Xml.Linq;
+
+namespace OSPSuite.Core.Converters.v12
+{
+   public class Converter120To121 : IObjectConverter
+   {
+      // 12.1 pkml is not compatible with 12.0, but you don't need an explicit conversion to move forward.
+      // To satisfy the next converter, the object must pass through v12.1 conversion
+      public bool IsSatisfiedBy(int version) => version == PKMLVersion.V12_0;
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate)
+      {
+         return (PKMLVersion.V12_1, false);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element)
+      {
+         return (PKMLVersion.V12_1, false);
+      }
+   }
+}
