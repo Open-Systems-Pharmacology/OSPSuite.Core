69d3da73a91f70b2bfcb510c476f98c039d8d435
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 10:43:33 -0400

    add empty project conversion

---
+++ src/OSPSuite.Core/Converters/13/Converter121To130.cs
@@ -0,0 +1,20 @@
+﻿using OSPSuite.Core.Serialization;
+using System.Xml.Linq;
+
+namespace OSPSuite.Core.Converters._13
+{
+   public class Converter121To130 : IObjectConverter
+   {
+      public bool IsSatisfiedBy(int version) => version == PKMLVersion.V12_1;
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate)
+      {
+         return (PKMLVersion.V13_0, true);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element)
+      {
+         return (PKMLVersion.V13_0, true);
+      }
+   }
+}
---
+++ src/OSPSuite.Core/Converters/v12/Converter120To1121.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+using OSPSuite.Core.Serialization;
+
+namespace OSPSuite.Core.Converters.v12
+{
+   public class Converter120To1121 : IObjectConverter
+   {
+      public bool IsSatisfiedBy(int version) => version == PKMLVersion.V12_0;
+
+      public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate)
+      {
+         return (PKMLVersion.V12_1, true);
+      }
+
+      public (int convertedToVersion, bool conversionHappened) ConvertXml(XElement element)
+      {
+         return (PKMLVersion.V12_1, true);
+      }
+   }
+}
