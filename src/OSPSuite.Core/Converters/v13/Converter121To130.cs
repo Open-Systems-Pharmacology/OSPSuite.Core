24516d87ac3cff85bb105b165ac27cd692acbd23
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-08-27 14:11:03 -0400

    add comment

--- src/OSPSuite.Core/Converters/v13/Converter121To130.cs
+++ src/OSPSuite.Core/Converters/v13/Converter121To130.cs
@@ -5,6 +5,8 @@ namespace OSPSuite.Core.Converters.v13
 {
    public class Converter121To130 : IObjectConverter
    {
+      // 13.0 pkml is not compatible with 12.1, but you don't need an explicit conversion to move forward.
+      // To satisfy the next converter, the object must pass through v13.0 conversion
       public bool IsSatisfiedBy(int version) => version == PKMLVersion.V12_1;
 
       public (int convertedToVersion, bool conversionHappened) Convert(object objectToUpdate)
