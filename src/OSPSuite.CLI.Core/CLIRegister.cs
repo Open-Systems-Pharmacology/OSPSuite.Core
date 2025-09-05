466f5969da571ba4cfd6a5a368f786f38a47dba0
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-09-05 10:20:49 -0400

    Fixes #2623 Export inputs as the project is created from snapshot

--- src/OSPSuite.CLI.Core/CLIRegister.cs
+++ src/OSPSuite.CLI.Core/CLIRegister.cs
@@ -1,8 +1,9 @@
 ﻿using OSPSuite.CLI.Core.MinimalImplementations;
+using OSPSuite.CLI.Core.Services;
 using OSPSuite.Core;
 using OSPSuite.Core.Commands;
-using OSPSuite.Core.Diagram;
 using OSPSuite.Core.Domain;
+using OSPSuite.Infrastructure.Import.Services;
 using OSPSuite.Utility.Container;
 using IContainer = OSPSuite.Utility.Container.IContainer;
 
@@ -19,14 +20,16 @@ namespace OSPSuite.CLI.Core
             //Register Minimal implementations
             scan.IncludeNamespaceContainingType<DisplayUnitRetriever>();
 
-            // This will be registered as singleton
+            // These will be registered as singleton
             scan.ExcludeType<GroupRepository>();
             scan.WithConvention<OSPSuiteRegistrationConvention>();
+            scan.ExcludeType<CsvSeparatorSelector>();
          });
 
          //Singletons
          container.Register<IGroupRepository, GroupRepository>(LifeStyle.Singleton);
          container.Register<IOSPSuiteExecutionContext, ExecutionContext>(LifeStyle.Singleton);
+         container.Register<ICsvDynamicSeparatorSelector, ICsvSeparatorSelector, CsvSeparatorSelector>(LifeStyle.Singleton);
       }
    }
 }
