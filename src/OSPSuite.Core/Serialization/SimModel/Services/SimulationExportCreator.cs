c22a51d582aac8eb35e8202f138fb6b8310baf97
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-10-07 13:23:42 -0400

    add link to comment

--- src/OSPSuite.Core/Serialization/SimModel/Services/SimulationExportCreator.cs
+++ src/OSPSuite.Core/Serialization/SimModel/Services/SimulationExportCreator.cs
@@ -329,6 +329,7 @@ namespace OSPSuite.Core.Serialization.SimModel.Services
 
             default:
                // We need to use the G15 format specifier to revert to the behavior of .NET Framework for compatibility
+               // https://devblogs.microsoft.com/dotnet/floating-point-parsing-and-formatting-improvements-in-net-core-3-0/#potential-impact-to-existing-code
                return new ExplicitFormulaExport {Equation = formula.Calculate(usingFormula).ToString("G15", NumberFormatInfo.InvariantInfo)};
          }
       }
