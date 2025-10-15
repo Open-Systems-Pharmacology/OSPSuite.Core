ad02ea5c49767bfe510bacbf9230bea162013636
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-10-15 14:36:44 -0400

    Fixes #2661 Simulation runs in .NET Framework but not in .NET 8
    (cherry picked from commit a0030af603538ac184c0d83f54cfbe862e9b11c7)

---
+++ tests/OSPSuite.Core.Tests/Mappers/SimModelNullableDoubleAttributeMapperSpecs.cs
@@ -0,0 +1,28 @@
+﻿using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Serialization.SimModel;
+using OSPSuite.Core.Serialization.SimModel.Serializer;
+
+namespace OSPSuite.Core.Mappers
+{
+   public class concern_for_SimModelNullableDoubleAttributeMapper : ContextSpecification<SimModelNullableDoubleAttributeMapper>
+   {
+      protected SimModelSerializationContext _context;
+
+      protected override void Context()
+      {
+         sut = new SimModelNullableDoubleAttributeMapper();
+         _context = new SimModelSerializationContext();
+      }
+      
+   }
+
+   public class When_converting_nullable_numbers_with_too_much_precision : concern_for_SimModelNullableDoubleAttributeMapper
+   {
+      [Observation]
+      public void the_precision_should_be_reduced()
+      {
+         sut.Convert(0.11704000000000003, _context).ShouldBeEqualTo("0.11704");
+      }
+   }
+}
