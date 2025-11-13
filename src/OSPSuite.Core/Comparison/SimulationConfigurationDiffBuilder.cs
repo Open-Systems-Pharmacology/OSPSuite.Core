104fcb77ada8e6317d30b15abc08a2680e2217a8
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-11-13 05:52:58 -0500

    Fixes #2660 Missing diffbuilder for SimulationConfiguration (#2697)
    * Fixes #2660 Missing diffbuilder for SimulationConfiguration
    
    * simplify missingdiffItem
    
    * remove expression
    
    * names are different

---
+++ src/OSPSuite.Core/Comparison/SimulationConfigurationDiffBuilder.cs
@@ -0,0 +1,36 @@
+﻿using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Domain.Services;
+
+namespace OSPSuite.Core.Comparison
+{
+   public class SimulationConfigurationDiffBuilder : ShallowDiffBuilder<SimulationConfiguration>
+   {
+      private readonly IObjectComparer _objectComparer;
+      private readonly EnumerableComparer _enumerableComparer;
+
+      public SimulationConfigurationDiffBuilder(IObjectComparer objectComparer, IObjectTypeResolver objectTypeResolver, EnumerableComparer enumerableComparer) : base(objectTypeResolver)
+      {
+         _objectComparer = objectComparer;
+         _enumerableComparer = enumerableComparer;
+      }
+
+      public override void Compare(IComparison<SimulationConfiguration> comparison)
+      {
+         AddShallowDifference(x => x.Individual, comparison);
+         
+         AddPresentByNameDifference(x => x.ExpressionProfiles, comparison);
+
+         // perform a full comparison of the settings
+         _objectComparer.Compare(comparison.ChildComparison(x => x.SimulationSettings));
+
+         CompareValues(x => x.SimModelExportMode, x => x.SimModelExportMode, comparison);
+         CompareValues(x => x.CreateAllProcessRateParameters, x => x.CreateAllProcessRateParameters, comparison);
+         CompareValues(x => x.PerformCircularReferenceCheck, x => x.PerformCircularReferenceCheck, comparison);
+         CompareValues(x => x.ShouldValidate, x => x.ShouldValidate, comparison);
+         CompareValues(x => x.ShowProgress, x => x.ShowProgress, comparison);
+
+         _enumerableComparer.CompareEnumerables(comparison, x => x.ModuleConfigurations, x => x.Module.Name);
+      }
+
+   }
+}
---
+++ tests/OSPSuite.Core.IntegrationTests/DiffBuilders/SimulationConfigurationDiffBuilderSpecs.cs
@@ -0,0 +1,225 @@
+﻿using OSPSuite.BDDHelper;
+using OSPSuite.BDDHelper.Extensions;
+using OSPSuite.Core.Domain;
+using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Serialization.SimModel.Services;
+using OSPSuite.Helpers;
+using OSPSuite.Utility.Container;
+
+namespace OSPSuite.Core.DiffBuilders
+{
+   internal abstract class concern_for_SimulationSettingsDiffBuilder : concern_for_ObjectComparer
+   {
+      protected override void Context()
+      {
+         base.Context();
+         var configurationCreator = IoC.Resolve<ModelHelperForSpecs>();
+
+         var simulationConfiguration1 = configurationCreator.CreateSimulationConfiguration();
+         var simulationConfiguration2 = configurationCreator.CreateSimulationConfiguration();
+
+         AdjustSimulationConfigurationsForDiff(simulationConfiguration1, simulationConfiguration2);
+
+         _object1 = simulationConfiguration1;
+         _object2 = simulationConfiguration2;
+      }
+
+      protected abstract void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2);
+   }
+
+   internal class When_comparing_similar_simulation_configuration : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         // No adjustments compare identical configurations
+      }
+
+      [Observation]
+      public void the_report_should_not_contain_any_differences()
+      {
+         _report.Count.ShouldBeEqualTo(0);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_individuals : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration1.Individual.WithName("individual");
+         simulationConfiguration2.Individual.WithName("individual2");
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_module_count : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration1.AddModuleConfiguration(new ModuleConfiguration(new Module()));
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_modules : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration1.AddModuleConfiguration(new ModuleConfiguration(new Module().WithName("module1")));
+         simulationConfiguration2.AddModuleConfiguration(new ModuleConfiguration(new Module().WithName("module2")));
+      }
+
+      [Observation]
+      public void the_report_should_have_differences()
+      {
+         _report.Count.ShouldBeEqualTo(2);
+      }
+   }
+
+   internal class When_comparing_configurations_with_same_module_but_different_selected_initial_conditions : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         var module = new Module();
+         var initialConditionsBuildingBlock = new InitialConditionsBuildingBlock();
+         module.Add(initialConditionsBuildingBlock);
+
+         simulationConfiguration1.AddModuleConfiguration(new ModuleConfiguration(module) { SelectedInitialConditions = initialConditionsBuildingBlock });
+         simulationConfiguration2.AddModuleConfiguration(new ModuleConfiguration(new Module()));
+      }
+
+      [Observation]
+      public void the_report_should_have_differences()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_same_module_but_different_selected_parameter_values : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         var module = new Module();
+         var parameterValues = new ParameterValuesBuildingBlock();
+         module.Add(parameterValues);
+
+         simulationConfiguration1.AddModuleConfiguration(new ModuleConfiguration(module) { SelectedParameterValues = parameterValues });
+         simulationConfiguration2.AddModuleConfiguration(new ModuleConfiguration(new Module()));
+      }
+
+      [Observation]
+      public void the_report_should_have_differences()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_expression_profiles : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration1.AddExpressionProfile(new ExpressionProfileBuildingBlock().WithName("expression|profile|1"));
+         simulationConfiguration2.AddExpressionProfile(new ExpressionProfileBuildingBlock().WithName("expression|profile|2"));
+      }
+
+      [Observation]
+      public void the_report_should_report_difference()
+      {
+         // absent and present x 2
+         _report.Count.ShouldBeEqualTo(2);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_expression_profile_count : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration2.AddExpressionProfile(new ExpressionProfileBuildingBlock().WithName("expression|profile|2"));
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_export_mode : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration2.SimModelExportMode = SimModelExportMode.Optimized;
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_process_rate_creation : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration2.CreateAllProcessRateParameters = true;
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_circular_reference_check : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration2.PerformCircularReferenceCheck = false;
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_should_validate : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration2.ShouldValidate = false;
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+
+   internal class When_comparing_configurations_with_different_show_progress : concern_for_SimulationSettingsDiffBuilder
+   {
+      protected override void AdjustSimulationConfigurationsForDiff(SimulationConfiguration simulationConfiguration1, SimulationConfiguration simulationConfiguration2)
+      {
+         simulationConfiguration2.ShowProgress = false;
+      }
+
+      [Observation]
+      public void the_report_should_have_one_difference()
+      {
+         _report.Count.ShouldBeEqualTo(1);
+      }
+   }
+}
