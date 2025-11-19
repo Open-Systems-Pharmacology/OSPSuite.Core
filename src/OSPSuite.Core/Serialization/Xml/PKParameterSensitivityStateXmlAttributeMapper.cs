ef8d08592386e7a63ea88a05172cf0d1a43c2e6f
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2025-11-19 12:24:05 -0500

    2447 sensitivity analysis runs but no results are produced (#2709)
    * Fixes #2447 Sensitivity analysis runs, but no results are produced
    
    * add test

---
+++ src/OSPSuite.Core/Serialization/Xml/PKParameterSensitivityStateXmlAttributeMapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using OSPSuite.Core.Domain.SensitivityAnalyses;
+using OSPSuite.Serializer.Attributes;
+
+namespace OSPSuite.Core.Serialization.Xml
+{
+   public class PKParameterSensitivityStateXmlAttributeMapper : AttributeMapper<PKParameterSensitivityState, SerializationContext>
+   {
+      public override object ConvertFrom(string attributeValue, SerializationContext context)
+      {
+         var flagValue = int.Parse(attributeValue);
+         return (PKParameterSensitivityState)flagValue;
+      }
+   
+      public override string Convert(PKParameterSensitivityState valueToConvert, SerializationContext context)
+      {
+         return ((int)valueToConvert).ToString(CultureInfo.InvariantCulture);
+      }
+   }
+}
--- tests/OSPSuite.Core.Tests/Services/SensitivityAnalysisRunResultCalculatorSpecs.cs
+++ tests/OSPSuite.Core.Tests/Services/SensitivityAnalysisRunResultCalculatorSpecs.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using FakeItEasy;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
@@ -46,13 +47,13 @@ namespace OSPSuite.Core.Services
       protected override void Context()
       {
          base.Context();
-         _pkParameter1 = new QuantityPKParameter {QuantityPath = "Output1", Name = "AUC"};
+         _pkParameter1 = new QuantityPKParameter { QuantityPath = "Output1", Name = "AUC" };
          _pkParameter1.SetValue(0, 10);
          _pkParameter1.SetValue(1, 11);
          _pkParameter1.SetValue(2, 12);
          _pkParameter1.SetValue(4, _defaultPK1Value);
 
-         _pkParameter2 = new QuantityPKParameter {QuantityPath = "Output2", Name = "Cmax"};
+         _pkParameter2 = new QuantityPKParameter { QuantityPath = "Output2", Name = "Cmax" };
          _pkParameter2.SetValue(0, 20);
          _pkParameter2.SetValue(1, 21);
          _pkParameter2.SetValue(2, 22);
@@ -76,12 +77,12 @@ namespace OSPSuite.Core.Services
          _sensitivityAnalysis.AddSensitivityParameter(_sensitivityParameter2);
          _sensitivityAnalysis.AddSensitivityParameter(_sensitivityParameter3);
 
-         var pv11 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 0, variation: new[] {15d, 200d, 300d});
-         var pv12 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 1, variation: new[] {20d, 200d, 300d});
-         var pv21 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 2, variation: new[] {100d, 21d, 300d});
-         var pv22 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 3, variation: new[] {100d, 31d, 300d});
+         var pv11 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 0, variation: new[] { 15d, 200d, 300d });
+         var pv12 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 1, variation: new[] { 20d, 200d, 300d });
+         var pv21 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 2, variation: new[] { 100d, 21d, 300d });
+         var pv22 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 3, variation: new[] { 100d, 31d, 300d });
 
-         _variationData.DefaultValues = new[] {100d, 200d, 300d};
+         _variationData.DefaultValues = new[] { 100d, 200d, 300d };
          _variationData.AddVariation(pv11);
          _variationData.AddVariation(pv12);
          _variationData.AddVariation(pv21);
@@ -120,15 +121,15 @@ namespace OSPSuite.Core.Services
       }
    }
 
-   public class When_calculating_the_sensitivity_run_results_based_on_a_sensitivity_analysis_and_the_output_should_be_calculated : concern_for_SensitivityAnalysisRunResultCalculator
+   public abstract class When_calculating_the_sensitivity_run_results : concern_for_SensitivityAnalysisRunResultCalculator
    {
-      private SensitivityAnalysisRunResult _result;
+      protected SensitivityAnalysisRunResult _result;
       private QuantityPKParameter _pkParameter1;
       private QuantityPKParameter _pkParameter2;
-      private SensitivityParameter _sensitivityParameter1;
-      private SensitivityParameter _sensitivityParameter2;
-      private readonly float _defaultPK1Value = 50;
-      private readonly float _defaultPK2Value = 70;
+      protected SensitivityParameter _sensitivityParameter1;
+      protected SensitivityParameter _sensitivityParameter2;
+      private float _defaultPK1Value;
+      private float _defaultPK2Value;
       private IndividualResults _resV11;
       private IndividualResults _resV12;
       private IndividualResults _resV21;
@@ -138,13 +139,14 @@ namespace OSPSuite.Core.Services
       protected override void Context()
       {
          base.Context();
-         _pkParameter1 = new QuantityPKParameter {QuantityPath = "Output1", Name = "AUC"};
+         (_defaultPK1Value, _defaultPK2Value) = GetDefaultPkValues();
+         _pkParameter1 = new QuantityPKParameter { QuantityPath = "Output1", Name = "AUC" };
          _pkParameter1.SetValue(0, 10);
          _pkParameter1.SetValue(1, 11);
          _pkParameter1.SetValue(2, 12);
          _pkParameter1.SetValue(4, _defaultPK1Value);
 
-         _pkParameter2 = new QuantityPKParameter {QuantityPath = "Output2", Name = "Cmax"};
+         _pkParameter2 = new QuantityPKParameter { QuantityPath = "Output2", Name = "Cmax" };
          _pkParameter2.SetValue(0, 20);
          _pkParameter2.SetValue(1, 21);
          _pkParameter2.SetValue(2, 22);
@@ -165,37 +167,37 @@ namespace OSPSuite.Core.Services
          _sensitivityAnalysis.AddSensitivityParameter(_sensitivityParameter1);
          _sensitivityAnalysis.AddSensitivityParameter(_sensitivityParameter2);
 
-         var pv11 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 0, variation: new[] {15d, 200d, 300d});
-         var pv12 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 1, variation: new[] {20d, 200d, 300d});
-         var pv21 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 2, variation: new[] {100d, 21d, 300d});
-         var pv22 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 3, variation: new[] {100d, 31d, 300d});
+         var pv11 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 0, variation: new[] { 15d, 200d, 300d });
+         var pv12 = new ParameterVariation(parameterName: _sensitivityParameter1.Name, parameterIndex: 0, variationId: 1, variation: new[] { 20d, 200d, 300d });
+         var pv21 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 2, variation: new[] { 100d, 21d, 300d });
+         var pv22 = new ParameterVariation(parameterName: _sensitivityParameter2.Name, parameterIndex: 1, variationId: 3, variation: new[] { 100d, 31d, 300d });
 
-         _variationData.DefaultValues = new[] {100d, 200d, 300d};
+         _variationData.DefaultValues = new[] { 100d, 200d, 300d };
          _variationData.AddVariation(pv11);
          _variationData.AddVariation(pv12);
          _variationData.AddVariation(pv21);
          _variationData.AddVariation(pv22);
 
-         _timeValues = new QuantityValues {QuantityPath = "Time", Values = new[] {1, 2, 3, 4, 5f}};
+         _timeValues = new QuantityValues { QuantityPath = "Time", Values = new[] { 1, 2, 3, 4, 5f } };
 
-         _resV11 = new IndividualResults {IndividualId = 0, Time = _timeValues };
-         _resV11.Add(new QuantityValues {QuantityPath = "Output1", Values = new[] {111.1f, 211.1f, 311.1f, 411.1f, 511.1f}});
-         _resV11.Add(new QuantityValues {QuantityPath = "Output2", Values = new[] {111.2f, 211.2f, 311.2f, 411.2f, 511.2f}});
+         _resV11 = new IndividualResults { IndividualId = 0, Time = _timeValues };
+         _resV11.Add(new QuantityValues { QuantityPath = "Output1", Values = new[] { 111.1f, 211.1f, 311.1f, 411.1f, 511.1f } });
+         _resV11.Add(new QuantityValues { QuantityPath = "Output2", Values = new[] { 111.2f, 211.2f, 311.2f, 411.2f, 511.2f } });
          _resV11.UpdateQuantityTimeReference();
 
-         _resV12 = new IndividualResults {IndividualId = 1, Time = _timeValues };
-         _resV12.Add(new QuantityValues {QuantityPath = "Output1", Values = new[] {112.1f, 212.1f, 312.1f, 412.1f, 512.1f}});
-         _resV12.Add(new QuantityValues {QuantityPath = "Output2", Values = new[] {112.2f, 212.2f, 312.2f, 412.2f, 512.2f}});
+         _resV12 = new IndividualResults { IndividualId = 1, Time = _timeValues };
+         _resV12.Add(new QuantityValues { QuantityPath = "Output1", Values = new[] { 112.1f, 212.1f, 312.1f, 412.1f, 512.1f } });
+         _resV12.Add(new QuantityValues { QuantityPath = "Output2", Values = new[] { 112.2f, 212.2f, 312.2f, 412.2f, 512.2f } });
          _resV12.UpdateQuantityTimeReference();
 
-         _resV21 = new IndividualResults {IndividualId = 2, Time = _timeValues };
-         _resV21.Add(new QuantityValues {QuantityPath = "Output1", Values = new[] {121.1f, 221.1f, 321.1f, 421.1f, 521.1f}});
-         _resV21.Add(new QuantityValues {QuantityPath = "Output2", Values = new[] {121.2f, 221.2f, 321.2f, 421.2f, 521.2f}});
+         _resV21 = new IndividualResults { IndividualId = 2, Time = _timeValues };
+         _resV21.Add(new QuantityValues { QuantityPath = "Output1", Values = new[] { 121.1f, 221.1f, 321.1f, 421.1f, 521.1f } });
+         _resV21.Add(new QuantityValues { QuantityPath = "Output2", Values = new[] { 121.2f, 221.2f, 321.2f, 421.2f, 521.2f } });
          _resV21.UpdateQuantityTimeReference();
 
-         _resV22 = new IndividualResults {IndividualId = 3, Time = _timeValues };
-         _resV22.Add(new QuantityValues {QuantityPath = "Output1", Values = new[] {122.1f, 222.1f, 322.1f, 422.1f, 522.1f}});
-         _resV22.Add(new QuantityValues {QuantityPath = "Output2", Values = new[] {122.2f, 222.2f, 322.2f, 422.2f, 522.2f}});
+         _resV22 = new IndividualResults { IndividualId = 3, Time = _timeValues };
+         _resV22.Add(new QuantityValues { QuantityPath = "Output1", Values = new[] { 122.1f, 222.1f, 322.1f, 422.1f, 522.1f } });
+         _resV22.Add(new QuantityValues { QuantityPath = "Output2", Values = new[] { 122.2f, 222.2f, 322.2f, 422.2f, 522.2f } });
          _resV22.UpdateQuantityTimeReference();
 
 
@@ -205,9 +207,19 @@ namespace OSPSuite.Core.Services
          _simulationResults.Add(_resV22);
       }
 
+      protected abstract (float defaultPK1Value, float defaultPK2Value) GetDefaultPkValues();
+
       protected override void Because()
       {
          _result = sut.CreateFor(_sensitivityAnalysis, _variationData, _simulationResults, new List<IndividualRunInfo>(), addOutputParameterSensitivitiesToResult: true);
+      }
+   }
+
+   public class When_calculating_the_sensitivity_run_results_based_on_a_sensitivity_analysis_and_the_output_should_be_calculated : When_calculating_the_sensitivity_run_results
+   {
+      protected override (float defaultPK1Value, float defaultPK2Value) GetDefaultPkValues()
+      {
+         return (50, 70);
       }
 
       [Observation]
@@ -222,13 +234,33 @@ namespace OSPSuite.Core.Services
          _result.OutputParameterSensitivitiesBySensitivityParameterName("Output2", _sensitivityParameter2.Name).Length.ShouldBeEqualTo(2);
 
          //test some of the values
-         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output1", _sensitivityParameter1.Name)[0].OutputValues.ShouldBeEqualTo(new[] {111.1f, 211.1f, 311.1f, 411.1f, 511.1f});
+         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output1", _sensitivityParameter1.Name)[0].OutputValues.ShouldBeEqualTo(new[] { 111.1f, 211.1f, 311.1f, 411.1f, 511.1f });
          _result.OutputParameterSensitivitiesBySensitivityParameterName("Output1", _sensitivityParameter1.Name)[0].TimeValues.ShouldBeEqualTo(new[] { 1, 2, 3, 4, 5f });
-         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output1", _sensitivityParameter1.Name)[1].OutputValues.ShouldBeEqualTo(new[] {112.1f, 212.1f, 312.1f, 412.1f, 512.1f});
+         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output1", _sensitivityParameter1.Name)[1].OutputValues.ShouldBeEqualTo(new[] { 112.1f, 212.1f, 312.1f, 412.1f, 512.1f });
 
          //test some of the values
-         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output2", _sensitivityParameter2.Name)[0].OutputValues.ShouldBeEqualTo(new[] {121.2f, 221.2f, 321.2f, 421.2f, 521.2f});
-         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output2", _sensitivityParameter2.Name)[1].OutputValues.ShouldBeEqualTo(new[] {122.2f, 222.2f, 322.2f, 422.2f, 522.2f});
+         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output2", _sensitivityParameter2.Name)[0].OutputValues.ShouldBeEqualTo(new[] { 121.2f, 221.2f, 321.2f, 421.2f, 521.2f });
+         _result.OutputParameterSensitivitiesBySensitivityParameterName("Output2", _sensitivityParameter2.Name)[1].OutputValues.ShouldBeEqualTo(new[] { 122.2f, 222.2f, 322.2f, 422.2f, 522.2f });
+      }
+
+      [Observation]
+      public void state_should_indicate_successful_pk_parameter_calculation()
+      {
+         _result.AllPKParameterSensitivities.All(x => x.State == PKParameterSensitivityState.Success).ShouldBeTrue();
+      }
+   }
+
+   public class When_calculating_the_sensitivity_run_results_based_on_a_sensitivity_analysis_and_the_pk_parameter_cannot_be_calculated : When_calculating_the_sensitivity_run_results
+   {
+      protected override (float defaultPK1Value, float defaultPK2Value) GetDefaultPkValues()
+      {
+         return (0, float.NaN);
+      }
+
+      [Observation]
+      public void state_should_indicate_failed_pk_parameter_calculation()
+      {
+         _result.AllPKParameterSensitivities.All(x => x.State == PKParameterSensitivityState.FailedToCalculateDefaultPKValue).ShouldBeTrue();
       }
    }
 }
