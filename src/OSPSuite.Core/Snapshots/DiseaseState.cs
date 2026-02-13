fe41935ad94027e69f8ae9733afbd16df0260016
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-02-13 15:14:58 -0500

    Fixes #2771 move individualcharacteristics class to core (#2772)
    * Fixes #2771 Move IndividualCharacteristics class to Core
    
    * remove ontogeny
    
    * add file
    
    * PR feedback

---
+++ src/OSPSuite.Core/Snapshots/DiseaseState.cs
@@ -0,0 +1,16 @@
+﻿using OSPSuite.Core.Domain;
+
+namespace OSPSuite.Core.Snapshots;
+
+public class DiseaseState : IWithName
+{
+   /// <summary>
+   ///    Name of disease state associated with OriginData
+   /// </summary>
+   public string Name { get; set; }
+
+   /// <summary>
+   ///    List of disease state parameters associated with the selected disease state
+   /// </summary>
+   public Parameter[] Parameters { get; set; }
+}
