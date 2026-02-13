ae17ec1ed67d75c4a82f9af41203109a87a19fb1
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-02-13 09:17:25 -0500

    remove ontogeny

--- src/OSPSuite.R/Domain/IndividualCharacteristics.cs
+++ src/OSPSuite.R/Domain/IndividualCharacteristics.cs
@@ -4,21 +4,13 @@ using OSPSuite.Core.Snapshots;
 namespace OSPSuite.R.Domain;
 
 /// <summary>
-///    Wrapper object for .net that encapsulates origin data and molecule ontogenies
+///    Wrapper object for .net that encapsulates origin data and disease state
 /// </summary>
 public class IndividualCharacteristics : OriginData
 {
-   private readonly List<MoleculeOntogeny> _moleculeOntogenies = new List<MoleculeOntogeny>();
-
-   public IReadOnlyList<MoleculeOntogeny> MoleculeOntogenies => _moleculeOntogenies;
-
-   public MoleculeOntogeny[] MoleculeOntogeniesAsArray => _moleculeOntogenies.ToArray();
-
-   public void AddMoleculeOntogeny(MoleculeOntogeny moleculeOntogeny) => _moleculeOntogenies.Add(moleculeOntogeny);
-
    public void AddDiseaseStateParameter(Parameter diseaseStateParameter)
    {
-      DiseaseStateParameters = DiseaseStateParameters == null ? new[] { diseaseStateParameter } : new List<Parameter>(DiseaseStateParameters) { diseaseStateParameter }.ToArray();
+      DiseaseStateParameters = DiseaseStateParameters == null ? [diseaseStateParameter] : new List<Parameter>(DiseaseStateParameters) { diseaseStateParameter }.ToArray();
    }
 
    public int? Seed { get; set; }
