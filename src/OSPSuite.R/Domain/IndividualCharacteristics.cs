2dac44d57e180af66c633ac4bc9b0a1d08cef349
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-02-13 14:19:41 -0500

    PR feedback

--- src/OSPSuite.R/Domain/IndividualCharacteristics.cs
+++ src/OSPSuite.R/Domain/IndividualCharacteristics.cs
@@ -4,10 +4,18 @@ using OSPSuite.Core.Snapshots;
 namespace OSPSuite.R.Domain;
 
 /// <summary>
-///    Wrapper object for .net that encapsulates origin data and disease state
+///    Wrapper object for .net that encapsulates origin data and disease state and molecule ontogenies
 /// </summary>
 public class IndividualCharacteristics : OriginData
 {
+   private readonly List<MoleculeOntogeny> _moleculeOntogenies = new List<MoleculeOntogeny>();
+
+   public IReadOnlyList<MoleculeOntogeny> MoleculeOntogenies => _moleculeOntogenies;
+
+   public MoleculeOntogeny[] MoleculeOntogeniesAsArray => _moleculeOntogenies.ToArray();
+
+   public void AddMoleculeOntogeny(MoleculeOntogeny moleculeOntogeny) => _moleculeOntogenies.Add(moleculeOntogeny);
+
    public void AddDiseaseStateParameter(Parameter diseaseStateParameter)
    {
       DiseaseStateParameters = DiseaseStateParameters == null ? [diseaseStateParameter] : new List<Parameter>(DiseaseStateParameters) { diseaseStateParameter }.ToArray();
