d7c20200da175adcbca00d7bea3aa1ea3c7471bc
Benjamin Perez <108261271+benjaperez1983@users.noreply.github.com>
2026-02-23 14:47:11 +0100

    Update SimulationBatch.cs

--- src/OSPSuite.R/Domain/SimulationBatch.cs
+++ src/OSPSuite.R/Domain/SimulationBatch.cs
@@ -80,7 +80,7 @@ namespace OSPSuite.R.Domain
       public void Initialize(IModelCoreSimulation simulation, SimulationBatchOptions simulationBatchOptions, SimulationRunOptions simulationRunOptions)
       {
          _simModelBatch.CheckForNegativeValues = simulationRunOptions?.CheckForNegativeValues ?? true;
-         _simModelBatch.AutoReduceTolerances = simulationRunOptions?.AutoReduceTolerances ?? false;
+         _simModelBatch.AutoReduceTolerances = simulationRunOptions?.AutoReduceTolerances ?? true;
 
          //SimModel optionally caches XML used for loading a simulation as string.
          //This XML string was used e.g. by the old Matlab-/R-Toolbox when saving a simulation to XML.
