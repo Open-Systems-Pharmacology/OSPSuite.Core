b696c8ba2dc1afc19895fa34907e7eaf2b8494bc
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-05-05 17:46:55 +0000

    Fixes #2848 Apply extending module's formula when merging observers (#2849)

--- tests/OSPSuite.Core.Tests/Domain/SimulationBuilderSpecs.cs
+++ tests/OSPSuite.Core.Tests/Domain/SimulationBuilderSpecs.cs
@@ -3,6 +3,7 @@ using FakeItEasy;
 using OSPSuite.BDDHelper;
 using OSPSuite.BDDHelper.Extensions;
 using OSPSuite.Core.Domain.Builder;
+using OSPSuite.Core.Domain.Formulas;
 using OSPSuite.Core.Domain.Services;
 using OSPSuite.Core.Domain.UnitSystem;
 using OSPSuite.Utility.Extensions;
@@ -90,6 +91,116 @@ namespace OSPSuite.Core.Domain
          var observedBuilder = sut.Observers.ElementAt(0);
          sut.MoleculeListFor(observedBuilder).MoleculeNames.ShouldContain("molecule1");
          sut.MoleculeListFor(observedBuilder).MoleculeNames.ShouldContain("molecule2");
+      }
+   }
+
+   internal class When_extending_an_observer_builder_with_a_different_formula : concern_for_SimulationBuilder
+   {
+      private SimulationConfiguration _simulationConfiguration;
+      private ModuleConfiguration _moduleConfiguration1;
+      private ModuleConfiguration _moduleConfiguration2;
+      private ICloneManagerForModel _cloneManagerForModel;
+      private IContainerMergeTask _containerMergeTask;
+      private IFormula _formulaFromModule1;
+      private IFormula _formulaFromModule2;
+
+      protected override void Context()
+      {
+         base.Context();
+         _moduleConfiguration1 = CreateModuleConfiguration();
+         _moduleConfiguration2 = CreateModuleConfiguration();
+
+         _simulationConfiguration = new SimulationConfiguration();
+         _simulationConfiguration.AddModuleConfiguration(_moduleConfiguration1);
+         _simulationConfiguration.AddModuleConfiguration(_moduleConfiguration2);
+
+         _moduleConfiguration2.Module.MergeBehavior = MergeBehavior.Extend;
+
+         var observer1 = _moduleConfiguration1.Module.Observers.AmountObserverBuilders.ElementAt(0);
+         var observer2 = _moduleConfiguration2.Module.Observers.AmountObserverBuilders.ElementAt(0);
+
+         _formulaFromModule1 = A.Fake<IFormula>();
+         _formulaFromModule2 = A.Fake<IFormula>();
+         observer1.Formula = _formulaFromModule1;
+         observer2.Formula = _formulaFromModule2;
+
+         _cloneManagerForModel = A.Fake<ICloneManagerForModel>();
+         _containerMergeTask = A.Fake<IContainerMergeTask>();
+
+         //only testing the formula extend behavior. No need to really clone
+         A.CallTo(() => _cloneManagerForModel.CloneAndKeepId<ObserverBuilder>(observer1)).Returns(observer1);
+         A.CallTo(() => _cloneManagerForModel.CloneAndKeepId<ObserverBuilder>(observer2)).Returns(observer2);
+
+         sut = new SimulationBuilder(_cloneManagerForModel, _containerMergeTask);
+      }
+
+      protected override void Because()
+      {
+         sut.PerformMerge(_simulationConfiguration);
+      }
+
+      [Observation]
+      public void the_formula_should_be_taken_from_the_extending_module()
+      {
+         var observerBuilder = sut.Observers.ElementAt(0);
+         observerBuilder.Formula.ShouldBeEqualTo(_formulaFromModule2);
+      }
+   }
+
+   internal class When_extending_an_observer_builder_across_three_modules_with_different_formulas : concern_for_SimulationBuilder
+   {
+      private SimulationConfiguration _simulationConfiguration;
+      private ModuleConfiguration _moduleConfiguration1;
+      private ModuleConfiguration _moduleConfiguration2;
+      private ModuleConfiguration _moduleConfiguration3;
+      private ICloneManagerForModel _cloneManagerForModel;
+      private IContainerMergeTask _containerMergeTask;
+      private IFormula _formulaFromModule3;
+
+      protected override void Context()
+      {
+         base.Context();
+         _moduleConfiguration1 = CreateModuleConfiguration();
+         _moduleConfiguration2 = CreateModuleConfiguration();
+         _moduleConfiguration3 = CreateModuleConfiguration();
+
+         _simulationConfiguration = new SimulationConfiguration();
+         _simulationConfiguration.AddModuleConfiguration(_moduleConfiguration1);
+         _simulationConfiguration.AddModuleConfiguration(_moduleConfiguration2);
+         _simulationConfiguration.AddModuleConfiguration(_moduleConfiguration3);
+
+         _moduleConfiguration2.Module.MergeBehavior = MergeBehavior.Extend;
+         _moduleConfiguration3.Module.MergeBehavior = MergeBehavior.Extend;
+
+         var observer1 = _moduleConfiguration1.Module.Observers.AmountObserverBuilders.ElementAt(0);
+         var observer2 = _moduleConfiguration2.Module.Observers.AmountObserverBuilders.ElementAt(0);
+         var observer3 = _moduleConfiguration3.Module.Observers.AmountObserverBuilders.ElementAt(0);
+
+         observer1.Formula = A.Fake<IFormula>();
+         observer2.Formula = A.Fake<IFormula>();
+         _formulaFromModule3 = A.Fake<IFormula>();
+         observer3.Formula = _formulaFromModule3;
+
+         _cloneManagerForModel = A.Fake<ICloneManagerForModel>();
+         _containerMergeTask = A.Fake<IContainerMergeTask>();
+
+         A.CallTo(() => _cloneManagerForModel.CloneAndKeepId<ObserverBuilder>(observer1)).Returns(observer1);
+         A.CallTo(() => _cloneManagerForModel.CloneAndKeepId<ObserverBuilder>(observer2)).Returns(observer2);
+         A.CallTo(() => _cloneManagerForModel.CloneAndKeepId<ObserverBuilder>(observer3)).Returns(observer3);
+
+         sut = new SimulationBuilder(_cloneManagerForModel, _containerMergeTask);
+      }
+
+      protected override void Because()
+      {
+         sut.PerformMerge(_simulationConfiguration);
+      }
+
+      [Observation]
+      public void the_formula_should_be_taken_from_the_last_extending_module()
+      {
+         var observerBuilder = sut.Observers.ElementAt(0);
+         observerBuilder.Formula.ShouldBeEqualTo(_formulaFromModule3);
       }
    }
 }
