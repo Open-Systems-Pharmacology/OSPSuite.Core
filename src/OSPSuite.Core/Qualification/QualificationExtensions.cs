8282537a5ace3db18d0bfcc12afd177136c59e29
Robert McIntosh <261477+rwmcintosh@users.noreply.github.com>
2026-06-03 06:59:30 -0400

    Restore ForProject extension used by QualificationRunner
    ForProject<T> was removed as dead, but QualificationRunner (a downstream
    consumer not in the analysis solution) uses it. Not dead — restored.

---
+++ src/OSPSuite.Core/Qualification/QualificationExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OSPSuite.Core.Qualification
+{
+   public static class QualificationExtensions
+   {
+      public static T[] ForProject<T>(this IEnumerable<T> referencingProject, string projectName) where T : IReferencingProject =>
+         referencingProject?.Where(x => string.Equals(x.Project, projectName)).ToArray();
+   }
+}
